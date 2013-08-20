81f5d3ba5d53d1fed76c1720425d1a7cbe1ad2f8
John Simons <john@thesimonshouse.com>
2013-08-20 17:58:15 +1000

    Adding project.

---
+++ src/Pulse.Host/Installer.cs
@@ -0,0 +1,26 @@
+﻿namespace Pulse.Host
+{
+    using System.ComponentModel;
+    using System.ServiceProcess;
+
+    [RunInstaller(true)]
+    public class Installer : ServiceProcessInstaller
+    {
+        private ServiceProcessInstaller processInstaller;
+        private readonly ServiceInstaller serviceInstaller;
+
+        public Installer()
+        {
+            Account = ServiceAccount.LocalSystem;
+            serviceInstaller = new ServiceInstaller
+            {
+                StartType = ServiceStartMode.Automatic,
+                ServiceName = "AdvancedInstallerLicenser",
+                DisplayName = "Advanced Installer Licenser",
+                Description = "Utility for cloud agent that automatically licenses AdvanceInstaller"
+            };
+
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
---
+++ src/Pulse.Host/PulseBootstrapper.cs
@@ -0,0 +1,42 @@
+﻿namespace Pulse.Host
+{
+    using System.IO;
+    using Nancy;
+    using Nancy.Conventions;
+    using Nancy.Diagnostics;
+    using Nancy.Responses;
+
+    public class PulseBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            conventions.StaticContentsConventions.Clear();
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/lib", "/app/lib"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/js", "/app/js"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css", "/app/css"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/img", "/app/img"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/fonts", "/app/fonts"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/partials", "/app/partials"));
+            conventions.StaticContentsConventions.Add((ctx, root) =>
+            {
+                var reqPath = ctx.Request.Path;
+
+                if (!reqPath.Equals("/"))
+                {
+                    return null;
+                }
+
+                var fileName = Path.GetFullPath(Path.Combine(root, "app/index.html"));
+
+                return new GenericFileResponse(fileName, ctx);
+            });
+        }
+
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration {Password = @"Welcome1"}; }
+        }
+    }
+}
