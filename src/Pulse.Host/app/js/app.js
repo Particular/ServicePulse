169e95d93257458cd02c3f818f5dbf88f44dea8a
John Simons <john@thesimonshouse.com>
2013-08-28 11:08:50 +1000

    Testing signalr

--- src/Pulse.Host/app/js/app.js
+++ src/Pulse.Host/app/js/app.js
@@ -2,9 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }])
+    .run(['$rootScope', '$log', function ($rootScope, $log) {
+        $rootScope.$log = $log;
+    }]);
--- src/Pulse.Host/app/js/controllers.js
+++ src/Pulse.Host/app/js/controllers.js
@@ -2,9 +2,19 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+    .controller('MyCtrl1', ['$scope', 'streamService', function ($scope, streamService) {
 
+        $scope.pings = [];
+        $scope.callSignalR = function() {
+            streamService.send('ServiceBus.Management.Infrastructure.SignalR.Ping, ServiceBus.Management', { title: 'John' });
+        };
+        streamService.subscribe($scope, 'ping', function (message) {
+            $scope.$apply(function(scope) {
+                scope.pings.push(message);
+            });
+        });
+        
   }])
   .controller('MyCtrl2', [function() {
 
