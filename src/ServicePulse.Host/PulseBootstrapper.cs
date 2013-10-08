d419fa70d73b7d294101b5adb5214ba2c5d40467
John Simons <john@thesimonshouse.com>
2013-10-08 17:32:57 +1100

    Added embedded resources

--- src/ServicePulse.Host/PulseBootstrapper.cs
+++ src/ServicePulse.Host/PulseBootstrapper.cs
@@ -1,6 +1,8 @@
 ﻿namespace Pulse.Host
 {
+#if DEBUG
     using System;
+#endif
     using System.IO;
     using Nancy;
     using Nancy.Conventions;
@@ -12,8 +14,9 @@
         protected override void ConfigureConventions(NancyConventions conventions)
         {
             base.ConfigureConventions(conventions);
-
+           
             conventions.StaticContentsConventions.Clear();
+#if DEBUG
             conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/lib", "/app/lib"));
             conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/js", "/app/js"));
             conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css", "/app/css"));
@@ -32,29 +35,50 @@
 
                 var fileName = Path.GetFullPath(Path.Combine(root, "app/index.html"));
 
-                return new GenericFileResponse(fileName, ctx);
-            });
-        }
+                 return new GenericFileResponse(fileName, ctx);
+                });
+                
+#else
+            conventions.StaticContentsConventions.Add((ctx, root) =>
+            {
+                var reqPath = ctx.Request.Path.Replace('/', '.');
 
-        protected override IRootPathProvider RootPathProvider
-        {
-            get { return new CustomRootPathProvider(); }
+                if (reqPath.Equals("."))
+                {
+                    reqPath = "index.html";
+                }
+
+                if (reqPath.StartsWith("."))
+                {
+                    reqPath = reqPath.Substring(1);
+                }
+
+                return new EmbeddedFileResponse(
+                    GetType().Assembly,
+                    "ServicePulse.Host.app",
+                    Path.GetFileName(reqPath));
+            });
+#endif
         }
 
         protected override DiagnosticsConfiguration DiagnosticsConfiguration
         {
             get { return new DiagnosticsConfiguration {Password = @"Welcome1"}; }
         }
+#if DEBUG
+        protected override IRootPathProvider RootPathProvider
+        {
+            get { return new CustomRootPathProvider(); }
+        }
 
         class CustomRootPathProvider : IRootPathProvider
         {
             public string GetRootPath()
             {
-#if DEBUG
+
                 return Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\"));
-#endif
-                return AppDomain.CurrentDomain.BaseDirectory;
             }
         }
+#endif
     }
 }
