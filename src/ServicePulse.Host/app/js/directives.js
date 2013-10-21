9cfd115b9fc1a894a52e14aac18cd8d253fa892b
John Simons <john@thesimonshouse.com>
2013-10-21 17:41:45 +1100

    Added moment for dates
    Added NoIE page

--- src/ServicePulse.Host/app/js/directives.js
+++ src/ServicePulse.Host/app/js/directives.js
@@ -3,24 +3,36 @@
 /* Directives */
 angular.module('sc.directives', [])
     .directive('moment', ['$timeout', function($timeout) {
-        return function(scope, element, attrs) {
-            var timeoutId;
+        return {
+            restrict: 'E',
+            link: function(scope, element, attrs) {
+                var timeoutId = null;
 
-            function updateText() {
-                element.text(moment(attrs.moment).fromNow());
+                attrs.$observe('date', function (value) {
+                    if (value) {
+                        updateText();
+                        updateLoop();
+                    }
+                });
+
+                function updateText() {
+                    element.text(moment(attrs.date).fromNow());
+                }
+
+                function updateLoop() {
+                    timeoutId = $timeout(function() {
+                        updateText();
+                        updateLoop();
+                    }, 5000);
+                }
+
+                element.on('$destroy', function() {
+                    if (timeoutId == null) {
+                        return;
+                    }
+
+                    $timeout.cancel(timeoutId);
+                });
             }
-
-            function updateLoop() {
-                timeoutId = $timeout(function() {
-                    updateText();
-                    updateLoop();
-                }, 5000);
-            }
-
-            element.on('$destroy', function() {
-                $timeout.cancel(timeoutId);
-            });
-
-            updateLoop();
         };
     }]);
