05215d0f2465a40f60cc82ea70302acfbe4d12ab
John Simons <john.simons@nservicebus.com>
2014-01-17 09:35:07 +1100

    Bumped beta expire to 1st of May

--- src/ServicePulse.Host/app/js/app.js
+++ src/ServicePulse.Host/app/js/app.js
@@ -41,7 +41,7 @@ angular.module('sc').controller('AppCtrl', ['$scope', 'notifications', function 
         notifications.remove(notification);
     };
     
-    if (new Date() > new Date(2014, 2, 10)) {
+    if (new Date() > new Date(2014, 5, 1)) {
         notifications.pushSticky('<h4>Beta period has elapsed!</h4>Continued use is of this version of ServicePulse is unauthorized. To receive the latest and licensed release of ServicePulse please go to <a href="http://particular.net/downloads">http://particular.net/downloads</a>', 'info');
     }
 
