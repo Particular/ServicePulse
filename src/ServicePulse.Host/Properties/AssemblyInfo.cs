fcb671d634c16b77b80475736df9dd4bbf38a730
John Simons <john.simons@nservicebus.com>
2014-02-13 15:14:49 +1100

    Updated Assemblyinfo

--- src/ServicePulse.Host/Properties/AssemblyInfo.cs
+++ src/ServicePulse.Host/Properties/AssemblyInfo.cs
@@ -1,10 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ServicePulse.Host")]
-[assembly: AssemblyDescription("Monitoring and operations for NServiceBus")]
-[assembly: AssemblyCopyright("Copyright 2013 NServiceBus Ltd. All rights reserved")]
-[assembly: AssemblyProduct("ServicePulse")]
+[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
 [assembly: AssemblyCompany("NServiceBus Ltd.")]
-[assembly: AssemblyConfiguration("release")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("ServicePulse")]
