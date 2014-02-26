d8736a3f5f51678d53d398c1601756f9c4d95f9c
Simon Cropp <simon.cropp@gmail.com>
2014-02-26 20:47:18 +1100

    add logging to msi exec

--- src/Chocolatey/chocolateyInstall.ps1
+++ src/Chocolatey/chocolateyInstall.ps1
@@ -1,1 +1,25 @@
-﻿Install-ChocolateyPackage 'ServicePulse.install' 'exe' '' 'https://github.com/Particular/ServicePulse/releases/download/SemVer/Particular.ServicePulse-SemVer.exe'
+﻿$packageName = "ServicePulse"
+$url = "https://github.com/Particular/ServicePulse/releases/download/SemVer/Particular.ServicePulse-SemVer.exe"
+
+try {
+
+    $chocTempDir = Join-Path $env:TEMP "chocolatey"
+    $tempDir = Join-Path $chocTempDir "$packageName"
+    if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+    $file = Join-Path $tempDir "$($packageName)Install.exe"
+    $logFile = Join-Path $tempDir "msiexe.log"
+
+	If (Test-Path $logFile){
+		Remove-Item $logFile
+	}
+
+    Get-ChocolateyWebFile $packageName $file $url 
+	$msiArguments  ="/quiet  /L*V `"$logFile`""
+	Write-Host "Starting installer with arguments: $msiArguments";
+    Start-ChocolateyProcessAsAdmin "$msiArguments" $file -validExitCodes 0
+
+    Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
