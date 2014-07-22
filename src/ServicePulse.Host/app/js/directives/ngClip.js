1e3229ebaae51121ea404c171a8533b8aace5547
Greg Bielleman <greg.bielleman@gmail.com>
2014-07-22 16:00:12 +1000

    updates ngClip tp use new Zeroclipboard

--- src/ServicePulse.Host/app/js/directives/ngClip.js
+++ src/ServicePulse.Host/app/js/directives/ngClip.js
@@ -1,10 +1,8 @@
+
 'use strict';
 
 angular.module('ngClipboard', []).
-  value('ZeroClipboardConfig', {
-     swfpath: '/lib/ZeroClipboard.swf'
-  }).
-  directive('clipCopy', ['ZeroClipboardConfig', function () {
+  directive('clipCopy', function () {
       return {
           scope: {
               clipComplete: '&',
@@ -12,25 +10,26 @@ angular.module('ngClipboard', []).
           },
           restrict: 'A',
           link: function (scope, element, attrs) {
-              // Create the clip object
-              var clip = new ZeroClipboard(element);
 
-              clip.on('load', function () {
-                  console.log("loaded");
+              var clip = new ZeroClipboard(element);
+              clip.on('ready', function () {
+                  console.log('ready');
                   if (angular.isDefined(attrs.clipLoad)) {
                       scope.$apply(scope.clipLoad);
                   }
               });
-              clip.on('complete', function () {
-                  console.log("loaded");
+
+              clip.on('aftercopy', function () {
+                  console.log('copied data');
                   if (angular.isDefined(attrs.clipComplete)) {
                       scope.$apply(scope.clipComplete);
                   }
-                  console.log('Copy button 2');
               });
-              clip.on("noflash wrongflash", function() {
+
+              clip.on('error', function (e) {
+                  console.log('error' + e.message );
                   return element.remove();
               });
           }
       };
-  }]);
+  });
