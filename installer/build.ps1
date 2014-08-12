e020babad4fa2bdf40fe7ff186a39c24e7504ad1
Greg Bielleman <greg.bielleman@gmail.com>
2014-08-12 14:10:22 +1000

    Fix version number handling in installer build

--- installer/build.ps1
+++ installer/build.ps1
@@ -6,11 +6,27 @@ param(
 
 pushd $PSScriptRoot
 
+#Copy CustomActions
+New-Item -Name "CustomActions" -ItemType Directory -Force | Out-Null
+Get-ChildItem CustomActions |  Remove-Item -Force
+Copy-Item ..\src\ServicePulse.Install.CustomActions\bin\Debug\ServicePulse.Install.CustomActions.CA.dll -Destination .\CustomActions -ErrorAction Stop
+ 
+#Copy Binaries 
+New-Item -Name "Binaries" -ItemType Directory -Force | Out-Null
+Get-ChildItem Binaries | Remove-Item -Force
+Copy-Item -Container ..\src\ServicePulse.Host\bin\Debug\*.* -Destination .\Binaries -ErrorAction Stop
+
+
 $AdvancedInstallerPath = Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Caphyon\Advanced Installer\"  | Select -ExpandProperty "Advanced Installer Path" 
 $AdvinstCLI = Join-Path -Path $AdvancedInstallerPath -ChildPath "bin\x86\AdvancedInstaller.com"
 
 $setupProjectFile = "ServicePulse.aip"
 $packageName = "Particular.ServicePulse-$PackageVersion.exe"
+
+# MSI version must be in classic x.x.x.x format
+if ($PackageVersion -match "-") {
+    $PackageVersion = "$PackageVersion".Split('-')[0]
+}
 
 # edit Advanced Installer Project   
 & $AdvinstCLI /edit $setupProjectFile /SetVersion $PackageVersion
