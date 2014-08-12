a7b0682ba0c97573398ce47f2762491add2c1142
Greg Bielleman <greg.bielleman@gmail.com>
2014-08-12 12:16:38 +1000

    Change Installer script param name
    version clashes with Powershell.exe command line args

--- installer/build.ps1
+++ installer/build.ps1
@@ -1,7 +1,7 @@
 #Requires -Version 3
 
 param(
-    [parameter(Mandatory=$True)] [string]$version
+    [parameter(Mandatory=$True)] [string] $PackageVersion
 )
 
 pushd $PSScriptRoot
@@ -10,10 +10,10 @@ $AdvancedInstallerPath = Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Caphyon\Ad
 $AdvinstCLI = Join-Path -Path $AdvancedInstallerPath -ChildPath "bin\x86\AdvancedInstaller.com"
 
 $setupProjectFile = "ServicePulse.aip"
-$packageName = "Particular.ServicePulse-$version.exe"
+$packageName = "Particular.ServicePulse-$PackageVersion.exe"
 
 # edit Advanced Installer Project   
-& $AdvinstCLI /edit $setupProjectFile /SetVersion $version
+& $AdvinstCLI /edit $setupProjectFile /SetVersion $PackageVersion
 & $AdvinstCLI /edit $setupProjectFile /SetPackageName $packageName -buildname DefaultBuild
         
 # Build setup with Advanced Installer 
