fb5994ce07b72016c6e865ec367fe677dd298421
Pete <peter.giles@particular.net>
2015-11-03 10:14:10 +1100

    tests are running now

--- src/ServicePulse.Host.Tests/tests/js/angular_tests.js
+++ src/ServicePulse.Host.Tests/tests/js/angular_tests.js
@@ -1,10 +1,35 @@
-﻿///<reference path="_references.js"/>
+﻿/// <reference path="_references.js" />
+
+describe('Angular libraries available in tests', function () {
+
+    //fails
+    it('should load angular.mock.module', function () {
+        expect(typeof (angular.mock.module) == typeof (undefined)).toEqual(false);
+    });
+
+    //fails
+    it('should load window.module', function () {
+        expect(typeof (module) == typeof (undefined)).toEqual(false);
+    });
+
+    //passes
+    it('should load angular', function () {
+        expect(typeof (angular) == typeof (undefined)).toEqual(false);
+    });
+
+    //passes
+    it('should load angular.mock', function () {
+        expect(typeof (angular.mock) == typeof (undefined)).toEqual(false);
+    });
+
+});
+
 
 var myApp = angular.module('myApp', []);
 
-//myApp.controller('HelloWorldController', ['$scope', function ($scope) {
-//    $scope.greeting = 'Hello World!';
-//}]);
+myApp.controller('HelloWorldController', ['$scope', function ($scope) {
+    $scope.greeting = 'Hello World!';
+}]);
 
 describe('Hello World example ', function () {
 
@@ -13,15 +38,15 @@ describe('Hello World example ', function () {
     var HelloWorldController,
     scope;
 
-    //beforeEach(inject(function ($rootScope, $controller) {
-    //    scope = $rootScope.$new();
-    //    HelloWorldController = $controller('HelloWorldController', {
-    //        $scope: scope
-    //    });
-    //}));
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        HelloWorldController = $controller('HelloWorldController', {
+            $scope: scope
+        });
+    }));
 
     it('says hello world!', function () {
-        expect(scope.greeting).toEqual('Hello world!');
+        expect(scope.greeting).toEqual('Hello World!');
     });
 
     it('hello!', function () {
