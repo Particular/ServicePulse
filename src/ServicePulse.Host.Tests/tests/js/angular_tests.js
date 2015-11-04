a1c7cca1f2a94ab36104b3b5f228a61f6d277714
Pete <peter.giles@particular.net>
2015-11-04 13:25:25 +1100

    clean up after merge

---
+++ src/ServicePulse.Host.Tests/tests/js/angular_tests.js
@@ -0,0 +1,56 @@
+﻿/// <reference path="_references.js" />
+
+describe('Angular libraries available in tests', function () {
+
+    //fails
+    it('should load angular.mock.module', function () {
+        expect(typeof (angular.mock.module) == typeof (undefined)).toEqual(false);
+    });
+
+    //fails
+    it('should load window.module', function () {
+        expect(typeof (module) == typeof (undefined)).toEqual(false);
+    });
+
+    //passes
+    it('should load angular', function () {
+        expect(typeof (angular) == typeof (undefined)).toEqual(false);
+    });
+
+    //passes
+    it('should load angular.mock', function () {
+        expect(typeof (angular.mock) == typeof (undefined)).toEqual(false);
+    });
+
+});
+
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('HelloWorldController', ['$scope', function ($scope) {
+    $scope.greeting = 'Hello World!';
+}]);
+
+describe('Hello World example ', function () {
+
+    beforeEach(module('myApp'));
+
+    var HelloWorldController,
+    scope;
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        HelloWorldController = $controller('HelloWorldController', {
+            $scope: scope
+        });
+    }));
+
+    it('says hello world!', function () {
+        expect(scope.greeting).toEqual('Hello World!');
+    });
+
+    it('hello!', function () {
+        expect('hello!').toEqual('hello!');
+    });
+
+});
---
+++ src/ServicePulse.Host.Tests/tests/js/services/services.uri.tests.js
@@ -0,0 +1,66 @@
+﻿/// <reference path="../_references.js" />
+
+var scConfig = scConfig || {};
+scConfig.service_control_url = 'http://localhost:33333/api/';
+
+describe("Unit: Uri Service ", function() {
+
+    it('should load angular', function() {
+        expect(typeof (angular) == typeof (undefined)).toEqual(false);
+    });
+
+    describe("Uri Joins:", function() {
+
+        beforeEach(module('services.uri'));
+
+        it('should contain a uri service',
+            inject(function(uri) {
+                expect(typeof (uri) == typeof(undefined)).toEqual(false);;
+            }));
+
+        it('should make a url for eventlogitems', inject(function(uri) {
+            var url = uri.join('http://localhost:33333/api/', 'eventlogitems');
+            expect(url).toEqual('http://localhost:33333/api/eventlogitems');
+        }));
+
+        it('should make a url for get Failed Messages For Exception Group', inject(function(uri) {
+            var groupId = '85147b12-458c-431d-a389-35ea53abc9e1';
+            var page = 1;
+            var sortBy = 'time_sent';
+            var url = uri.join(scConfig.service_control_url, 'recoverability', 'groups', groupId, 'errors?page=' + page + '&sort=' + sortBy);
+            expect(url).toEqual('http://localhost:33333/api/recoverability/groups/85147b12-458c-431d-a389-35ea53abc9e1/errors?page=1&sort=time_sent');
+        }));
+
+
+        it('should make a url for get Failed Messages For Exception Group', inject(function(uri) {
+            var messageId = '85147b12-458c-431d-a389-35ea53abc9e1';
+            var url = uri.join(scConfig.service_control_url, 'messages', messageId, 'body');
+            expect(url).toEqual('http://localhost:33333/api/messages/85147b12-458c-431d-a389-35ea53abc9e1/body');
+        }));
+
+
+        it('should make a url for getMessageBody', inject(function(uri) {
+            var messageId = '85147b12-458c-431d-a389-35ea53abc9e1';
+            var url = uri.join(scConfig.service_control_url, 'messages', messageId, 'body');
+            expect(url).toEqual('http://localhost:33333/api/messages/85147b12-458c-431d-a389-35ea53abc9e1/body');
+        }));
+
+        it('should make a url for getMessageHeaders', inject(function(uri) {
+            var messageId = '85147b12-458c-431d-a389-35ea53abc9e1';
+            var url = uri.join(scConfig.service_control_url, 'messages', 'search', messageId);
+            expect(url).toEqual('http://localhost:33333/api/messages/search/85147b12-458c-431d-a389-35ea53abc9e1');
+        }));
+
+        it('should make a url for getTotalFailedMessages', inject(function(uri) {
+
+            var url = uri.join(scConfig.service_control_url, 'errors?status=unresolved');
+            expect(url).toEqual('http://localhost:33333/api/errors?status=unresolved');
+        }));
+
+        it('should make a url for retry errors', inject(function(uri) {
+            var url = uri.join(scConfig.service_control_url, 'errors', 'retry');
+            expect(url).toEqual('http://localhost:33333/api/errors/retry');
+        }));
+
+    });
+});
