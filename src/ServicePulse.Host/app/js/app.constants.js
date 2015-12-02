50a16dcdcc29ed00d16533c7a9e64c35d6e7a4c7
Ramon Smits <ramon.smits@gmail.com>
2015-12-02 17:32:59 +0100

    Fixes #235

--- src/ServicePulse.Host/app/js/app.constants.js
+++ src/ServicePulse.Host/app/js/app.constants.js
@@ -4,7 +4,7 @@
         .constant('version', '1.2.0')
         .constant('scConfig', {
             service_control_url: 'http://localhost:33333/api/',
-            service_pulse_url: 'http://platformupdate.particular.net/servicepulse.txt'
+            service_pulse_url: '//platformupdate.particular.net/servicepulse.txt'
         });
 
 }(window, window.angular));
