c9f5e0a98eef642e64190170fdb2545115d64434
Pete <peter.giles@particular.net>
2015-12-03 14:17:24 +1100

    Added an error handler for version check

--- src/ServicePulse.Host/app/js/services/services.platform-update.js
+++ src/ServicePulse.Host/app/js/services/services.platform-update.js
@@ -1,15 +1,33 @@
-﻿'use strict';
+﻿;
+(function(window, angular, undefined) {
+    "use strict";
 
-angular.module('services.platformUpdateService', [])
-    .service('platformUpdateService', [
-        '$http', 'scConfig', function ($http, scConfig) {
+    function service($http, scConfig) {
 
-            this.getReleases = function () {
-                return $http
-                    .get(scConfig.service_pulse_url, { responseType: 'json' })
-                    .then(function (response) {
-                        return response;
-                    });
-            };
-        }
-    ]);
+        function _getReleases() {
+            return $http
+                .get(scConfig.service_pulse_url, {
+                     responseType: "json"
+                })
+                .then(function(response) {
+                    return response;
+                }, function(response) {
+                    return response;
+                });
+        };
+
+        return {
+            getReleases: _getReleases
+        };
+    }
+
+    service.$inject = [
+        "$http", "scConfig"
+    ];
+
+
+    angular.module("services.platformUpdateService", [])
+        .service("platformUpdateService", service);
+
+
+})(window, window.angular);
