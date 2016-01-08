041bb430b384d697e08d5c1c84a689d1e58b6465
Greg Bielleman <greg.bielleman@particular.net>
2016-01-08 13:39:20 +1100

    Remove legacy choco package

--- src/Setup/chocolatey/chocolateyInstall.ps1
+++
@@ -1,3 +0,0 @@
-﻿$productName = "ServicePulse"
-$url = "https://github.com/Particular/$productName/releases/download/$env:chocolateyPackageVersion/Particular.$productName-$env:chocolateyPackageVersion.exe"
-Install-ChocolateyPackage $productName 'exe' '/quiet' $url
