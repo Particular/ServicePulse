80bd9068c4dd688ede0964c41fab56d8668e9152
Pete <peter.giles@particular.net>
2016-01-14 15:40:30 +1100

    Alerts User to Upgrade ServiceControl

--- src/ServicePulse.Host/app/js/services/services.platform-update.js
+++ src/ServicePulse.Host/app/js/services/services.platform-update.js
@@ -2,30 +2,39 @@
 (function(window, angular, undefined) {
     "use strict";
 
-    function service($http, scConfig) {
+    function service($http, $q) {
 
-        function _getReleases() {
-            return $http
-                .get(scConfig.service_pulse_url, {
-                     responseType: "json"
-                })
-                .then(function(response) {
-                    return response;
-                }, function(response) {
-                    return {
-                        // suppress error
-                        data: []
-                    };
+        function getReleases() {
+
+
+            var service_product_urls = [
+                { product:'SP', url: '//platformupdate.particular.net/servicepulse.txt' },
+                { product: 'SC', url: '//platformupdate.particular.net/servicecontrol.txt' }
+            ];
+
+            return $q.all(service_product_urls.map(function (item) {
+                return $http({
+                    method: 'GET',
+                    url: item.url
                 });
+            }))
+          .then(function (results) {
+              var resultObj = {};
+              results.forEach(function (val, i) {
+                  resultObj[service_product_urls[i].product] = val.data;
+              });
+              return resultObj;
+          });
+
         };
 
         return {
-            getReleases: _getReleases
+            getReleases: getReleases
         };
     }
 
     service.$inject = [
-        "$http", "scConfig"
+        "$http", "$q", "scConfig"
     ];
 
 
