49b914cae66a6e1c9f195484292721bdcb1809c0
Pete <peter.giles@particular.net>
2016-01-14 15:56:03 +1100

    swallow error

--- src/ServicePulse.Host/app/js/services/services.platform-update.js
+++ src/ServicePulse.Host/app/js/services/services.platform-update.js
@@ -8,24 +8,26 @@
 
 
             var serviceProductUrls = [
-                { product:'SP', url: '//platformupdate.particular.net/servicepulse.txt' },
+                { product: 'SP', url: '//platformupdate.particular.net/servicepulse.txt' },
                 { product: 'SC', url: '//platformupdate.particular.net/servicecontrol.txt' }
             ];
 
-            return $q.all(serviceProductUrls.map(function (item) {
-                return $http({
-                    method: 'GET',
-                    url: item.url
+            return $q.all(serviceProductUrls.map(function(item) {
+                    return $http({
+                        method: 'GET',
+                        url: item.url
+                    });
+                }))
+                .then(function(results) {
+                    var resultObj = {};
+                    results.forEach(function(val, i) {
+                        resultObj[serviceProductUrls[i].product] = val.data;
+                    });
+                    return resultObj;
+                }, function () {
+                    // swallow errors
+                    return {};
                 });
-            }))
-          .then(function (results) {
-              var resultObj = {};
-              results.forEach(function (val, i) {
-                  resultObj[serviceProductUrls[i].product] = val.data;
-              });
-              return resultObj;
-          });
-
         };
 
         return {
