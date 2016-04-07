57e16779ffcd566df50c7c64ebed89ef24195209
Pete <peter.giles@particular.net>
2016-04-07 12:16:38 +1000

    Set minimum SC version

--- src/ServicePulse.Host/app/js/services/factory.shareddata.js
+++ src/ServicePulse.Host/app/js/services/factory.shareddata.js
@@ -32,7 +32,7 @@
 
         var environment = {
             sc_version: undefined,
-            minimum_supported_sc_version: "1.12.0",
+            minimum_supported_sc_version: "1.13.0",
             is_compatible_with_sc: true,
             sp_version: spVersion
         };
