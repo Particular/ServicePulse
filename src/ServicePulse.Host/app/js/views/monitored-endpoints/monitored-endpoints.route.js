08d1c87aebc760f7bdf791f16ea8dd7fec47976c
WilliamBZA <william.brander@gmail.com>
2017-06-02 11:24:49 +0200

    Add monitored endpoints structure

---
+++ src/ServicePulse.Host/app/js/views/monitored-endpoints/monitored-endpoints.route.js
@@ -0,0 +1,21 @@
+﻿(function (window, angular, undefined) {
+    'use strict';
+
+    function routeProvider($routeProvider) {
+        $routeProvider.when('/monitored-endpoints', {
+            data: {
+                pageTitle: 'Monitored Endpoints'
+            },
+            templateUrl: 'js/views/monitored-endpoints/monitored-endpoints.html',
+            controller: 'MonitoredEndpointsCtrl',
+            controllerAs: 'vm'
+        });
+    };
+
+    routeProvider.$inject = [
+        '$routeProvider'
+    ];
+
+    angular.module('monitored-endpoints')
+        .config(routeProvider);
+} (window, window.angular));
