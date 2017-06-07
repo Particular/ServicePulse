e874b76023b3c82af8d3c135e219c096e14c0855
Michał Wójcik <wojcik.mike@gmail.com>
2017-06-07 12:43:53 +0200

    Adding empty test file to endpoint monitoring

---
+++ src/ServicePulse.Host.Tests/tests/js/views/monitored_endpoints/controller.spec.js
@@ -0,0 +1,9 @@
+﻿describe('MonitoredEndpointsCtrl', function () {
+    beforeEach(module('sc'));
+
+    var $controller;
+
+    beforeEach(inject(function (_$controller_) {
+        $controller = _$controller_;
+    }));
+});
