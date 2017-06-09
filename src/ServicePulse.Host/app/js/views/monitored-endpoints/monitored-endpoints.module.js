10e72462449c4b74267397e91364fd077f25d698
WilliamBZA <william.brander@gmail.com>
2017-06-09 15:21:45 +0200

    Change attribute to element

--- src/ServicePulse.Host/app/js/views/monitored-endpoints/monitored-endpoints.module.js
+++ src/ServicePulse.Host/app/js/views/monitored-endpoints/monitored-endpoints.module.js
@@ -5,21 +5,26 @@
         .directive("graph",
             function() {
                 return {
-                    restrict: "A",
+                    restrict: "E",
+                    scope: {
+                        data: '=plotPoints'
+                    },
+                    template: '<svg></svg>',
                     link: function link(scope, element, attrs) {
+                        var svg = element.find('svg')[0];
+
                         var heigth = 50;
                         var width = 130;
                         var margin = 2;
 
-                        var data = [10, 15, 30, 50, 20, 10, 10, 15, 0];
                         var average = 25;
 
                         var scaleY = d3.scaleLinear()
-                            .domain([d3.min(data), d3.max(data)])
-                            .range([heigth - margin, margin]); // include some margin
+                            .domain([d3.min(scope.data), d3.max(scope.data)])
+                            .range([heigth - margin, margin]);
 
                         var scaleX = d3.scaleLinear()
-                            .domain([0, data.length - 1])
+                            .domain([0, scope.data.length - 1])
                             .range([margin, width - margin]);
 
                         var area = d3.area()
@@ -37,25 +42,17 @@
                             })
                             .curve(d3.curveNatural);
 
-                        var chart = d3.select(element[0])
+                        var chart = d3.select(svg)
                             .attr("width", width)
                             .attr("height", heigth);
 
                         chart.append("path")
-                            .datum(data)
+                            .datum(scope.data)
                             .attr("d", area)
                             .attr("fill", attrs.color);
 
-                        // paint a border for the graph
-                        //chart.append("path")
-                        //    .datum(data)
-                        //    .attr("d", line)
-                        //    .attr("stroke", "red")
-                        //    .attr("stroke-width", 2)
-                        //    .attr("fill", "none");
-
                         chart.append("path")
-                            .datum(Array(data.length).fill(average))
+                            .datum(Array(scope.data.length).fill(average))
                             .attr("d", line)
                             .attr("stroke", "black");
                     }
