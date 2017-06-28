62fc184aedc21cf845e699300e1f55485b042797
Tim Bussmann <tim.bussmann@particular.net>
2017-06-28 14:25:25 +0200

    dispose subscription

--- src/ServicePulse.Host/app/js/views/monitored-endpoints/monitored-endpoints.controller.js
+++ src/ServicePulse.Host/app/js/views/monitored-endpoints/monitored-endpoints.controller.js
@@ -6,10 +6,11 @@
         monitoringService,
         toastService) {
 
+        var subscription;
         function updateUI() {
             $scope.endpoints = [];
 
-            monitoringService.endpoints.subscribe(function (endpoint) {
+            subscription = monitoringService.endpoints.subscribe(function (endpoint) {
                 var index = $scope.endpoints.findIndex(function (item) { return item.Name === endpoint.Name });
                 if (index >= 0) {
                     $scope.endpoints[index] = endpoint;
@@ -17,9 +18,13 @@
                     $scope.endpoints.push(endpoint);
                 }
             });
+
         }
 
         updateUI();
+        $scope.$on("$destroy", function handler() {
+            subscription.dispose();
+        });
     };
 
     controller.$inject = [
