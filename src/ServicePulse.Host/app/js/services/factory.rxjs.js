f9d5a7ec7ff1e2675f60e2c9cca38ed3ed4c434d
WilliamBZA <william.brander@gmail.com>
2017-07-03 13:12:08 +0200

    Rename RxJs to constant

--- src/ServicePulse.Host/app/js/services/factory.rxjs.js
+++
@@ -1,8 +0,0 @@
-; (function (window, angular, undefined) {
-    'use strict';
-
-    angular.module('sc')
-        .service('rx', function () {
-            return Rx;
-        });
-} (window, window.angular));
