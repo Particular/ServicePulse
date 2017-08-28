1511acabb8f11dcf89a639e85214c08dcf729c00
Michał Wójcik <wojcik.mike@gmail.com>
2017-08-28 10:56:55 +0200

    Adding big graph on the details page

---
+++ src/ServicePulse.Host/app/js/directives/ui.particular.largeGraph.js
@@ -0,0 +1,89 @@
+﻿(function(window, angular, undefined) {
+    'use strict';
+
+    angular.module('ui.particular.largeGraph', [])
+        .directive('largeGraph',
+            function() {
+                return {
+                    restrict: 'E',
+                    scope: {
+                        data: '=plotPoints',
+                        avg: '=plotAverage'
+                    },
+                    template: '<svg></svg>',
+                    link: function link(scope, element, attrs) {
+                        var svg = element.find('svg')[0];
+                        var heigth = 600;
+                        var graphWidth = 750;
+                        var totalWidth = 800;
+                        var margin = 20;
+                        var points = scope.data;
+                        var average = scope.avg;
+                        var max = Math.max(average * 1.5, d3.max(points));
+
+                        var scaleY = d3.scaleLinear()
+                            .domain([0, max])
+                            .range([heigth - margin, margin]);
+
+                        var scaleX = d3.scaleLinear()
+                            .domain([0, points.length - 1])
+                            .range([margin, graphWidth - margin]);
+
+                        var area = d3.area()
+                            .x(function (d, i) {
+                                return scaleX(i);
+                            })
+                            .y(function (d, i) { return scaleY(d); })
+                            .y1(function (d) { return scaleY(0); }) 
+                            .curve(d3.curveLinear);
+
+                        var line = d3.line()
+                            .x(function (d, i) {
+                                return scaleX(i);
+                            })
+                            .y(function(d, i) {
+                                return scaleY(d);
+                            })
+                            .curve(d3.curveLinear);
+
+                        var chart = d3.select(svg)
+                            .attr('width', totalWidth)
+                            .attr('height', heigth);
+
+                        chart.append('path')
+                            .datum(points)
+                            .attr('d', area)
+                            .attr('fill', attrs.color);
+
+                        chart.append('path')
+                            .datum(Array(points.length).fill(average))
+                            .attr('d', line)
+                            .attr('stroke', 'black');
+
+                        chart.append('path')
+                            .datum(Array(points.length).fill(0))
+                            .attr('d', line)
+                            .attr('stroke', 'gray');
+
+                        chart.append("text")
+                            .attr("x", graphWidth - margin + 3)
+                            .attr("y", heigth / 2 + 5)
+                            .attr("text-anchor", "start")
+                            .attr("font-size", 12)
+                            .attr("font-family", "sans-serif")
+                            .text(average.toFixed(2));
+
+                        var scaleYValue = (scaleY(average == 0 ? 1 : average) + 3) | 0;
+
+                        chart.append("text")
+                            .attr("x", 0)
+                            .attr("y", scaleYValue)
+                            .attr("text-anchor", "start")
+                            .attr("font-size", 12)
+                            .attr("font-family", "sans-serif")
+                            .text("ø");
+                    }
+                };
+            });
+
+}(window, window.angular));
