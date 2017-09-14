4dcdbbc9606d81a8cbc72abb3fbfafcad63cc5ef
WilliamBZA <william.brander@gmail.com>
2017-09-14 12:10:53 +0200

    Stop showing average indicator

--- src/ServicePulse.Host/app/js/directives/ui.particular.graph.js
+++ src/ServicePulse.Host/app/js/directives/ui.particular.graph.js
@@ -72,16 +72,6 @@
                             .attr("font-size", 12)
                             .attr("font-family", "sans-serif")
                             .text(average.toFixed(2));
-
-                        var scaleYValue = (scaleY(average == 0 ? 1 : average) + 3) | 0;
-
-                        chart.append("text")
-                            .attr("x", 0)
-                            .attr("y", scaleYValue)
-                            .attr("text-anchor", "start")
-                            .attr("font-size", 12)
-                            .attr("font-family", "sans-serif")
-                            .text("ø");
                     }
                 };
             });
