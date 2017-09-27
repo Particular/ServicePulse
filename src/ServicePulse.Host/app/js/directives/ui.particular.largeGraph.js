7bb9cb91bf79d6734bed6455800bec5a156b9c34
Michał Wójcik <wojcik.mike@gmail.com>
2017-09-27 12:20:38 +0200

    Adding second data series to big graph (#503)
    * Refactoring big graph a bit to allow for 2 data series. Changing the code a bit
    
    * Adding 2nd series of data
    
    * Fixing typo in height and changing the graphs to be a bit transparent
    
    * Adding groups for every series with class. Moving drawing of labele names to common area.
    
    * Moving hard coded labels to controller
    
    * Making the color gray again ;)
    
    * Remove the title from the graph
    
    * Adding color to y axis to correlate labels with graphs

--- src/ServicePulse.Host/app/js/directives/ui.particular.largeGraph.js
+++ src/ServicePulse.Host/app/js/directives/ui.particular.largeGraph.js
@@ -1,100 +1,139 @@
 ﻿(function(window, angular, undefined) {
     'use strict';
 
+    function drawDataSeries(dataSeries, color, yAxisAllignedLeft, scaleX, chart, height, graphWidth, margin, dates, className) {
+
+        var max = Math.max(dataSeries.average * 1.5, d3.max(dataSeries.points));
+
+        var scaleY = d3.scaleLinear()
+            .domain([0, max])
+            .range([height - margin, margin]);
+
+        var area = d3.area()
+            .x(function (d, i) {
+                return scaleX(i);
+            })
+            .y(function (d, i) { return scaleY(d); })
+            .y1(function (d) { return scaleY(0); })
+            .curve(d3.curveLinear);
+
+        var line = d3.line()
+            .x(function (d, i) {
+                return scaleX(i);
+            })
+            .y(function (d, i) {
+                return scaleY(d);
+            })
+            .curve(d3.curveLinear);
+
+        var group = chart.append('g').attr('class', className);
+
+        group.append('path')
+            .datum(dataSeries.points)
+            .attr('d', area)
+            .attr('fill', color)
+            .attr('opacity', 0.2)
+            .attr('stroke', color);
+
+        group.append('path')
+            .datum(Array(dataSeries.points.length).fill(dataSeries.average))
+            .attr('d', line)
+            .attr('stroke', '#AAAFAD')
+            .attr('stroke-width', 2);
+
+        group.selectAll('dot')
+            .data(dataSeries.points)
+            .enter().append('circle')
+            .attr('r', 3)
+            .attr('cx', function (d, i) { return scaleX(i); })
+            .attr('cy', function (d) { return scaleY(d); })
+            .append('svg:title')
+            .text(function (d, i) { return dates[i] + ' | ' + d; });
+            
+        group.append('g')
+            .attr('class', 'y axis')
+            .attr('transform', 'translate(' + (yAxisAllignedLeft ? margin : graphWidth - margin) + ', 0)')
+            .call((yAxisAllignedLeft ? d3.axisLeft(scaleY) : d3.axisRight(scaleY)));
+    }
+
     angular.module('ui.particular.largeGraph', [])
         .directive('largeGraph',
             function() {
                 return {
                     restrict: 'E',
-                    scope: {
-                        data: '=plotPoints',
-                        avg: '=plotAverage',
+                    scope: {                        
                         dates: '=xaxisPoints',
+                        firstDataSeries: '=firstDataSeries',
+                        secondDataSeries: '=secondDataSeries',
                         width: '=plotWidth',
-                        heigth: '=plotHeight'
+                        height: '=plotHeight'
                     },
                     template: '<svg></svg>',
                     link: function link(scope, element, attrs) {
-                        scope.$watch('data', function () {
+                        scope.$watch('firstDataSeries', function () {
                             d3.selectAll('large-graph svg > *').remove();
                             var svg = element.find('svg')[0];
-                            var margin = 35;
+                            var margin = 60;
 
                             var totalWidth = scope.width;
-                            var heigth = scope.heigth;
+                            var height = scope.height;
                             var graphWidth = totalWidth - (2 * margin) - 30;
-                            var points = scope.data;
-                            var average = scope.avg;
+                            var firstSeries = scope.firstDataSeries;
+                            var secondSeries = scope.secondDataSeries;
+
                             var dates = scope.dates;
-                            var max = Math.max(average * 1.5, d3.max(points));
-                            
-                            var scaleY = d3.scaleLinear()
-                                .domain([0, max])
-                                .range([heigth - margin, margin]);
 
                             var scaleX = d3.scaleLinear()
-                                .domain([0, points.length - 1])
+                                .domain([0, firstSeries.points.length - 1])
                                 .range([margin, graphWidth - margin]);
 
-                            var area = d3.area()
-                                .x(function (d, i) {
-                                    return scaleX(i);
-                                })
-                                .y(function (d, i) { return scaleY(d); })
-                                .y1(function (d) { return scaleY(0); })
-                                .curve(d3.curveLinear);
-
-                            var line = d3.line()
-                                .x(function (d, i) {
-                                    return scaleX(i);
-                                })
-                                .y(function (d, i) {
-                                    return scaleY(d);
-                                })
-                                .curve(d3.curveLinear);
-
+                            
                             var chart = d3.select(svg)
                                 .attr('width', totalWidth)
-                                .attr('height', heigth);
-
-                            chart.append('path')
-                                .datum(points)
-                                .attr('d', area)
-                                .attr('fill', attrs.color);
-
-                            chart.append('path')
-                                .datum(Array(points.length).fill(average))
-                                .attr('d', line)
-                                .attr('stroke', '#AAAFAD');
-
-                            chart.selectAll('dot')
-                                .data(points)
-                                .enter().append('circle')
-                                .attr('r', 3)
-                                .attr('cx', function (d, i) { return scaleX(i); })
-                                .attr('cy', function (d) { return scaleY(d); })
-                                .append('svg:title')
-                                .text(function (d, i) { return dates[i] + ' | ' + d; }); 
-
-                            chart.append('text')
-                                .attr('x', graphWidth - margin + 3)
-                                .attr('y', heigth / 2 + 5)
-                                .attr('text-anchor', 'start')
-                                .attr('font-size', 12)
-                                .attr('font-family', 'sans-serif')
-                                .text(average.toFixed(2));
+                                .attr('height', height);
 
                             chart.append('g')
                                 .attr('class', 'x axis')
-                                .attr('transform', 'translate(0,' + (heigth - margin) + ')')
+                                .attr('transform', 'translate(0,' + (height - margin) + ')')
                                 .call(d3.axisBottom(scaleX).tickFormat(function(d) {
                                     return dates[d];
                                 }));
-                            
-                            chart.append('g')
-                                .attr('class', 'y axis')
-                                .attr('transform', 'translate(' + margin + ', 0)')
-                                .call(d3.axisLeft(scaleY));
+
+                            drawDataSeries(firstSeries,
+                                attrs.firstSeriesColor,
+                                true,
+                                scaleX,
+                                chart,
+                                height,
+                                graphWidth,
+                                margin,
+                                dates,
+                                firstSeries.className);
+
+                            drawDataSeries(secondSeries,
+                                attrs.secondSeriesColor,
+                                false,
+                                scaleX,
+                                chart,
+                                height,
+                                graphWidth,
+                                margin,
+                                dates,
+                                secondSeries.className);
+
+                            chart.append('text')
+                                .attr('text-anchor', 'middle')
+                                .attr('stroke', attrs.firstSeriesColor)
+                                .attr('opacity', 0.5)
+                                .attr('transform', 'translate(' + (margin / 2) + ',' + (height / 2) + ')rotate(-90)')
+                                .text(firstSeries.axisName);
+
+                            chart.append('text')
+                                .attr('text-anchor', 'middle')  
+                                .attr('stroke', attrs.secondSeriesColor)
+                                .attr('opacity', 0.5)
+                                .attr('transform', 'translate(' + (graphWidth) + ',' + (height / 2) + ')rotate(90)') 
+                                .text(secondSeries.axisName);
                         });
                     }
                 };
