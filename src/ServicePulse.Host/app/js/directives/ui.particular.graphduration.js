4b21480ef586354ed0767aaf4ea43ae723691d14
WilliamBZA <WilliamBZA@users.noreply.github.com>
2017-10-13 08:09:20 +0200

    Removed double semi colon

--- src/ServicePulse.Host/app/js/directives/ui.particular.graphduration.js
+++ src/ServicePulse.Host/app/js/directives/ui.particular.graphduration.js
@@ -5,7 +5,7 @@
         .filter('graphduration', ['formatter', function (formatter) {
             return function (input) {
                 if (input) {
-                    var lastValue = input.points.length > 0 ? input.points[input.points.length - 1] : 0;;
+                    var lastValue = input.points.length > 0 ? input.points[input.points.length - 1] : 0;
                     input.displayValue = formatter.formatTime(lastValue);
                 }
 
