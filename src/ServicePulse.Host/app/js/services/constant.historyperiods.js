a5ec2432f927f8fb13ac6fcd3613cf296ce5c8ae
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-10-24 14:02:26 +0200

    changing refresh rate from 1sec to 2sec for 1 min history period

--- src/ServicePulse.Host/app/js/services/constant.historyperiods.js
+++ src/ServicePulse.Host/app/js/services/constant.historyperiods.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('sc').constant('historyPeriods', [
-        { value: 1, text: "Last 1 min.", refreshInterval: 1*1000 },
+        { value: 1, text: "Last 1 min.", refreshInterval: 2*1000 },
         { value: 5, text: "Last 5 min.", refreshInterval: 15*1000 },
         { value: 10, text: "Last 10 min.", refreshInterval: 30*1000 },
         { value: 15, text: "Last 15 min.", refreshInterval: 45*1000 },
