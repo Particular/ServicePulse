c56a0210b03ad696d2ff492ee4236113c9f5b1ce
Tomasz Masternak <tomasz.masternak@particular.net>
2017-10-30 06:33:33 +0100

    refresh reates one third smaller (#538)

--- src/ServicePulse.Host/app/js/services/constant.historyperiods.js
+++ src/ServicePulse.Host/app/js/services/constant.historyperiods.js
@@ -2,11 +2,11 @@
     'use strict';
 
     angular.module('sc').constant('historyPeriods', [
-        { value: 1, text: "Last 1 min.", refreshInterval: 2*1000 },
-        { value: 5, text: "Last 5 min.", refreshInterval: 15*1000 },
-        { value: 10, text: "Last 10 min.", refreshInterval: 30*1000 },
-        { value: 15, text: "Last 15 min.", refreshInterval: 45*1000 },
-        { value: 30, text: "Last 30 min.", refreshInterval: 90*1000 },
-        { value: 60, text: "Last hour", refreshInterval: 180*1000 }
+        { value: 1, text: "Last 1 min.", refreshInterval: 1*1000 },
+        { value: 5, text: "Last 5 min.", refreshInterval: 5*1000 },
+        { value: 10, text: "Last 10 min.", refreshInterval: 10*1000 },
+        { value: 15, text: "Last 15 min.", refreshInterval: 15*1000 },
+        { value: 30, text: "Last 30 min.", refreshInterval: 30*1000 },
+        { value: 60, text: "Last hour", refreshInterval: 60*1000 }
     ]);
 } (window, window.angular));
