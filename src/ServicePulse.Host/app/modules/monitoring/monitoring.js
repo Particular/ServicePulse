bcf5a7753c87aaa88dd7a79a6c7924af882ccb87
Michał Wójcik <wojcik.mike@gmail.com>
2017-11-20 12:33:02 +0100

    Moving graph directives into the monitoring module

--- src/ServicePulse.Host/app/modules/monitoring/monitoring.js
+++ src/ServicePulse.Host/app/modules/monitoring/monitoring.js
@@ -1,2 +1,7 @@
 ﻿require('./js/monitored_endpoints.module');
 require('./js/endpoint_details.module');
+
+require('./js/directives/ui.particular.graph.js');
+require('./js/directives/ui.particular.graphdecimal.js');
+require('./js/directives/ui.particular.graphduration.js');
+require('./js/directives/ui.particular.largeGraph.js');
