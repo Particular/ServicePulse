0292216090c7f6c10a4ccd08a51a2b9ed88aae66
Michał Wójcik <wojcik.mike@gmail.com>
2017-11-20 14:17:22 +0100

    Moving dependencies to submodules

--- src/ServicePulse.Host/app/modules/monitoring/monitoring.js
+++ src/ServicePulse.Host/app/modules/monitoring/monitoring.js
@@ -1,7 +1,2 @@
 ﻿require('./js/monitored_endpoints.module');
 require('./js/endpoint_details.module');
-
-require('./js/directives/ui.particular.graph.js');
-require('./js/directives/ui.particular.graphdecimal.js');
-require('./js/directives/ui.particular.graphduration.js');
-require('./js/directives/ui.particular.largeGraph.js');
