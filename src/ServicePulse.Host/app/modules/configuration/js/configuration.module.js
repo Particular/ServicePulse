9f8be48cf79ad8e28ed00eeb7f07161ffb91fda3
WilliamBZA <william.brander@gmail.com>
2018-12-05 14:36:29 +0200

    Bind tabs control

--- src/ServicePulse.Host/app/modules/configuration/js/configuration.module.js
+++ src/ServicePulse.Host/app/modules/configuration/js/configuration.module.js
@@ -9,15 +9,22 @@
 
     require('./directives/ui.particular.configurationTabs');
 
+<<<<<<< HEAD
     require('./license/license.module');
 
+=======
+>>>>>>> Bind tabs control
     angular.module('configuration', [
         'ui.bootstrap',
         'configuration.route',
         'configuration.controller',
         'configuration.service',
+<<<<<<< HEAD
         'configuration.tabs',
         'configuration.license'
+=======
+        'configuration.tabs'
+>>>>>>> Bind tabs control
     ]);
 
 } (window, window.angular));
