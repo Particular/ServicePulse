360b546945d3ac3ba053e1414bb6f446b54a9ce3
Mauro Servienti <mauro.servienti@gmail.com>
2019-03-19 17:53:29 +0100

    searc for urls in local storage

--- src/ServicePulse.Host/app/modules/configuration/connectionsFactory.js
+++ src/ServicePulse.Host/app/modules/configuration/connectionsFactory.js
@@ -13,25 +13,65 @@
 
     function connectionFactory() {
 
-        let qs = {};
+        let mus = null;
+        let scu = null;
 
-        let hashQs = decodeURIComponent(window.location.hash).split('?');
-        if (hashQs.length === 2) {
-            let segments = hashQs[1].split(';');
-            for (var i = 0; i < segments.length; i++) {
-                let segment = segments[i].split('=');
-                qs[segment[0]] = segment[1];
+        function extractQuerystringFromHash() {
+            let temp = {};
+            let hashQs = decodeURIComponent(window.location.hash).split('?');
+            if (hashQs.length === 2) {
+                let segments = hashQs[1].split(';');
+                for (var i = 0; i < segments.length; i++) {
+                    let segment = segments[i].split('=');
+                    temp[segment[0]] = segment[1];
+                }
             }
+
+            return temp;
         }
 
-        console.log(qs);
+        let qs = extractQuerystringFromHash();
+        console.debug(qs);
+
+        function rebuildHash() {
+            //let currentHash = decodeURIComponent(window.location.hash).split('?');
+            //if (currentHash.length === 1) {
+            //    //there isn't a qs appended to hash
+            //    let newHash = currentHash[0] + '?';
+            //    for (var i = 0; i < qs.length; i++) {
+            //        let element = qs[i];
+            //        console.debug('element', element);
+            //        newHash += element[0] + '=' + element[1] + ';';
+            //    }
+            //    let newEncodedHash = encodeURIComponent(newHash);
+            //    window.location.hash = newEncodedHash;
+            //}
+            //else {
+            //    //there is already a qs appended to the hash, need to decode, append and re-encode
+            //}
+        }
 
         this.getMonitoringUrls = function () {
 
             console.debug('getMonitoringUrls');
-            let mus = qs['mus'];
             if (mus && mus !== null) {
-                console.log('Monitoring Urls found in QS: ', mus);
+                console.debug('returning previously retrievd Monitoring Urls: ', mus);
+                return mus;
+            }
+
+            mus = qs['mus'];
+            if (mus && mus !== null) {
+                console.debug('Monitoring Urls found in QS: ', mus);
+                window.localStorage.setItem('mus', mus);
+                console.debug('Monitoring Urls local storage value aligned with QS.');
+                return mus;
+            }
+
+            mus = window.localStorage.getItem('mus');
+            if (mus && mus !== null) {
+                console.debug('Monitoring Urls found in local storage: ', mus);
+                qs['mus'] = mus;
+                rebuildHash();
                 return mus;
             }
 
@@ -42,13 +82,28 @@
         this.getServiceControlUrl = function () {
 
             console.debug('getServiceControlUrl');
-            let csu = qs['csu'];
-            if (csu && csu !== null) {
-                console.log('ServiceControl Url found in QS: ', csu);
-                return csu;
+            if (scu && scu !== null) {
+                console.debug('returning previously retrievd ServiceControl Url: ', scu);
+                return scu;
             }
 
-            console.log('returning default SC Url: ', window.defaultConfig.service_control_url);
+            scu = qs['scu'];
+            if (scu && scu !== null) {
+                console.debug('ServiceControl Url found in QS: ', scu);
+                window.localStorage.setItem('scu', scu);
+                console.debug('ServiceControl Url local storage value aligned with QS.');
+                return scu;
+            }
+
+            scu = window.localStorage.getItem('scu');
+            if (scu && scu !== null) {
+                console.debug('ServiceControl Url found in local storage: ', scu);
+                qs['scu'] = scu;
+                rebuildHash();
+                return scu;
+            }
+
+            console.debug('returning default SC Url: ', window.defaultConfig.service_control_url);
             return window.defaultConfig.service_control_url;
         };
     }
