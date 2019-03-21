f5aa34e7a8b3d2614334f4263f8628e9cd095334
mauroservienti <mauro.servienti@gmail.com>
2019-03-21 17:16:30 +0100

    WIP Initial support for connections testing

--- src/ServicePulse.Host/app/modules/configuration/js/connections/connections.controller.js
+++ src/ServicePulse.Host/app/modules/configuration/js/connections/connections.controller.js
@@ -3,21 +3,35 @@
 
     function controller(
         $scope,
+        $http,
         notifyService,
         connectionsManager) {
         var notifier = notifyService();
         var vm = this;
-       
+
+        var initialServiceControlUrl = connectionsManager.getServiceControlUrl();
+        var initialMonitoringUrl = connectionsManager.getMonitoringUrl();
+
         vm.loadingData = false;
-        vm.configuredServiceControlUrl = connectionsManager.getServiceControlUrl();
-        vm.configuredMonitoringUrl = connectionsManager.getMonitoringUrl();
+        vm.configuredServiceControlUrl = initialServiceControlUrl;
+        vm.configuredMonitoringUrl = initialMonitoringUrl;
         vm.testAndSave = function () {
+
+            if (vm.configuredServiceControlUrl !== initialServiceControlUrl) {
+                //changed -> needs validation
+            }
+
+            if (vm.configuredMonitoringUrl !== initialMonitoringUrl) {
+                //changed -> needs validation
+            }
+
             connectionsManager.updateConnections(vm.configuredServiceControlUrl, vm.configuredMonitoringUrl);
         };
     }
 
     controller.$inject = [
         '$scope',
+        '$http',
         'notifyService',
         'connectionsManager'
     ];
