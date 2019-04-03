3158ef1f02e90df29a98064de3a67d7f32e947d8
WilliamBZA <william.brander@gmail.com>
2019-04-03 13:12:56 +0200

    Change connectionsManager to class

--- src/ServicePulse.Host/app/modules/configuration/connectionsManager.js
+++ src/ServicePulse.Host/app/modules/configuration/connectionsManager.js
@@ -3,44 +3,45 @@
 (function (window, angular, $, undefined) {
     'use strict';
 
-    function connectionsManager() {
+    class connectionsManager {
 
-        let scu = null;
-        let mu = null;
+        constructor() {
+            const urlParams = new URLSearchParams(window.location.search);
 
-        var urlParams = new URLSearchParams(window.location.search);
+            if (urlParams.has('scu')) {
+                this.serviceControlUrl = urlParams.get('scu');
+                window.localStorage.setItem('scu', this.serviceControlUrl);
+                console.debug(`ServiceControl Url found in QS and stored in local storage: ${this.serviceControlUrl}`);
+            } else if (window.localStorage.getItem('scu')) {
+                this.serviceControlUrl = window.localStorage.getItem('scu');
+                console.debug(`ServiceControl Url, not in QS, found in local storage: ${this.serviceControlUrl}`);
+            } else if (window.defaultConfig && window.defaultConfig.service_control_url) {
+                this.serviceControlUrl = window.defaultConfig.service_control_url;
+                console.debug(`setting ServiceControl Url to its default value: ${window.defaultConfig.service_control_url}`);
+            } else {
+                console.warn('ServiceControl Url is not defined.');
+            }
 
-        if (urlParams.has('scu')) {
-            scu = urlParams.get('scu');
-            window.localStorage.setItem('scu', scu);
-            console.debug('ServiceControl Url found in QS and stored in local storage: ', scu);
-        } else if (window.localStorage.getItem('scu')) {
-            scu = window.localStorage.getItem('scu');
-            console.debug('ServiceControl Url, not in QS, found in local storage: ', scu);
-        } else if (window.defaultConfig && window.defaultConfig.service_control_url) {
-            scu = window.defaultConfig.service_control_url;
-            console.debug('setting ServiceControl Url to its default value: ', window.defaultConfig.service_control_url);
-        } else {
-            console.error('ServiceControl Url is not defined.');
+            if (urlParams.has('mu')) {
+                this.monitoringUrl = urlParams.get('mu');
+                window.localStorage.setItem('mu', mu);
+                console.debug(`Monitoring Url found in QS and stored in local storage: ${this.monitoringUrl}`);
+            } else if (window.localStorage.getItem('mu')) {
+                this.monitoringUrl = window.localStorage.getItem('mu');
+                console.debug(`Monitoring Url, not in QS, found in local storage: ${this.monitoringUrl}`);
+            } else if (window.defaultConfig && window.defaultConfig.monitoring_urls && window.defaultConfig.monitoring_urls.length) {
+                this.monitoringUrl = window.defaultConfig.monitoring_urls[0];
+                console.debug(`setting Monitoring Url to its default value: ${window.defaultConfig.monitoring_urls[0]}`);
+            } else {
+                console.warn('Monitoring Url is not defined.');
+            }
         }
 
-        if (urlParams.has('mu')) {
-            mu = urlParams.get('mu');
-            window.localStorage.setItem('mu', mu);
-            console.debug('Monitoring Url found in QS and stored in local storage: ', mu);
-        } else if (window.localStorage.getItem('mu')) {
-            mu = window.localStorage.getItem('mu');
-            console.debug('Monitoring Url, not in QS, found in local storage: ', mu);
-        } else if (window.defaultConfig && window.defaultConfig.monitoring_urls && window.defaultConfig.monitoring_urls.length) {
-            mu = window.defaultConfig.monitoring_urls[0];
-            console.debug('setting Monitoring Url to its default value: ', window.defaultConfig.monitoring_urls);
-        } else {
-            console.warn('Monitoring Url is not defined.');
-        }
+        getMonitoringUrl() { return this.monitoringUrl; }
+        getServiceControlUrl() { return this.serviceControlUrl; }
 
-        this.getMonitoringUrl = function () { return mu; };
-        this.getServiceControlUrl = function () { return scu; };
-        this.updateConnections = function (serviceControlUrl, monitoringUrl) {
+        updateConnections(serviceControlUrl, monitoringUrl) {
+            const urlParams = new URLSearchParams(window.location.search);
 
             if (!serviceControlUrl) {
                 throw 'ServiceControl URL is mandatory';
@@ -61,11 +62,10 @@
             let newSearch = urlParams.toString();
             console.debug('updateConnections - new query string: ', newSearch);
             window.location.search = newSearch;
-        };
+        }
     }
 
     window.connectionsManager = new connectionsManager();
     angular.module('configuration')
         .service('connectionsManager', function () { return window.connectionsManager; });
-
 }(window, window.angular, window.jQuery));
