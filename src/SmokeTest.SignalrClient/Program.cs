e529c6f60e24804417963812f0a8974e75f1a3a1
Brandon Ording <bording@gmail.com>
2019-04-05 14:08:04 -0400

    Update smoke test projects

---
+++ src/SmokeTest.SignalrClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace SmokeTest.SignalrClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start(new ProcessStartInfo("index.html") { UseShellExecute = true });
+        }
+    }
+}
