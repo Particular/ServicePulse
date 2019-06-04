cab14d124a5eb3a488bd50500c828c7e9f2ff5e7
mauroservienti <mauro.servienti@gmail.com>
2019-06-04 13:45:27 +0200

    logic to load the message to edit and manage tabs

--- src/ServicePulse.Host/app/js/views/message/editor/messageEditorModal.controller.js
+++ src/ServicePulse.Host/app/js/views/message/editor/messageEditorModal.controller.js
@@ -4,18 +4,98 @@
     function controller(
         messageId,
         $uibModalInstance,
-        $scope) {
+        $scope,
+        serviceControlService,
+        moment,
+        $filter) {
         console.debug('messageId: ', messageId);
+
+        function prettifyText(text, contentType) {
+            if (contentType === 'application/json') {
+                return $filter('json')(text);
+            }
+            if (contentType === 'text/xml') {
+                return $filter('prettyXml')(text);
+            }
+            return text;
+        }
+
+        function getContentType(headers) {
+            var header = headers.find(function (element) { return element.key === 'NServiceBus.ContentType'; });
+            return header ? header.value : null;
+        }
+
+        function isContentTypeSupported(contentType) {
+            if (contentType === 'application/json' || contentType === 'text/xml') {
+                return true;
+            }
+
+            return false;
+        }
+        
+        $scope.getContentType = getContentType;
+
+        $scope.togglePanel = function (message, panelnum) {
+            if (message.notFound || message.error)
+                return false;
+
+            if (!angular.isDefined(message.messageHeaders)) {
+                serviceControlService.getMessageHeaders(message.message_id).then(function (msg) {
+                    message.messageHeaders = msg.data[0].headers;
+                }, function () {
+                    message.headersUnavailable = "message headers unavailable";
+                });
+            }
+
+            if (angular.isDefined(message.messageHeaders) && !angular.isDefined(message.messageBody)) {
+                serviceControlService.getMessageBody(message.message_id).then(function (msg) {
+                    var bodyContentType = getContentType(message.messageHeaders);
+                    message.bodyContentType = bodyContentType;
+                    message.isContentTypeSupported = isContentTypeSupported(bodyContentType);
+                    message.messageBody = prettifyText(msg.data, bodyContentType);
+                }, function () {
+                    message.bodyUnavailable = "message body unavailable";
+                });
+            }
+            
+            message.panel = panelnum;
+            return false;
+        };
+
+        function loadMessageById (id) {
+            return serviceControlService.getFailedMessageById(id)
+                .then(function (response) {
+                    var message = response.data;
+                    $scope.message = message;
+                },
+                function (response) {
+                    if (response.status === 404) {
+                        $scope.message = { notFound: true };
+                    } else {
+                        $scope.message = { error: true };
+                    }
+                });
+        };
+
+        $scope.message = null;
 
         $scope.cancel = function() {
             $uibModalInstance.dismiss('cancel');
         };
+
+        loadMessageById(messageId)
+            .then(function () { 
+                $scope.togglePanel($scope.message, 0); 
+            });
     }
 
     controller.$inject = [
         'messageId',
         '$uibModalInstance',
         '$scope',
+        'serviceControlService',
+        'moment',
+        '$filter',
     ];
 
     angular.module("sc")
