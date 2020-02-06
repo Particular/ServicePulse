7188b671e8725ea9ed7834a5339f1ec42ace7312
aleksandr-samila <aleks.samila@particular.net>
2020-02-06 12:13:26 +0200

    Added Self Hosting Owin stuff

---
+++ src/ServicePulse.Host/Owin/OwinBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace ServicePulse.Host.Owin
+{
+    using System.Web.Http;
+
+    public class OwinBootstrapper
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //HttpConfiguration config = new HttpConfiguration();
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new {id = RouteParameter.Optional}
+            //);
+
+            ////config.Routes.
+            //app.UseWebApi(config);
+            
+        }
+    }
+
+}
