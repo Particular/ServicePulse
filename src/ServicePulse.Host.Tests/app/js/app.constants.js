2c78d17dc6558d8971b62c5e96e66ccea0cc3bcb
Bob Langley <bob.langley@particular.net>
2020-03-10 15:28:26 -0700

    Resolve directory traversal vulnerability in static file middleware

---
+++ src/ServicePulse.Host.Tests/app/js/app.constants.js
@@ -0,0 +1,7 @@
+window.defaultConfig = {
+    default_route: '/dashboard',
+    version: '1.2.0',
+    service_control_url: 'http://some.host.com:33333/api/',
+    monitoring_urls: ['http://some.host.com:33633/'],
+    showPendingRetry: true
+};
