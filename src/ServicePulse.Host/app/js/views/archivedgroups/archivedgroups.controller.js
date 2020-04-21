0fbb3aa812f13fd09cb461d15b1a8d7843c2e28d
WilliamBZA <william.brander@gmail.com>
2020-04-21 14:08:17 +0200

    Add archive groups scaffolding

---
+++ src/ServicePulse.Host/app/js/views/archivedgroups/archivedgroups.controller.js
@@ -0,0 +1,350 @@
+﻿(function (window, angular) {
+    'use strict';
+
+    function createWorkflowState(optionalStatus, optionalTotal, optionalFailed) {
+        if (optionalTotal && optionalTotal <= 1) {
+            optionalTotal = optionalTotal * 100;
+        }
+        return {
+            status: optionalStatus || 'working',
+            total: optionalTotal || 0,
+            failed: optionalFailed || false
+        };
+    }
+
+    function controller(
+        $scope,
+        $timeout,
+        $interval,
+        $location,
+        $cookies,
+        sharedDataService,
+        notifyService,
+        serviceControlService,
+        archivedMessageGroupsService,
+        toastService,
+        $routeParams) {
+
+        var vm = this;
+        var notifier = notifyService();
+
+        serviceControlService.performingDataLoadInitially = true;
+       
+        vm.loadingData = false;
+        vm.archiveGroups = [];
+        vm.availableClassifiers = [];
+        vm.selectedExceptionGroup = {};
+        vm.stats = sharedDataService.getstats();
+
+        vm.viewExceptionGroup = function (group) {
+            if (vm.isBeingArchived(group.operation_status) || vm.isBeingRetried(group)) {
+                return;
+            }
+            sharedDataService.set(group);
+            $location.path('/failed-messages/groups/' + group.id);
+        };
+
+        vm.unarchiveGroup = function (group) {
+            debugger;
+        };
+
+        var getClasses = function (stepStatus, currentStatus, statusArray) {
+            var indexOfStep = statusArray.indexOf(stepStatus);
+            var indexOfCurrent = statusArray.indexOf(currentStatus);
+            if (indexOfStep > indexOfCurrent) {
+                return 'left-to-do';
+            } else if (indexOfStep === indexOfCurrent) {
+                return 'active';
+            } else {
+                return 'completed';
+            }
+        };
+
+        var statusesForRetryOperation = ['waiting', 'preparing', 'queued', 'forwarding'];
+        vm.getClassesForRetryOperation = function(stepStatus, currentStatus) {
+            if (currentStatus === 'queued') {
+                currentStatus = 'forwarding';
+            }
+            return getClasses(stepStatus, currentStatus, statusesForRetryOperation);
+        };
+
+        var statusesForArchiveOperation = ['archivestarted', 'archiveprogressing', 'archivefinalizing', 'archivecompleted'];
+        vm.getClassesForArchiveOperation = function(stepStatus, currentStatus) {
+            return getClasses(stepStatus, currentStatus, statusesForArchiveOperation);
+        };
+
+        vm.isBeingRetried = function(group) {
+            return group.workflow_state.status !== 'none' && (group.workflow_state.status !== 'completed' || group.need_user_acknowledgement === true) && !vm.isBeingArchived(group.workflow_state.status);
+        };
+
+        vm.isBeingArchived = function (status) {
+            return status === 'archivestarted' || status === 'archiveprogressing' || status === 'archivefinalizing' || status === 'archivecompleted';
+        };
+
+        var initializeGroupState = function (group) {
+            var operationStatus = (group.operation_status ? group.operation_status.toLowerCase() : null) ||
+                'none';
+            if (operationStatus === 'preparing' && group.operation_progress === 1) {
+                operationStatus = 'queued';
+            }
+
+            group.workflow_state = createWorkflowState(operationStatus, group.operation_progress, group.operation_failed);
+
+            return group;
+        };
+
+        var getExceptionGroups = function () {
+            vm.archiveGroups = [];
+            return archivedMessageGroupsService.getArchivedGroups(vm.selectedClassification)
+                .then(function (response) {
+                    if (response.status === 304 && vm.exceptionGroups.length > 0) {
+                        return true;
+                    }
+
+                    if (response.data.length > 0) {
+
+                        // need a map in some ui state for controlling animations
+                        vm.exceptionGroups = response.data.map(initializeGroupState);
+
+                        if (vm.exceptionGroups.length !== vm.stats.number_of_exception_groups) {
+                            vm.stats.number_of_exception_groups = vm.exceptionGroups.length;
+                            notifier.notify('ExceptionGroupCountUpdated', vm.stats.number_of_exception_groups);
+                        }
+                    }
+                    return true;
+                });
+        };
+
+        var saveSelectedClassification = function(classification) {
+            $cookies.put('archived_groups_classification', classification);
+        };
+
+        var getDefaultClassification = function (classifiers) {
+            if ($routeParams.groupBy) {
+                saveSelectedClassification($routeParams.groupBy);
+                return $routeParams.groupBy;
+            }
+
+            var storedClassification = $cookies.get('archived_groups_classification');
+
+            if (typeof storedClassification === 'undefined') {
+                return classifiers[0];
+            }
+
+            return storedClassification;
+        };
+
+        vm.selectClassification = function (newClassification) {
+            vm.loadingData = true;
+            vm.selectedClassification = newClassification;
+
+            saveSelectedClassification(newClassification);
+
+            return autoGetExceptionGroups().then(function () {
+                vm.loadingData = false;
+
+                return true;
+            });
+        };
+
+        var initialLoad = function () {
+            vm.loadingData = true;
+
+            archivedMessageGroupsService.getArchivedGroupClassifiers().then(function (classifiers) {
+                vm.availableClassifiers = classifiers;
+                vm.selectedClassification = getDefaultClassification(classifiers);
+
+                autoGetExceptionGroups().then(function () {
+                    vm.loadingData = false;
+                    vm.initialLoadComplete = true;
+                    
+                    notifier.notify('InitialLoadComplete');
+
+                    return true;
+                });
+            });
+        };
+
+        vm.updateExceptionGroups = function() {
+            return serviceControlService.getExceptionGroups(vm.selectedClassification)
+                .then(function(response) {
+                    if (response.status === 304) {
+                        return true;
+                    }
+                    var exceptionGroupsToBeRemoved = vm.exceptionGroups.filter(function(item) {
+                        return !response.data.some(function(d) {
+                            return d.id === item.id;
+                        });
+                    });
+                    exceptionGroupsToBeRemoved.forEach(function(item) {
+                        vm.exceptionGroups.splice(vm.exceptionGroups.indexOf(item), 1);
+                    });
+
+                    vm.exceptionGroups.forEach(function(group) {
+                        var d = response.data.filter(function(item) {
+                            return item.id === group.id;
+                        })[0];
+
+                        for (var prop in d) {
+                            group[prop] = d[prop];
+                        }
+                        initializeGroupState(group);
+                    });
+
+                    response.data.filter(function(group) {
+                            return !vm.exceptionGroups.some(function(item) {
+                                return item.id === group.id;
+                            });
+                        })
+                        .forEach(function(group) {
+                            vm.exceptionGroups.push(group);
+                            initializeGroupState(group);
+                        });
+
+                    if (vm.exceptionGroups.length !== vm.stats.number_of_exception_groups) {
+                        vm.stats.number_of_exception_groups = vm.exceptionGroups.length;
+                        notifier.notify('ExceptionGroupCountUpdated', vm.stats.number_of_exception_groups);
+                    }
+
+                    return true;
+                });
+        };
+
+        var historicGroupsEtag;
+
+        var getHistoricGroups = function() {
+            serviceControlService.getHistoricGroups()
+                .then(function (response) {
+                    if (historicGroupsEtag === response.etag) {
+                        return true;
+                    }
+                    historicGroupsEtag = response.etag;
+                    vm.historicGroups = response.data.historic_operations;
+                });
+        };
+
+        var groupUpdatedInterval = $interval(function () {
+            getHistoricGroups();
+            vm.updateExceptionGroups();
+        }, 5000);
+
+        $scope.$on('$destroy', function () {
+            if (angular.isDefined(groupUpdatedInterval)) {
+                $interval.cancel(groupUpdatedInterval);
+                groupUpdatedInterval = undefined;
+            }
+        });
+
+        var archiveOperationEventHandler = function (data, status) {
+            var group = vm.exceptionGroups.filter(function (item) { return item.id === data.request_id });
+
+            group.forEach(function (item) {
+                item
+                    .workflow_state =
+                    createWorkflowState(status,
+                        data.progress.percentage);
+
+                item.operation_remaining_count = data.progress.messages_remaining;
+                item.operation_messages_completed_count = data.progress.number_of_messages_archived;
+                item.operation_start_time = data.start_time;
+
+                if (status === 'archivecompleted') {
+                    item.operation_completion_time = data.completion_time;
+                    item.need_user_acknowledgement = true;
+                }
+            });
+        };
+
+        var retryOperationEventHandler = function (data, status) {
+            var group = vm.exceptionGroups.filter(function (item) { return item.id === data.request_id });
+            
+            group.forEach(function (item) {
+                    if (status === 'preparing' && data.progress.percentage === 1) {
+                        status = 'queued';
+                    }
+                    item
+                        .workflow_state =
+                        createWorkflowState(status,
+                            data.progress.percentage,
+                            data.failed || false);
+
+                    item.operation_remaining_count = data.progress.messages_remaining;
+                    item.operation_start_time = data.start_time;
+
+                if (status === 'completed') {
+                    item.need_user_acknowledgement = true;
+                    item.operation_completion_time = data.completion_time;
+                }
+            });
+        };
+
+        notifier.subscribe($scope, function (event, data) {
+            archiveOperationEventHandler(data, 'archivestarted');
+        }, 'ArchiveOperationStarting');
+
+        notifier.subscribe($scope, function (event, data) {
+            archiveOperationEventHandler(data, 'archiveprogressing');
+        }, 'ArchiveOperationBatchCompleted');
+
+        notifier.subscribe($scope, function (event, data) {
+            archiveOperationEventHandler(data, 'archivefinalizing');
+        }, 'ArchiveOperationFinalizing');
+
+        notifier.subscribe($scope, function (event, data) {
+            archiveOperationEventHandler(data, 'archivecompleted');
+            getHistoricGroups();
+            
+            toastService.showInfo('Group ' + data.group_name + ' was archived succesfully.', 'Archive operation completed', true);
+            
+        }, 'ArchiveOperationCompleted');
+
+
+        notifier.subscribe($scope, function (event, data) {
+            retryOperationEventHandler(data, 'waiting');
+        }, 'RetryOperationWaiting');
+
+        notifier.subscribe($scope, function (event, data) {
+            retryOperationEventHandler(data, 'preparing');
+        }, 'RetryOperationPreparing');
+
+        notifier.subscribe($scope, function (event, data) {
+            retryOperationEventHandler(data, 'forwarding');
+        }, 'RetryOperationForwarding');
+        
+        notifier.subscribe($scope, function (event, data) {
+            retryOperationEventHandler(data, 'forwarding');
+        }, 'RetryOperationForwarded');
+
+        notifier.subscribe($scope, function (event, data) {
+            retryOperationEventHandler(data, 'completed');
+            getHistoricGroups();
+            if (data.failed) {
+                toastService.showInfo('Group ' + data.originator + ' was retried however and error have occured and not all messages were retried. Retry the remaining messages afterwards.', 'Retry operation completed', true);
+            } else {
+                toastService.showInfo('Group ' + data.originator + ' was retried succesfully.', 'Retry operation completed', true);
+            }
+        }, 'RetryOperationCompleted');
+
+        // INIT
+        initialLoad();
+        getHistoricGroups();
+    }
+
+    controller.$inject = [
+        '$scope',
+        '$timeout',
+        '$interval',
+        '$location',
+        '$cookies',
+        'sharedDataService',
+        'notifyService',
+        'serviceControlService',
+        'archivedMessageGroupsService',
+        'toastService',
+        '$routeParams'
+    ];
+
+    angular.module('sc')
+        .controller('archivedMessageGroupsController', controller);
+
+})(window, window.angular);
---
+++ src/ServicePulse.Host/app/js/views/archivedgroups/archivedgroups.service.js
@@ -0,0 +1,107 @@
+﻿(function (window, angular) {
+    'use strict';
+
+    function service(
+            $http,
+            $log,
+            $timeout,
+            $q,
+            notifyService,
+            connectionsManager,
+            uri
+        ) {
+
+        var notifier = notifyService();
+        var scu = connectionsManager.getServiceControlUrl();
+
+        function patchPromise(url, success, error, ids) {
+
+            var defer = $q.defer();
+
+            success = success || 'success';
+            error = error || 'error';
+
+            $http.patch(url, ids)
+                .then(function (response) {
+                    defer.resolve(success + ':' + response);
+                }, function (response) {
+                    defer.reject(error + ':' + response);
+                });
+
+            return defer.promise;
+        }
+
+        return {
+            getArchivedGroupClassifiers: function() {
+                var url = uri.join(scu, 'recoverability', 'classifiers');
+                return $http.get(url).then(function (response) {
+                    return response.data;
+                });
+            },
+
+            getArchivedGroups: function(classifier) {
+
+            },
+
+            getArchivedMessages: function (sort, page, direction, start, end) {
+                var url = '';
+                if (start && end) {
+                    url = uri.join(scu, 'errors?status=archived&page=' + page + '&sort=' + sort + '&direction=' + direction + '&modified=' + start + '...' + end);
+                } else {
+                    url = uri.join(scu, 'errors?status=archived&page=' + page + '&sort=' + sort + '&direction=' + direction);
+                } 
+
+                return $http.get(url).then(function (response) {
+                    debugger;
+                    return {
+                        data: response.data,
+                        total: response.headers('Total-Count')
+                    };
+                });
+
+            },
+
+            getArchivedCount: function () {
+
+                var url = uri.join(scu, 'errors?status=archived');
+
+                return $http.head(url).then(function (response) {
+                   
+                    return response.headers('Total-Count');
+                });
+            },
+
+            restoreFromArchive: function (startdate, enddate, success, error) {
+
+                var url = uri.join(scu, 'errors', startdate.format('YYYY-MM-DDTHH:mm:ss') + '...' + enddate.format('YYYY-MM-DDTHH:mm:ss'), 'unarchive');
+                return patchPromise(url, success, error);
+            },
+
+            restoreMessageFromArchive: function (id, success, error) {
+
+                var url = uri.join(scu, 'errors', 'unarchive');
+                return patchPromise(url, success, error, [id]);
+            },
+
+            restoreMessagesFromArchive: function (ids, success, error) {
+
+                var url = uri.join(scu, 'errors', 'unarchive');
+                return patchPromise(url, success, error, ids);
+            }
+        };
+    }
+
+    service.$inject = [
+        '$http',
+        '$log',
+        '$timeout',
+        '$q',
+        'notifyService',
+        'connectionsManager',
+        'uri'
+    ];
+
+    angular.module('sc')
+        .service('archivedMessageGroupsService', service);
+
+})(window, window.angular);
