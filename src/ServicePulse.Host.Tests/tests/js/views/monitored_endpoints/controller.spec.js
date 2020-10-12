f4d1baa289f07aef05c5f6734a18d884aa14b448
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-10-12 18:03:44 +0200

    Add unit tests for passing filter value via url query (#946)
    * add tests for passing filter value via url query
    
    * remove unused dependencies from controller
    
    * also remove params from DI config

--- src/ServicePulse.Host.Tests/tests/js/views/monitored_endpoints/controller.spec.js
+++ src/ServicePulse.Host.Tests/tests/js/views/monitored_endpoints/controller.spec.js
@@ -1,9 +1,42 @@
-﻿describe('monitoredEndpointsCtrl', function () {
-    beforeEach(module('sc'));
+﻿describe("monitoredEndpointsCtrl", function () {
+    beforeEach(module("sc"));
 
-    var $controller;
+    var $controller,$rootScope,$location;
 
-    beforeEach(inject(function (_$controller_) {
+    beforeEach(inject(function(_$controller_, _$rootScope_, _$location_) {
         $controller = _$controller_;
+        $rootScope = _$rootScope_;
+        $location = _$location_;
     }));
+
+    describe("when passing filter query string", function() {
+        it("should apply filter", function() {
+            var scope = $rootScope.$new();
+            $location.search().filter = "MyFilter";
+            $controller("monitoredEndpointsCtrl",
+                {
+                    $scope: scope,
+                    $location: $location
+                });
+
+            expect(scope.filter.name).toBe("MyFilter");
+        });
+    });
+
+    describe("when changing filter value", function() {
+        it("should update URL query parameter", function() {
+            var scope = $rootScope.$new();
+            $location.search().filter = "InitialValue";
+            $controller("monitoredEndpointsCtrl",
+                {
+                    $scope: scope,
+                    $location: $location
+                });
+
+            scope.filter.name = "NewValue";
+
+            scope.$apply();
+            expect($location.search().filter).toBe("NewValue");
+        });
+    });
 });
