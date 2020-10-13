ba1c048ea6dcebd87e9958c30b61310e89a9a561
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-10-13 17:48:08 +0200

    add missing semicolon

---
+++ src/ServicePulse.Host.Tests/tests/js/services/services.endpointGrouping.spec.js
@@ -0,0 +1,18 @@
+﻿//describe("endpointGroupingService", function() {
+//    beforeEach(module("sc"));
+
+//    var service;
+
+//    beforeEach(function() {
+//        service = _endpointGroupingService_;
+//    });
+
+//    it("returns no groups on empty input", function() {
+//        //var result = service.group([]);
+
+//        //expect(result).toBe([]);
+
+
+//    });
+//});
+
---
+++ src/ServicePulse.Host/app/modules/monitoring/js/services/services.endpointGrouping.js
@@ -0,0 +1,12 @@
+﻿//(function (angular) {
+//    "use strict";
+
+//    angular.module("services.endpointGrouping")
+//        .service("endpointGroupingService", Service);
+
+//    function Service() {
+//        this.group = function(endpoints) {
+//            return endpoints;
+//        };
+//    }
+//})(window.angular);
