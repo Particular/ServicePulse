ac227e22fee9520ad58a4b2ac9335527aadc7a88
Ramon Smits <ramon.smits@gmail.com>
2021-04-14 10:56:56 +0200

    Pull latest versions of images.

--- src/builddockerimages.ps1
+++ src/builddockerimages.ps1
@@ -4,5 +4,8 @@ node ./node_modules/webpack/bin/webpack.js --config app/modules/modules.webpackc
 
 cd ..
 
+docker pull  mcr.microsoft.com/windows/servercore/iis
 docker build -f .\dockerfile.iis -t particular/servicepulse.iis .
+
+docker pull ubuntu:latest
 docker build -f .\dockerfile.nginx -t particular/servicepulse.nginx .
