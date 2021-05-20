b727fa9ff101448008254180a1c967b8ff7ebf52
aleksandr-samila <aleks.samila@particular.net>
2021-05-20 14:40:00 +0300

    fix

--- src/ServicePulse.Host/app/js/views/failed_groups/controller.js
+++ src/ServicePulse.Host/app/js/views/failed_groups/controller.js
@@ -418,8 +418,6 @@
             archiveOperationEventHandler(data, "archivecompleted");
             getHistoricGroups();
 
-            toastService.showInfo("Group " + data.group_name + " was deleted succesfully.", "Delete operation completed", true);
-
         }, 'ArchiveOperationCompleted');
 
 
@@ -442,11 +440,6 @@
         notifier.subscribe($scope, function (event, data) {
             retryOperationEventHandler(data, 'completed');
             getHistoricGroups();
-            if (data.failed) {
-                toastService.showInfo("Group " + data.originator + " was retried however and error have occured and not all messages were retried. Retry the remaining messages afterwards.", "Retry operation completed", true);
-            } else {
-                toastService.showInfo("Group " + data.originator + " was retried succesfully.", "Retry operation completed", false);
-            }
         }, 'RetryOperationCompleted');
 
         // INIT
