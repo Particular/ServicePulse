99e07800baca51faf3c2f9b2824fdb70d3be025f
Tomek Masternak <tomasz.masternak@particular.net>
2021-11-22 13:43:20 +0100

    adding copy-to-clipboard for the code snippets

--- src/ServicePulse.Host/app/js/directives/ngHighlight.js
+++ src/ServicePulse.Host/app/js/directives/ngHighlight.js
@@ -1,6 +1,8 @@
 (function (window, angular, hljs) {
     'use strict';
 
+    var counter = 0; //used to generate unique identifiers for code samples
+
     function Directive() {
         return {
             restrict: 'E',
@@ -12,12 +14,31 @@
 
                 scope.$watch('text',
                     function() {
+                        
                         var codeTag = element.find('code')[0];
                         var languageCode = codeTag.parentNode.parentNode.getAttribute("lang");
                         codeTag.textContent = scope.text;
                         codeTag.className += ' language-' + languageCode;
-
                         hljs.highlightElement(codeTag);
+
+                        var nextSnippetId = 'hljs' + (counter++);
+                        var parentElement = element.find('pre')[0];
+
+                        if(parentElement.id == '') {
+                            parentElement.id = nextSnippetId;
+
+                            highlightJsBadge({
+                                contentSelector: '#' + nextSnippetId,
+                                loadDelay:0,                
+                                copyIconClass: 'fa fa-copy',
+                                checkIconClass: 'fa fa-check text-success',
+                            
+                                // hook to allow modifying the text before it's pasted
+                                onBeforeTextCopied: function(text, codeElement) {
+                                return text;   //  you can fix up the text here
+                                }
+                            });
+                        }
                     }
                 );
             }
--- src/ServicePulse.Host/app/modules/shell/shell.js
+++ src/ServicePulse.Host/app/modules/shell/shell.js
@@ -17,6 +17,7 @@ require('ui-select');
 require('expose-loader?d3!d3');
 require('rx');
 require('expose-loader?hljs!highlight.js')
+require('expose-loader?hljsbg!highlightjs-badge')
 require('bootstrap/dist/css/bootstrap.css');
 require('highlight.js/styles/stackoverflow-dark.css');
 require('angularjs-toaster/toaster.css');
