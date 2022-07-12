959015c0bb479c1b04e3a7179d53863e59fb869a
Michał Wójcik <michal.wojcik@particular.net>
2022-07-12 15:20:17 +0200

    Fixing introduced bugs

--- src/ServicePulse.Host/app/js/directives/ui.particular.flow-diagram.js
+++ src/ServicePulse.Host/app/js/directives/ui.particular.flow-diagram.js
@@ -41,7 +41,7 @@
                 "messageId": message.message_id,
                 "parentId": parentid,
                 "type": message.headers.findIndex(function(x) { return x.key === 'NServiceBus.DeliverAt'; }) > -1 ? 'Timeout message' : message.headers.find(function(x) { return x.key === 'NServiceBus.MessageIntent'; }).value === 'Publish' ? 'Event message' : 'Command message',
-                "isError": message.headers.findIndex(function(x) { x.key === 'NServiceBus.ExceptionInfo.ExceptionType'; }) > -1,
+                "isError": message.headers.findIndex(function(x) { return x.key === 'NServiceBus.ExceptionInfo.ExceptionType'; }) > -1,
                 "sagaName": saga,
                 "link" : {
                     "name" : "Link "+message.id,
@@ -167,7 +167,7 @@
                 return '<div style="width:' + 
                     (rectNode.width - rectNode.textMargin * 2) + ' px; height:' + 
                     (rectNode.height - rectNode.textMargin * 2) + ' px;" class="node-text wordwrap">' +
-                    '<i class="fa '+ (d.data.type === 'Delay' ? 'pa-flow-timeout' : d.data.type === 'Event' ? 'pa-flow-event' : 'pa-flow-command') + '" title="' + d.data.type + '"></i><div class="lead righ-side-ellipsis" title="' + d.data.nodeName + '"><strong>' + (d.data.isError ? '<a href=#/failed-messages/message/' + d.data.id + '>' + d.data.nodeName + '</a>' : d.data.nodeName) + '</strong></div>' + 
+                    '<i class="fa '+ (d.data.type === 'Timeout message' ? 'pa-flow-timeout' : d.data.type === 'Event message' ? 'pa-flow-event' : 'pa-flow-command') + '" title="' + d.data.type + '"></i><div class="lead righ-side-ellipsis" title="' + d.data.nodeName + '"><strong>' + (d.data.isError ? '<a href=#/failed-messages/message/' + d.data.id + '>' + d.data.nodeName + '</a>' : d.data.nodeName) + '</strong></div>' + 
                     '<span class="time-sent"><sp-moment date="' + d.data.timeSent + '"></sp-moment></span>' +
                     (d.data.sagaName ? '<i class="fa pa-flow-saga"></i><div class="saga lead righ-side-ellipsis" title="' + d.data.sagaName + '">' + d.data.sagaName + '</div>' : '') + 
 '</div>';
