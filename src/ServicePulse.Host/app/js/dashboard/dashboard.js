36c2f308c68889553755166f3cfbaa7b5467bbb9
williambza <william.brander@gmail.com>
2022-08-04 10:11:42 +0200

    Add css classes

--- src/ServicePulse.Host/app/js/dashboard/dashboard.js
+++ src/ServicePulse.Host/app/js/dashboard/dashboard.js
@@ -20,11 +20,28 @@ let app = createApp({
             getEventLogItems().then(data => {
                 data.forEach(event => {
                     // set date to moment date
-                    event.raised_at = moment(event.raised_at)
+                    event.raised_at = moment(event.raised_at);
                 });
 
                 this.eventLogItems = data;
             });
         }
     },
+    computer: {
+        iconClasses() {
+            return {
+                'normal': this.severity ==='info',
+                'danger': this.severity ==='error',
+                'fa-heartbeat': this.category === 'Endpoints' || this.category === 'EndpointControl' || this.category === 'HeartbeatMonitoring',
+                'fa-check': this.category === 'CustomChecks',
+                'fa-envelope': this.category === 'MessageFailures' || this.category === 'Recoverability',
+                'pa-redirect-source pa-redirect-large': this.category === 'MessageRedirects',
+                'fa-exclamation': this.category === 'ExternalIntegrations',
+                'fa-times fa-error': (this.severity === 'error' || this.category === 'MessageRedirects') && this.severity === 'error',
+                'fa-pencil': (this.severity === 'error' || this.category === 'MessageRedirects') && this.category === 'MessageRedirects' && this.event_type === 'MessageRedirectChanged',
+                'fa-plus': (this.severity === 'error' || this.category === 'MessageRedirects') && this.category === 'MessageRedirects' && this.event_type === 'MessageRedirectCreated',
+                'fa-trash': (this.severity === 'error' || this.category === 'MessageRedirects') && this.category === 'MessageRedirects' && this.event_type === 'MessageRedirectRemoved'
+              }
+        }
+    }
 }).mount('#last-ten-events');
