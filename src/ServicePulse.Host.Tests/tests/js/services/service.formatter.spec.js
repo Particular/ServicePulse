13f511e60b351c53ab0f2dab12aeac8443d58490
Christian <christian.quiros@particular.net>
2022-08-10 13:19:45 +0000

    Timespan sparkline values show 'Invalid date' when displaying HR unit values bug fix (#1266)
    * adding formatter failing test
    
    * making failing test pass
    
    * upgrading moment-duration-format

---
+++ src/ServicePulse.Host.Tests/tests/js/services/service.formatter.spec.js
@@ -0,0 +1,13 @@
+﻿describe('formatter', function () {
+    beforeEach(module('sc'));
+
+    var formatter;
+
+    beforeEach(inject( (_formatter_) => {
+        formatter = _formatter_;
+    }));
+
+    it('should format time value with hour format', () => {        
+        expect(formatter.formatTime(70825325).value).toEqual('19:40');        
+    });   
+});
