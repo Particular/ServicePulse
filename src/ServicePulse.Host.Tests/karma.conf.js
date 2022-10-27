aa5a58cc827e02ecfb296835ca7a85c2d15229d8
Michał Wójcik <michal.wojcik@particular.net>
2022-10-27 10:07:54 +0200

    Fixing tests

--- src/ServicePulse.Host.Tests/karma.conf.js
+++ src/ServicePulse.Host.Tests/karma.conf.js
@@ -1,7 +1,7 @@
 // karma.conf.js
 module.exports = function(config) {
     config.set({
-        browsers: ['PhantomJS', 'PhantomJS_custom', 'Chrome'],
+        browsers: ['Chrome'],
         basePath: '../ServicePulse.Host/app',
         preprocessors: {
             '**/*.js': ['eslint']
@@ -10,11 +10,11 @@ module.exports = function(config) {
             showWarnings: false
         },
         files: [
-            './angular/modules/dist/shell.dist.js',
+            './modules/dist/shell.dist.js',
             '../../ServicePulse.Host.Tests/tests/js/angular-mocks.js',
             './angular/**/*.html',
             './js/app.constants.js',
-            './angular/modules/dist/configuration.dist.js',
+            './modules/dist/configuration.dist.js',
             './angular/app.js',
             './angular/app.bootstrap.js',
             './angular/**/*.module.js',
@@ -23,23 +23,12 @@ module.exports = function(config) {
             './js/polyfill/**/*.js',
             './angular/services/**/*.js',
             './angular/views/**/*.js',
-            './angular/modules/dist/monitoring.dist.js',
+            './modules/dist/monitoring.dist.js',
             '../../ServicePulse.Host.Tests/tests/**/*.spec.js'
         ],
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine', 'requirejs'],
         // you can define custom flags
         customLaunchers: {
-            'PhantomJS_custom': {
-                base: 'PhantomJS',
-                options: {
-                    windowName: 'SpecsRunner.html',
-                    settings: {
-                        webSecurityEnabled: false
-                    },
-                },
-                flags: ['--load-images=true'],
-                debug: true
-            },
             'chrome_without_security': {
                 base: 'Chrome',
                 flags: ['--disable-web-security']
