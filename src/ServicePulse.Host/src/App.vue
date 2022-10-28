d2d7c89e003131922dfbe964bd738b7df974f4d1
Michał Wójcik <michal.wojcik@particular.net>
2022-10-28 13:14:27 +0200

    Adding vue template

---
+++ src/ServicePulse.Host/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Dashboard</RouterLink>
+        <RouterLink to="/configuration">Configuration</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
