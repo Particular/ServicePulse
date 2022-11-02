c25f97d0d8ed3fe85b608f69c81973cba8a66a27
Michał Wójcik <michal.wojcik@particular.net>
2022-11-02 08:59:49 +0100

    Adding DashboardItem.vue and plugging it into the main screen

---
+++ src/ServicePulse.Host/src/components/DashboardItem.vue
@@ -0,0 +1,18 @@
+﻿<script setup>
+const props = defineProps({
+  counter: Number,
+  url: String,
+  iconClass: String
+})
+</script>
+
+<template>
+  <a class="summary-item" :class="{  'summary-danger'  : counter > 0, 'summary-info' : counter === 0 || !counter }" :href="url">
+    <i class="fa fa-3x" :class="iconClass">
+    </i>
+    <span v-if="counter > 0" class="badge badge-important">{{counter}}</span>
+    <h4>
+      <slot></slot>
+    </h4>  
+  </a>
+</template>
--- src/ServicePulse.Host/src/views/DashboardView.vue
+++ src/ServicePulse.Host/src/views/DashboardView.vue
@@ -1,5 +1,36 @@
+<script setup>
+import DashboardItem from "../components/DashboardItem.vue";
+
+</script>
 <template>
-  <div class="container">
-    <h1>Dashboard</h1>
+  <div class="row">
+    <div class="col-sm-12">
+      <h1>Dashboard</h1>
+    </div>
   </div>
+
+  <div class="row">
+    <div class="col-sm-12">
+      <h6>System status</h6>
+      <div class="row box system-status">
+        <div class="col-sm-12">
+          <div class="row">
+            <div class="col-xs-4">
+              <DashboardItem :counter="10" :url="'/endpoints'" :iconClass="'fa-heartbeat'">Heartbeats</DashboardItem>
+            </div>
+            <div class="col-xs-4">
+              <DashboardItem :counter="20" :url="'/failed-messages/groups'" :iconClass="'fa-envelope'">Failed Messages</DashboardItem>
+            </div>
+            <div class="col-xs-4">
+              <DashboardItem :counter="30" :url="'/custom-checks'" :iconClass="'fa-check'">Custom Checks</DashboardItem>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+  
 </template>
