a60f071055ec687e7d3f4f91cc992f251a4bd136
Tomek Masternak <tomasz.masternak@particular.net>
2022-11-07 11:53:18 +0100

    ServicePulse.Host building js dist files

---
+++ src/ServicePulse.Host/build.ps1
@@ -0,0 +1,13 @@
+﻿cd angular
+npm run load
+cd ..
+
+cd vue
+npm run build
+cd ..
+
+New-Item -ItemType Directory -Force -Path dist
+Copy-Item -Path angular\app -Destination dist\angular -recurse -Force
+Copy-Item -Path vue\dist\* -Destination dist -recurse -Force
+
+
