d1661d05af58ae0fee100a2d7212330e29ea790c
Tomek Masternak <tomasz.masternak@particular.net>
2022-11-08 21:24:06 +0100

    app.constants.js moved to vue

--- src/ServicePulse.Host/vue/vite.config.js
+++ src/ServicePulse.Host/vue/vite.config.js
@@ -5,20 +5,35 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        {
+            name: 'app-constants-js-cache-busting',
+            transformIndexHtml(html) {
+                const cachebuster = Math.round(new Date().getTime() / 1000);
+                return html.replace(
+                    /app.constants.js/,
+                    ('app.constants.js?' + cachebuster)
+                );
+            }
+        }],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         }
     },
     build: {
-        outDir: "../dist"
+        outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            external: []
+        }
     },
     server: {
         host: true,
         fs: {
             // Allow serving files from one level up to the project root
-            allow: ['..'],
+            allow: ['..']
         }
     }
 });
