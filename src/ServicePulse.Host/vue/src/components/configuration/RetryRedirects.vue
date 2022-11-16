3a106a07e1f0b690cf5004b59f72e1286e6135dc
Jo Palac <j_palac@hotmail.com>
2022-11-16 15:39:03 +1000

    started on redirects

--- src/ServicePulse.Host/vue/src/components/configuration/RetryRedirects.vue
+++ src/ServicePulse.Host/vue/src/components/configuration/RetryRedirects.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { inject } from "vue";
+import { inject, ref, reactive, onMounted } from "vue";
 import PlatformLicenseExpired from "../PlatformLicenseExpired.vue";
 import PlatformTrialExpired from "../PlatformTrialExpired.vue";
 import PlatformProtectionExpired from "../PlatformProtectionExpired.vue";
 import ServiceControlNotAvailable from "../ServiceControlNotAvailable.vue";
-import { key_IsSCConnected, key_ScConnectedAtLeastOnce, key_IsSCConnecting, key_IsPlatformExpired, key_IsPlatformTrialExpired, key_IsInvalidDueToUpgradeProtectionExpired } from "./../../composables/keys.js"
+import NoData from "../NoData.vue"
+import Busy from "../Busy.vue"
+import { key_ServiceControlUrl, key_IsSCConnected, key_ScConnectedAtLeastOnce, key_IsSCConnecting, key_IsPlatformExpired, key_IsPlatformTrialExpired, key_IsInvalidDueToUpgradeProtectionExpired } from "./../../composables/keys.js"
+import {useRedirects, useUpdateRedirects, useCreateRedirects, useDeleteRedirects} from "../../composables/serviceRedirects.js"
 
 const isPlatformExpired = inject(key_IsPlatformExpired)
 const isPlatformTrialExpired = inject(key_IsPlatformTrialExpired)
@@ -13,6 +16,40 @@ const isInvalidDueToUpgradeProtectionExpired = inject(key_IsInvalidDueToUpgradeP
 const isSCConnected = inject(key_IsSCConnected)
 const scConnectedAtLeastOnce = inject(key_ScConnectedAtLeastOnce)
 const isSCConnecting = inject(key_IsSCConnecting)
+
+const configuredServiceControlUrl = inject(key_ServiceControlUrl)
+
+const loadingData = ref(true)
+const redirects = reactive({
+    total: 0,
+    data: []
+})
+
+function getRedirect() {
+    loadingData.value = true
+    useRedirects(configuredServiceControlUrl.value).then(result => {
+        redirects.total = result.total
+        redirects.data = result.data        
+
+        loadingData.value = false
+    }) 
+}
+
+function createRedirect() {
+    alert('create')
+}
+
+function editRedirect(redirect) {
+    alert('edit')
+}
+
+function deleteRedirect(redirect, successMsg, errorMsg) {
+    alert('delete')
+}
+
+onMounted(() => {
+    getRedirect()
+})
 
 </script>
 
@@ -28,24 +65,62 @@ const isSCConnecting = inject(key_IsSCConnecting)
 
             <template v-if="isSCConnected || scConnectedAtLeastOnce">
                 <section>
-                    <div class="box">
-                        <div class="row">
-                            <h2>RetryRedirects</h2>
-                        </div>
-                    </div>
-                    <!-- <busy v-show="vm.loadingData" message="fetching more redirects"></busy>
+                
+                    <Busy v-if="loadingData"></Busy>
 
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="btn-toolbar">
-                                <button type="button" class="btn btn-default" @click="vm.createRedirect()"><i class="fa pa-redirect-source pa-redirect-small"></i> Create Redirect</button>
+                                <button type="button" class="btn btn-default" @click="createRedirect"><i class="fa pa-redirect-source pa-redirect-small"></i> Create Redirect</button>
                                 <span></span>
                             </div>
                         </div>
                     </div>
 
-                    <no-data v-show="vm.redirects.length === 0 && !vm.loadingData" title="redirects" message="There are currently no redirects"></no-data>  -->
-                    </section>
+                    <NoData v-if="redirects.total === 0 && !loadingData" title="Redirects" message="There are currently no redirects"></NoData>
+
+                    <div class="row">
+                        <template v-if="redirects.total > 0">
+                            <div class="col-sm-12">
+                                <template v-for="redirect in redirects.sort(sourceEndpoint)" :key="redirect.redirectId">
+                                    <div class="row box repeat-modify">
+                                        <div class="row" id="{{redirect.from_physical_address}}">
+                                            <div class="col-sm-12">
+                                                <p class="lead hard-wrap truncate" uib-tooltip="{{redirect.from_physical_address}}">
+                                                    <i class="fa pa-redirect-source pa-redirect-small" uib-tooltip="Source queue name"></i>
+                                                    {{redirect.from_physical_address}}
+                                                </p>
+                                                <p class="lead hard-wrap truncate" uib-tooltip="{{redirect.to_physical_address}}">
+                                                    <i class="fa pa-redirect-destination pa-redirect-small" uib-tooltip="Destination queue name"></i>
+                                                    {{redirect.to_physical_address}}
+                                                </p>
+                                                <p class="metadata">
+                                                    <i class="fa fa-clock-o"></i>
+                                                    Last modified: {{redirect.last_modified}}
+                                                    <!-- Last modified: <sp-moment date="{{redirect.last_modified}}"></sp-moment> -->
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div isolate-click class="col-sm-12">
+                                                <p class="small">
+                                                    <button type="button" class="btn btn-link btn-sm" confirm-title="Are you sure you want to end the redirect?" @confirm-click="deleteRedirect(redirect, 'Redirect was deleted', 'Failed to delete redirect')"
+                                                            confirm-message="Once the redirect is ended, any affected messages will be sent to the original destination queue. Ensure this queue is ready to accept messages again.">
+                                                        End Redirect
+                                                    </button>
+                                                    <button type="button" class="btn btn-link btn-sm" @click="editRedirect(redirect)">
+                                                        Modify Redirect
+                                                    </button>
+                                                </p>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </template>
+                            </div>
+                        </template>
+                    </div>
+                </section>
             </template>   
         </section>
     </template> 
---
+++ src/ServicePulse.Host/vue/src/composables/serviceRedirects.js
@@ -0,0 +1,107 @@
+//import { ref } from "vue";
+import moment from 'moment';
+
+const redirects = {
+    data: [],
+    total:0
+}
+export function useRedirects(serviceControlUrl) {
+    return getRedirects(serviceControlUrl).then( ()=> {
+        if(redirects.data) (
+            redirects.data.forEach(function(item) {
+                item.last_modified = moment.utc(item.last_modified).local().format('YYYY-MM-DDTHH:mm:ss');
+            })            
+        )     
+        return redirects   
+    })
+}
+
+function getRedirects(serviceControlUrl) {
+    return fetch(serviceControlUrl + 'redirects')
+    .then(response => {
+        redirects.total = parseInt(response.headers.get('Total-Count'))
+        return response.json()
+    })
+    .then(data => {       
+        redirects.data = data
+    })
+    .catch(err => {        
+        console.log(err)
+    });
+}
+
+export function useUpdateRedirects(serviceControlUrl, redirectId, sourceEndpoint, targetEndpoint) {
+    const requestOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "id": redirectId, "fromphysicaladdress": sourceEndpoint, "tophysicaladdress": targetEndpoint },)
+      };
+      
+      return fetch(serviceControlUrl + 'redirects/' + redirectId, requestOptions)
+        .then(response => {
+            var result = {
+                message: response.ok? "success" :  "error:" + response.statusText,
+                status: response.status,
+                statusText: response.statusText,
+                data: response                
+            }    
+            return result
+        })       
+        .catch(err => {        
+            console.log(err)
+            var result = {
+                message: "error"        
+            }
+            return result        
+        });
+}
+
+export function useCreateRedirects(serviceControlUrl, sourceEndpoint, targetEndpoint) {
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "fromphysicaladdress": sourceEndpoint, "tophysicaladdress": targetEndpoint },)
+      };
+      
+      return fetch(serviceControlUrl + 'redirects', requestOptions)
+        .then(response => {
+            var result = {
+                message: response.ok? "success" :  "error:" + response.statusText,
+                status: response.status,
+                statusText: response.statusText,
+                data: response                
+            }    
+            return result
+        })       
+        .catch(err => {        
+            console.log(err)
+            var result = {
+                message: "error"        
+            }
+            return result        
+        });
+}
+
+export function useDeleteRedirects(serviceControlUrl, redirectId) {
+    const requestOptions = {
+        method: "DELETE",       
+      };
+      
+      return fetch(serviceControlUrl + 'redirects/' + redirectId, requestOptions)
+        .then(response => {
+            var result = {
+                message: response.ok? "success" :  "error:" + response.statusText,
+                status: response.status,
+                statusText: response.statusText,
+                data: response                
+            }    
+            return result
+        })       
+        .catch(err => {        
+            console.log(err)
+            var result = {
+                message: "error"        
+            }
+            return result        
+        });
+}
