6da882cb76bfcc0271f21c8f4ef1c426bc6987f1
Michał Wójcik <michal.wojcik@particular.net>
2022-11-21 15:27:29 +0100

    Fixing TimeSince, to display text and title

---
+++ src/ServicePulse.Host/vue/src/components/TimeSince.vue
@@ -0,0 +1,48 @@
+﻿<script setup>
+import { onMounted, onUnmounted, watch, ref } from "vue";
+import moment from "moment";
+
+const props = defineProps({
+  date: Date,
+  default: function () { return new Date() }
+});
+
+var minDate = "0001-01-01T00:00:00";
+var interval = null;
+var m;
+const title = ref(),
+  text = ref();
+
+function updateText() {
+  text.value = m.fromNow();
+  title.value =
+    m.format("LLLL") + " (local)\n" + m.utc().format("LLLL") + " (UTC)";
+}
+
+onMounted(() => {
+  interval = setInterval(function () {
+    updateText();
+  }, 5000);
+  if (props.date) {
+    m = moment(props.date);
+    updateText();
+  }
+});
+
+watch(props.date, async (value) => {
+  if (value && value !== minDate) {
+    m = moment(value);
+    updateText();
+  } else {
+    text.value = "unknown";
+  }
+});
+
+onUnmounted(() => clearInterval(interval));
+</script>
+
+<template>
+  <span v-bind:title="title">{{ text }}</span>
+</template>
+
+<style></style>
