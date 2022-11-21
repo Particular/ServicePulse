39f199843f30f81792cfc8cbf034a459abe782e1
Tomek Masternak <tomasz.masternak@particular.net>
2022-11-21 14:00:52 +0100

    enable eslinting in vue.js

--- src/ServicePulse.Host/vue/vite.config.js
+++ src/ServicePulse.Host/vue/vite.config.js
@@ -5,35 +5,36 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        {
-            name: 'app-constants-js-cache-busting',
-            transformIndexHtml(html) {
-                const cachebuster = Math.round(new Date().getTime() / 1000);
-                return html.replace(
-                    /app.constants.js/,
-                    ('app.constants.js?' + cachebuster)
-                );
-            }
-        }],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        }
+  plugins: [
+    vue(),
+    {
+      name: "app-constants-js-cache-busting",
+      transformIndexHtml(html) {
+        const cachebuster = Math.round(new Date().getTime() / 1000);
+        return html.replace(
+          /app.constants.js/,
+          "app.constants.js?" + cachebuster
+        );
+      },
     },
-    build: {
-        outDir: "../app",
-        emptyOutDir: true,
-        rollupOptions: {
-            external: []
-        }
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    server: {
-        host: true,
-        fs: {
-            // Allow serving files from one level up to the project root
-            allow: ['..']
-        }
-    }
+  },
+  build: {
+    outDir: "../app",
+    emptyOutDir: true,
+    rollupOptions: {
+      external: [],
+    },
+  },
+  server: {
+    host: true,
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: [".."],
+    },
+  },
 });
