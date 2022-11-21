87a57d60ac4222321d2dfbd1a8c6f55329177612
Szymon Pobiega <szymon.pobiega@gmail.com>
2022-11-21 18:01:40 +0100

    Display warning/error on the header bar

--- src/ServicePulse.Host/vue/src/components/ExclamationMark.vue
+++ src/ServicePulse.Host/vue/src/components/ExclamationMark.vue
@@ -6,6 +6,6 @@ const props = defineProps({
 
 <template>
   <template v-if="props.type">
-    <span class="fa fa-exclamation-triangle {{type}}"></span>
+    <span class="fa fa-exclamation-triangle" :class="type"></span>
   </template>
 </template>
