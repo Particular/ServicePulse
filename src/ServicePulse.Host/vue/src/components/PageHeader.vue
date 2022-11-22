575e1c459412ab39985c9a172bfeaa7eaf556f00
Jo Palac <j_palac@hotmail.com>
2022-11-22 12:21:10 +1000

    Exclamation name update and null check on license status

--- src/ServicePulse.Host/vue/src/components/PageHeader.vue
+++ src/ServicePulse.Host/vue/src/components/PageHeader.vue
@@ -1,98 +1,142 @@
 <script setup>
 import { RouterLink, useRoute } from "vue-router";
 import { inject, ref, computed } from "vue";
-import { key_License, key_UnableToConnectToServiceControl, key_UnableToConnectToMonitoring } from "./../composables/keys.js"
-import Exclamation from './Exclamation.vue'
+import {
+  key_License,
+  key_UnableToConnectToServiceControl,
+  key_UnableToConnectToMonitoring,
+} from "./../composables/keys.js";
+import ExclamationMark from "./ExclamationMark.vue";
 import { stats } from "../composables/serviceServiceControl.js";
-import { useLicenseWarningLevel } from "./../composables/serviceLicense.js"
+import { useLicenseWarningLevel } from "./../composables/serviceLicense.js";
 
 function subIsActive(input, exact) {
   const paths = Array.isArray(input) ? input : [input];
-  const route = useRoute()
-  return paths.some( path => {
+  const route = useRoute();
+  return paths.some((path) => {
     return exact ? route.path.endsWith(path) : route.path.indexOf(path) === 0; // current path starts with this path string
-  })
-} 
+  });
+}
 
-const unableToConnectToServiceControl = inject(key_UnableToConnectToServiceControl)
-const unableToConnectToMonitoring = inject(key_UnableToConnectToMonitoring)
-const license = inject(key_License)
-const expiredWarningType = ref(useLicenseWarningLevel(license.value.license_status))
+const unableToConnectToServiceControl = inject(
+  key_UnableToConnectToServiceControl
+);
+const unableToConnectToMonitoring = inject(key_UnableToConnectToMonitoring);
+const license = inject(key_License);
+const expiredWarningType = ref(
+  useLicenseWarningLevel(license.value ? license.value.license_status : "")
+);
 
-const displayWarn = computed(() => expiredWarningType.value === "warning")
-const displayDanger = computed(() => unableToConnectToServiceControl.value || unableToConnectToMonitoring.value || expiredWarningType.value === "danger")
-
+const displayWarn = computed(() => expiredWarningType.value === "warning");
+const displayDanger = computed(
+  () =>
+    unableToConnectToServiceControl.value ||
+    unableToConnectToMonitoring.value ||
+    expiredWarningType.value === "danger"
+);
 </script>
 
 <template>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="/">
-            <img alt="Service Pulse" src="@/assets/logo.svg">
-          </a>
-        </div>
-        
-        <div id="navbar" class="collapse navbar-collapse navbar-right navbar-inverse">
-          <ul class="nav navbar-nav navbar-inverse">
-            <li :class="{ active: subIsActive('/dashboard', true) }">
-              <RouterLink :to="{name:'dashboard'}">
-                <i class="fa fa-dashboard icon-white"></i>
-                  <span class="navbar-label">Dashboard</span>
-                </RouterLink>
-            </li>
-            <li :class="{ active: subIsActive('/a/#/endpoints') }">
-              <a href="/a/#/endpoints">
-                <i class="fa fa-heartbeat icon-white"></i>
-                <span class="navbar-label">Heartbeats</span>
-                <span v-if="stats.number_of_failed_heartbeats > 0" class="badge badge-important ">{{stats.number_of_failed_heartbeats}}</span>
-              </a>
-            </li>
-            <li :class="{ active: (subIsActive('/a/#/monitoring') || subIsActive('/a/#/monitoring/endpoint')) }">
-              <a href="/a/#/monitoring">
-                <i class="fa pa-monitoring icon-white"></i>
-                <span class="navbar-label">Monitoring</span>
-              </a>
-            </li>
-            <li :class="{ active: subIsActive('/a/#/failed-messages/groups') || subIsActive('/a/#/failed-messages/all') || subIsActive('/a/#/failed-messages/archived') || subIsActive('/a/#/failed-messages/pending-retries') }">            
-              <a href="/a/#/failed-messages/groups">
-                <i class="fa fa-envelope icon-white"></i>
-                <span class="navbar-label">Failed Messages</span>
-                <span v-if="stats.number_of_failed_messages > 0" class="badge badge-important ">{{stats.number_of_failed_messages}}</span>
-              </a>
-            </li>
-            <li :class="{ active: subIsActive('/a/#/custom-checks') }">
-              <a href="/a/#/custom-checks">
-                <i class="fa fa-check icon-white"></i>
-                <span class="navbar-label">Custom Checks</span>
-                <span v-if="stats.number_of_failed_checks > 0" class="badge badge-important ">{{stats.number_of_failed_checks}}</span>
-              </a>
-            </li>
-            <li :class="{ active: subIsActive('/a/#/events') }">
-              <a href="/a/#/events">
-                <i class="fa fa-list-ul icon-white"></i>
-                <span class="navbar-label">Events</span>
-              </a>
-            </li>
-            <li :class="{ active: subIsActive('/configuration') }">
-              <RouterLink :to="{name:'configuration'}" exact>
-                <i class="fa fa-cog icon-white"></i>
-                <span class="navbar-label">Configuration</span>
-                <Exclamation :type="'warning'" v-if="displayWarn" />
-                <Exclamation :type="'danger'" v-if="displayDanger" />
-              </RouterLink>                
-            </li>
-            <li>
-              <a class="btn-feedback" href="https://github.com/Particular/ServicePulse/issues/new" target="_blank">
-                <i class="fa fa-comment"></i>
-                <span class="navbar-label">Feedback</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/">
+          <img alt="Service Pulse" src="@/assets/logo.svg" />
+        </a>
       </div>
-    </nav>  
+
+      <div
+        id="navbar"
+        class="collapse navbar-collapse navbar-right navbar-inverse"
+      >
+        <ul class="nav navbar-nav navbar-inverse">
+          <li :class="{ active: subIsActive('/dashboard', true) }">
+            <RouterLink :to="{ name: 'dashboard' }">
+              <i class="fa fa-dashboard icon-white"></i>
+              <span class="navbar-label">Dashboard</span>
+            </RouterLink>
+          </li>
+          <li :class="{ active: subIsActive('/a/#/endpoints') }">
+            <a href="/a/#/endpoints">
+              <i class="fa fa-heartbeat icon-white"></i>
+              <span class="navbar-label">Heartbeats</span>
+              <span
+                v-if="stats.number_of_failed_heartbeats > 0"
+                class="badge badge-important"
+                >{{ stats.number_of_failed_heartbeats }}</span
+              >
+            </a>
+          </li>
+          <li
+            :class="{
+              active:
+                subIsActive('/a/#/monitoring') ||
+                subIsActive('/a/#/monitoring/endpoint'),
+            }"
+          >
+            <a href="/a/#/monitoring">
+              <i class="fa pa-monitoring icon-white"></i>
+              <span class="navbar-label">Monitoring</span>
+            </a>
+          </li>
+          <li
+            :class="{
+              active:
+                subIsActive('/a/#/failed-messages/groups') ||
+                subIsActive('/a/#/failed-messages/all') ||
+                subIsActive('/a/#/failed-messages/archived') ||
+                subIsActive('/a/#/failed-messages/pending-retries'),
+            }"
+          >
+            <a href="/a/#/failed-messages/groups">
+              <i class="fa fa-envelope icon-white"></i>
+              <span class="navbar-label">Failed Messages</span>
+              <span
+                v-if="stats.number_of_failed_messages > 0"
+                class="badge badge-important"
+                >{{ stats.number_of_failed_messages }}</span
+              >
+            </a>
+          </li>
+          <li :class="{ active: subIsActive('/a/#/custom-checks') }">
+            <a href="/a/#/custom-checks">
+              <i class="fa fa-check icon-white"></i>
+              <span class="navbar-label">Custom Checks</span>
+              <span
+                v-if="stats.number_of_failed_checks > 0"
+                class="badge badge-important"
+                >{{ stats.number_of_failed_checks }}</span
+              >
+            </a>
+          </li>
+          <li :class="{ active: subIsActive('/a/#/events') }">
+            <a href="/a/#/events">
+              <i class="fa fa-list-ul icon-white"></i>
+              <span class="navbar-label">Events</span>
+            </a>
+          </li>
+          <li :class="{ active: subIsActive('/configuration') }">
+            <RouterLink :to="{ name: 'configuration' }" exact>
+              <i class="fa fa-cog icon-white"></i>
+              <span class="navbar-label">Configuration</span>
+              <exclamation-mark :type="'warning'" v-if="displayWarn" />
+              <exclamation-mark :type="'danger'" v-if="displayDanger" />
+            </RouterLink>
+          </li>
+          <li>
+            <a
+              class="btn-feedback"
+              href="https://github.com/Particular/ServicePulse/issues/new"
+              target="_blank"
+            >
+              <i class="fa fa-comment"></i>
+              <span class="navbar-label">Feedback</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
-<style>
-</style>
+<style></style>
