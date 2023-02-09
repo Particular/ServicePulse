a0e9fa18630d159db2cd0042bdae187bddf52c87
Travis Nickels <travis.nickels@particular.net>
2023-02-08 21:47:35 -0800

    Updated license for failed message group

--- src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
+++ src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
@@ -1,18 +1,24 @@
-<script setup></script>
+<script setup>
+import { licenseStatus } from "../../composables/serviceLicense.js";
+import LicenseExpired from "../../components/LicenseExpired.vue";
+</script>
 
-<template v-if="!isExpired">
-  <section name="message_groups">
-    <div class="box">
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="list-section">
-            <h3>Failed Message group</h3>
-            <div class="col-sm-7 form-group">
-              <label for="serviceControlUrl"> message group detail </label>
+<template>
+  <LicenseExpired />
+  <template v-if="!licenseStatus.isExpired">
+    <section name="message_groups">
+      <div class="box">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="list-section">
+              <h3>Failed Message group</h3>
+              <div class="col-sm-7 form-group">
+                <label for="serviceControlUrl"> message group detail </label>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </template>
 </template>
