d767c7ff3f85647a8f539c733d7a2234f1cedf83
Travis Nickels <travis.nickels@particular.net>
2023-02-08 21:31:41 -0800

    Added FailedMessageGroup and updated FailedMessageView

---
+++ src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template v-if="!isExpired">
+  <section name="message_groups">
+    <div class="box">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="list-section">
+            <h3>Failed Message group</h3>
+            <div class="col-sm-7 form-group">
+              <label for="serviceControlUrl"> message group detail </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/ServicePulse.Host/vue/src/views/FailedMessagesView.vue
+++ src/ServicePulse.Host/vue/src/views/FailedMessagesView.vue
@@ -1,16 +1,93 @@
 <script setup>
+import { ref, computed, watch, onMounted } from "vue";
+import FailedMessageGroups from "../components/failedmessages/FailedMessageGroups.vue";
 import LicenseExpired from "../components/LicenseExpired.vue";
-import ServiceControlNotAvailable from "../components/ServiceControlNotAvailable.vue";
+/* import ServiceControlNotAvailable from "../components/ServiceControlNotAvailable.vue"; */
 import { licenseStatus } from "./../composables/serviceLicense.js";
+import { connectionState } from "../composables/serviceServiceControl";
+
+const routes = {
+  "failed-message-groups": {
+    component: FailedMessageGroups,
+    title: "Failed Message Groups",
+  },
+};
+const currentPath = ref(window.location.hash);
+const redirectCount = ref(0);
+function updateRedirectCount(newCount) {
+  redirectCount.value = newCount;
+}
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
+function subIsActive(subPath) {
+  return currentPath.value === subPath;
+}
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || "/"]
+    ? routes[currentPath.value.slice(1) || "/"].component
+    : FailedMessageGroups;
+});
+const currentEvents = ref({});
+watch(currentPath, async (newValue) => {
+  setupEvents(newValue);
+  if (routes[currentPath.value.slice(1) || "/"]) {
+    document.title =
+      routes[currentPath.value.slice(1) || "/"].title +
+      " - Configuration � ServicePulse";
+  }
+});
+function setupEvents(newPath) {
+  if (newPath === "#retry-redirects") {
+    currentEvents.value = { redirectCountUpdated: updateRedirectCount };
+  } else {
+    currentEvents.value = {};
+  }
+}
+onMounted(() => {
+  const path = currentPath.value.slice(1) || "/";
+  if (
+    path === "/" &&
+    !connectionState.connected &&
+    !connectionState.connectedRecently
+  ) {
+    window.location.hash = "connections";
+  }
+  setupEvents(currentPath.value);
+});
 </script>
 
 <template>
-    <LicenseExpired />
-    <template v-if="!licenseStatus.isExpired">
-      <div class="container">
-        <ServiceControlNotAvailable />
-  
-        Failed Messages Vue
+  <LicenseExpired />
+  <template v-if="!licenseStatus.isExpired">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1>Failed Messages</h1>
+        </div>
       </div>
-    </template>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="tabs">
+            <!--Failed Message Groups-->
+            <h5
+              :class="{
+                active:
+                  subIsActive('#failed-message-groups') || subIsActive(''),
+                disabled:
+                  !connectionState.connected &&
+                  !connectionState.connectedRecently,
+              }"
+            >
+              <a href="#failed-message-groups">Failed Message Groups</a>
+              <exclamation-mark :type="licenseStatus.warningLevel" />
+            </h5>
+          </div>
+        </div>
+      </div>
+      <component :is="currentView" v-on="currentEvents" />
+    </div>
   </template>
+</template>
+
+<style></style>
