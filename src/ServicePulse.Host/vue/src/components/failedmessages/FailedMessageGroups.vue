543ae452d60d79d67ab7a83dcee4b6ded19a539a
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-02-09 05:07:58 -0800

    added check for connection status

--- src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
+++ src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
@@ -1,24 +1,29 @@
 <script setup>
 import { licenseStatus } from "../../composables/serviceLicense.js";
 import LicenseExpired from "../../components/LicenseExpired.vue";
+import { connectionState } from "../../composables/serviceServiceControl.js";
+import ServiceControlNotAvailable from "../ServiceControlNotAvailable.vue";
 </script>
 
 <template>
-  <LicenseExpired />
-  <template v-if="!licenseStatus.isExpired">
-    <section name="message_groups">
-      <div class="box">
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="list-section">
-              <h3>Failed message group</h3>
-              <div class="col-sm-7 form-group">
-                <label for="serviceControlUrl"> message group detail </label>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </template>
+    <LicenseExpired />
+    <template v-if="!licenseStatus.isExpired">
+        <ServiceControlNotAvailable />
+        <template v-if="!connectionState.unableToConnect">
+            <section name="message_groups">
+                <div class="box">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="list-section">
+                                <h3>Failed message group</h3>
+                                <div class="col-sm-7 form-group">
+                                    <label for="serviceControlUrl"> message group detail </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </template>
+    </template>
 </template>
