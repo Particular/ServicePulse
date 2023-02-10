0f4971431e62a3b990ade54432262c3ae47b7c45
Travis Nickels <travis.nickels@particular.net>
2023-02-09 19:28:45 -0800

    Formatting

--- src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
+++ src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
@@ -7,27 +7,25 @@ import LastTenOperations from "../failedmessages/LastTenOperations.vue";
 </script>
 
 <template>
-    <LicenseExpired />
-    <template v-if="!licenseStatus.isExpired">
-        <ServiceControlNotAvailable />
-        <template v-if="!connectionState.unableToConnect">
-            <section name="message_groups">
-
-                <LastTenOperations></LastTenOperations>
-                <h3>Failed message group</h3>
-                <div class="box">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="list-section">
-
-                                <div class="col-sm-7 form-group">
-                                    <label for="serviceControlUrl"> message group detail </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <LicenseExpired />
+  <template v-if="!licenseStatus.isExpired">
+    <ServiceControlNotAvailable />
+    <template v-if="!connectionState.unableToConnect">
+      <section name="message_groups">
+        <LastTenOperations></LastTenOperations>
+        <h3>Failed message group</h3>
+        <div class="box">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="list-section">
+                <div class="col-sm-7 form-group">
+                  <label for="serviceControlUrl"> message group detail </label>
                 </div>
-            </section>
-        </template>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </template>
+  </template>
 </template>
