7cf869854dbdc863de9513ac446f298ca8894e8c
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-02-11 07:33:00 -0800

    Display the group messages - work in progress

--- src/ServicePulse.Host/vue/src/components/failedmessages/DeletedMessageGroups.vue
+++ src/ServicePulse.Host/vue/src/components/failedmessages/DeletedMessageGroups.vue
@@ -3,29 +3,28 @@ import { licenseStatus } from "../../composables/serviceLicense.js";
 import LicenseExpired from "../../components/LicenseExpired.vue";
 import { connectionState } from "../../composables/serviceServiceControl.js";
 import ServiceControlNotAvailable from "../ServiceControlNotAvailable.vue";
+import MessageGroupList from "../failedmessages/MessageGroupList.vue";
 </script>
 
 <template>
-    <LicenseExpired />
-    <template v-if="!licenseStatus.isExpired">
-        <ServiceControlNotAvailable />
-        <template v-if="!connectionState.unableToConnect">
-            <section name="message_groups">
-                <div class="box">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="list-section">
-                                <h3>Deleted message groups</h3>
-                                <div class="col-sm-7 form-group">
-                                    <label for="serviceControlUrl">
-                                        deleted message groups details
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <LicenseExpired />
+  <template v-if="!licenseStatus.isExpired">
+    <ServiceControlNotAvailable />
+    <template v-if="!connectionState.unableToConnect">
+      <section name="message_groups">
+        <div class="box">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="list-section">
+                <h3>Deleted message groups</h3>
+                <div class="col-sm-7 form-group">
+                  <MessageGroupList></MessageGroupList>
                 </div>
-            </section>
-        </template>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </template>
+  </template>
 </template>
--- src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
+++ src/ServicePulse.Host/vue/src/components/failedmessages/FailedMessageGroups.vue
@@ -4,30 +4,29 @@ import LicenseExpired from "../../components/LicenseExpired.vue";
 import { connectionState } from "../../composables/serviceServiceControl.js";
 import ServiceControlNotAvailable from "../ServiceControlNotAvailable.vue";
 import LastTenOperations from "../failedmessages/LastTenOperations.vue";
+import MessageGroupList from "../failedmessages/MessageGroupList.vue";
 </script>
 
 <template>
-    <LicenseExpired />
-    <template v-if="!licenseStatus.isExpired">
-        <ServiceControlNotAvailable />
-        <template v-if="!connectionState.unableToConnect">
-            <section name="message_groups">
-
-                <LastTenOperations></LastTenOperations>
-                <h3>Failed message group</h3>
-                <div class="box">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="list-section">
-
-                                <div class="col-sm-7 form-group">
-                                    <label for="serviceControlUrl"> message group detail </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <LicenseExpired />
+  <template v-if="!licenseStatus.isExpired">
+    <ServiceControlNotAvailable />
+    <template v-if="!connectionState.unableToConnect">
+      <section name="message_groups">
+        <LastTenOperations></LastTenOperations>
+        <h3>Failed message group</h3>
+        <div class="box">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="list-section">
+                <div class="col-sm-12 form-group">
+                  <MessageGroupList></MessageGroupList>
                 </div>
-            </section>
-        </template>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </template>
+  </template>
 </template>
