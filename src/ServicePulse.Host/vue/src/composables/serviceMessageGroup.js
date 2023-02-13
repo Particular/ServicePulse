a991231e703ed8722cdc3cb8a232838b836e7db9
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-02-12 22:13:06 -0800

    typo and spaces fix

--- src/ServicePulse.Host/vue/src/composables/serviceMessageGroup.js
+++ src/ServicePulse.Host/vue/src/composables/serviceMessageGroup.js
@@ -4,47 +4,27 @@ import {
   useFetchFromServiceControl,
 } from "./serviceServiceControlUrls.js";
 
-
 export function useGetExceptionGroups() {
-    //todo - add sort, page and classifers
-    return useFetchFromServiceControl("recoverability/groups")
-        .then((response) => {
-            return response.json();
-            //var result = {
-            //    message: response.ok ? "success" : "error:" + response.statusText,
-            //    status: response.status,
-            //    statusText: response.statusText,
-            //    data: response.json(),
-            //};
-            //return response.json();
-        })
-        .catch((err) => {
-            var result = {
-                message: "error",
-            };
-            return result;
-        });
+  //todo - add sort, page and classifers
+  return useFetchFromServiceControl("recoverability/groups")
+    .then((response) => {
+      return response.json();
+      //var result = {
+      //    message: response.ok ? "success" : "error:" + response.statusText,
+      //    status: response.status,
+      //    statusText: response.statusText,
+      //    data: response.json(),
+      //};
+      //return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+      var result = {
+        message: "error",
+      };
+      return result;
+    });
 }
-
-
-
-//get Exception groups count
-function getExceptionGroupsCount(groupId, sortBy, page) {
-    var url =
-        "/recoverability/groups" +
-        groupId +
-        "errors?page=" +
-        page +
-        "&sort=" +
-        sortBy +
-        "&status=unresolved";
-    return fetchWithErrorHandling(
-        () => useFetchFromServiceControl(url),
-        connectionState,
-        (response) => parseInt(response.headers.get("Total-Count"))
-    );
-}
-
 
 //delete note by group id
 export function useDeleteNote(groupId) {
