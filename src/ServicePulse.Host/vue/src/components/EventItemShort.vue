9bc24e071e8de66e95fc7094fd11eb0b87b66246
williambza <william.brander@gmail.com>
2023-02-17 10:31:57 +0200

    Update dashboard to bootstrap 5

--- src/ServicePulse.Host/vue/src/components/EventItemShort.vue
+++ src/ServicePulse.Host/vue/src/components/EventItemShort.vue
@@ -98,42 +98,28 @@ function navigateToEvent(eventLogItem) {
 
 <template>
   <div class="row events">
-    <div class="col-sm-12">
+    <div class="col-12">
       <h6>Last 10 events</h6>
 
-      <div
-        class="row box box-event-item"
-        v-for="eventLogItem in eventLogItems"
-        :key="eventLogItem.id"
-      >
-        <div class="col-xs-12" @click="navigateToEvent(eventLogItem)">
+      <div class="row box box-event-item" v-for="eventLogItem in eventLogItems" :key="eventLogItem.id">
+        <div class="col-12" @click="navigateToEvent(eventLogItem)">
           <div class="row">
-            <div class="col-xs-1">
+            <div class="col-1">
               <span class="fa-stack fa-lg">
-                <i
-                  class="fa fa-stack-2x"
-                  :class="iconClasses(eventLogItem)"
-                ></i>
-                <i
-                  v-if="
-                    eventLogItem.severity === 'error' ||
-                    eventLogItem.category === 'MessageRedirects'
-                  "
-                  class="fa fa-o fa-stack-1x fa-inverse"
-                  :class="iconSubClasses(eventLogItem)"
-                ></i>
+                <i class="fa fa-stack-2x" :class="iconClasses(eventLogItem)"></i>
+                <i v-if="eventLogItem.severity === 'error' || eventLogItem.category === 'MessageRedirects'" class="fa fa-o fa-stack-1x fa-inverse" :class="iconSubClasses(eventLogItem)"></i>
               </span>
             </div>
 
-            <div class="col-xs-9">
+            <div class="col-9">
               <div class="row box-header">
-                <div class="col-sm-12">
+                <div class="col-12">
                   <p class="lead">{{ eventLogItem.description }}</p>
                 </div>
               </div>
             </div>
 
-            <div class="col-xs-2">
+            <div class="col-2">
               <div>
                 <time-since :dateUtc="eventLogItem.raised_at"></time-since>
               </div>
@@ -143,13 +129,91 @@ function navigateToEvent(eventLogItem) {
       </div>
 
       <div class="row text-center">
-        <a
-          v-if="eventCount > 10"
-          class="btn btn-default btn-secondary btn-all-events"
-          href="/events"
-          >View all events</a
-        >
+        <div class="col-12">
+          <a v-if="eventCount > 10" class="btn btn-default btn-secondary btn-all-events" href="/events">View all events</a>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.row.box.box-event-item {
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
+
+.box-event-item .fa-stack {
+  height: 1em;
+}
+
+.row.box.box-event-item,
+.row.box.box-event-item .col-12 {
+  padding-top: 8px;
+  padding-bottom: 2px;
+  width: 100%;
+}
+
+.events {
+  margin-top: 30px;
+}
+
+.events .box {
+  padding-bottom: 0;
+}
+
+.events .box:hover {
+  cursor: pointer;
+  background-color: #edf6f7;
+  border: 1px solid #00a3c4;
+}
+
+.events p.lead {
+  padding-bottom: 10px;
+}
+
+.box-warning {
+  border-left-color: #aa6708;
+}
+
+.box-danger {
+  border-left-color: #ce4844;
+}
+
+.box-header {
+  padding-bottom: 3px;
+  padding-top: 2px;
+}
+
+.box-header ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.box {
+  box-shadow: none;
+  margin: 0 !important;
+  padding-bottom: 10px;
+}
+
+.fa-stack-2x {
+  font-size: 24px !important;
+}
+
+.lead {
+  word-wrap: break-word;
+  color: #181919 !important;
+  font-size: 14px !important;
+  font-weight: bold !important;
+  margin-bottom: 3px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.btn.btn-all-events {
+  width: 180px;
+  margin-top: 30px;
+}
+</style>
