93169f3ca720faa796208c17f0a9106d3fa85a70
williambza <william.brander@gmail.com>
2023-02-17 11:35:53 +0200

    Style the connections tab

--- src/ServicePulse.Host/vue/src/components/configuration/PlatformConnections.vue
+++ src/ServicePulse.Host/vue/src/components/configuration/PlatformConnections.vue
@@ -73,133 +73,76 @@ function saveConnections(event) {
     <section name="connections">
       <div class="box">
         <div class="row">
-          <div class="col-sm-12">
+          <div class="col-12">
             <form novalidate>
               <div class="row connection">
                 <h3>ServiceControl</h3>
-                <div class="col-sm-7 form-group">
-                  <label for="serviceControlUrl"
-                    >CONNECTION URL
+                <div class="col-7 form-group">
+                  <label for="serviceControlUrl">CONNECTION URL
                     <template v-if="connectionState.unableToConnect">
                       <span class="failed-validation">
-                        <i class="fa fa-exclamation-triangle"></i> Unable to
-                        connect</span
-                      >
+                        <i class="fa fa-exclamation-triangle"></i> Unable to connect
+                      </span>
                     </template>
                   </label>
-                  <input
-                    type="text"
-                    id="serviceControlUrl"
-                    name="serviceControlUrl"
-                    v-model="serviceControlUrl"
-                    class="form-control"
-                    style="color: #000"
-                    required
-                  />
+                  <input type="text" id="serviceControlUrl" name="serviceControlUrl" v-model="serviceControlUrl" class="form-control" style="color: #000" required />
                 </div>
 
-                <div class="col-sm-5 no-side-padding">
-                  <button
-                    class="btn btn-default btn-secondary btn-connection-test"
-                    :class="{ disabled: !configuredServiceControlUrl }"
-                    type="button"
-                    @click="testServiceControlUrl"
-                  >
+                <div class="col-5 no-side-padding">
+                  <button class="btn btn-default btn-secondary btn-connection-test" :class="{ disabled: !configuredServiceControlUrl }" type="button" @click="testServiceControlUrl">
                     Test
                   </button>
-                  <span
-                    class="connection-test connection-testing"
-                    v-if="testingServiceControl"
-                    ><i class="glyphicon glyphicon-refresh rotate"></i>
-                    Testing</span
-                  >
-                  <span
-                    class="connection-test connection-successful"
-                    v-if="
-                      serviceControlValid === true && !testingServiceControl
-                    "
-                    ><i class="fa fa-check"></i> Connection successful</span
-                  >
-                  <span
-                    class="connection-test connection-failed"
-                    v-if="
-                      serviceControlValid === false && !testingServiceControl
-                    "
-                    ><i class="fa fa-exclamation-triangle"></i> Connection
-                    failed</span
-                  >
+                  <span class="connection-test connection-testing" v-if="testingServiceControl">
+                    <i class="glyphicon glyphicon-refresh rotate"></i>Testing
+                  </span>
+                  <span class="connection-test connection-successful" v-if="serviceControlValid === true && !testingServiceControl">
+                    <i class="fa fa-check"></i> Connection successful
+                  </span>
+                  <span class="connection-test connection-failed" v-if="serviceControlValid === false && !testingServiceControl">
+                    <i class="fa fa-exclamation-triangle"></i> Connection failed
+                  </span>
                 </div>
               </div>
 
               <div class="row connection">
                 <h3>ServiceControl Monitoring</h3>
-                <div class="col-sm-7 form-group">
-                  <label for="monitoringUrl"
-                    >CONNECTION URL
+                <div class="col-7 form-group">
+                  <label for="monitoringUrl">CONNECTION URL
                     <span class="auxilliary-label">(OPTIONAL)</span>
                     <template v-if="monitoringConnectionState.unableToConnect">
                       <span class="failed-validation">
-                        <i class="fa fa-exclamation-triangle"></i> Unable to
-                        connect
+                        <i class="fa fa-exclamation-triangle"></i> Unable to connect
                       </span>
                     </template>
                   </label>
-                  <input
-                    type="text"
-                    id="monitoringUrl"
-                    name="monitoringUrl"
-                    v-model="monitoringUrl"
-                    class="form-control"
-                    required
-                  />
+                  <input type="text" id="monitoringUrl" name="monitoringUrl" v-model="monitoringUrl" class="form-control" required />
                 </div>
 
-                <div class="col-sm-5 no-side-padding">
-                  <button
-                    class="btn btn-default btn-secondary btn-connection-test"
-                    :class="{ disabled: !configuredMonitoringUrl }"
-                    type="button"
-                    @click="testMonitoringUrl"
-                  >
+                <div class="col-5 no-side-padding">
+                  <button class="btn btn-default btn-secondary btn-connection-test" :class="{ disabled: !configuredMonitoringUrl }" type="button" @click="testMonitoringUrl">
                     Test
                   </button>
-                  <span
-                    class="connection-test connection-testing"
-                    v-if="testingMonitoring"
-                    ><i class="glyphicon glyphicon-refresh rotate"></i>
-                    Testing</span
-                  >
-                  <span
-                    class="connection-test connection-successful"
-                    v-if="monitoringValid === true && !testingMonitoring"
-                    ><i class="fa fa-check"></i> Connection successful</span
-                  >
-                  <span
-                    class="connection-test connection-failed"
-                    v-if="monitoringValid === false && !testingMonitoring"
-                    ><i class="fa fa-exclamation-triangle"></i> Connection
-                    failed</span
-                  >
+                  <span class="connection-test connection-testing" v-if="testingMonitoring">
+                    <i class="glyphicon glyphicon-refresh rotate"></i>Testing
+                  </span>
+                  <span class="connection-test connection-successful" v-if="monitoringValid === true && !testingMonitoring">
+                    <i class="fa fa-check"></i> Connection successful
+                  </span>
+                  <span class="connection-test connection-failed" v-if="monitoringValid === false && !testingMonitoring">
+                    <i class="fa fa-exclamation-triangle"></i> Connection failed
+                  </span>
                 </div>
               </div>
 
-              <button
-                class="btn btn-primary"
-                type="button"
-                @click="saveConnections"
-              >
+              <button class="btn btn-primary" type="button" @click="saveConnections">
                 Save
               </button>
-              <span
-                class="connection-test connection-successful hide"
-                v-show="connectionSaved"
-                ><i class="fa fa-check"></i>Connection saved</span
-              >
-              <span
-                class="connection-test connection-failed hide"
-                v-show="!connectionSaved"
-                ><i class="fa fa-exclamation-triangle"></i> Unable to save</span
-              >
+              <span class="connection-test connection-successful hide save-connection" v-show="connectionSaved">
+                <i class="fa fa-check"></i>Connection saved
+              </span>
+              <span class="connection-test connection-failed hide save-connection" v-show="connectionSaved !== null && !connectionSaved">
+                <i class="fa fa-exclamation-triangle"></i> Unable to save
+              </span>
             </form>
           </div>
         </div>
@@ -207,3 +150,32 @@ function saveConnections(event) {
     </section>
   </template>
 </template>
+
+<style>
+form .connection h3 {
+  margin-bottom: 16px;
+  margin-top: 25px;
+  padding-left: 0;
+}
+
+form .connection .form-group {
+  padding-left: 0;
+}
+
+.connection:nth-child(2) h3 {
+  margin-top: 40px;
+}
+
+form .connection .form-group input {
+  font-size: 16px;
+  height: 44px;
+}
+
+.row.connection {
+  margin-left: 0px;
+}
+
+span.connection-test.save-connection {
+  top:0px;
+}
+</style>
