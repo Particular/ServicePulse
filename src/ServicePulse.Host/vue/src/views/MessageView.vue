6999704dce2e8c130a4933b10c77c2314194d528
Travis Nickels <travis.nickels@particular.net>
2023-02-17 09:22:06 -0800

    Capture and display error some message details

--- src/ServicePulse.Host/vue/src/views/MessageView.vue
+++ src/ServicePulse.Host/vue/src/views/MessageView.vue
@@ -1,32 +1,37 @@
 <script setup>
-import { ref, computed, watch, onMounted } from "vue";
+import { ref, computed, watch, onMounted, onBeforeMount } from "vue";
 import { useRoute } from "vue-router";
 import { connectionState } from "../composables/serviceServiceControl";
-import stackTrace from "../components/message/StackTrace.vue";
 import { useFetchFromServiceControl } from "../composables/serviceServiceControlUrls";
+import stackTrace from "../components/message/StackTrace.vue";
+import NoData from "../components/NoData.vue";
+import TimeSince from "../components/TimeSince.vue";
 
 const route = useRoute();
 const id = route.params.id;
-
-const routes = [
-  {
-    path: "/failed-messages/message/:id",
-  },
-  {
-    "stack-trace": { component: stackTrace, title: "Stack Trace" },
-  },
-];
-
 const messageErrorDetails = ref([]);
+const fetchError = ref({
+    error: false,
+});
+
+const routes = {
+    path: "/failed-messages/message/:id",
+    "stack-trace": { component: stackTrace, title: "Stack Trace" },
+};
 
 function getMessageErrorDetails() {
   return useFetchFromServiceControl("errors/last/" + id)
     .then((response) => {
-      return response.json();
+        return response.json();
     })
     .then((data) => {
-      messageErrorDetails.value = [];
-      messageErrorDetails.value = data;
+        messageErrorDetails.value = [];
+        messageErrorDetails.value = data;
+    })
+    .catch((err) => {
+        console.log(err);
+        fetchError.value.error = true;
+        return {};
     });
 }
 
@@ -47,80 +52,96 @@ watch(currentPath, async () => {
   if (routes[currentPath.value.slice(1) || "/"]) {
     document.title =
       routes[currentPath.value.slice(1) || "/"].title +
-      " - Configuration � ServicePulse";
+      " - Message � ServicePulse";
   }
 });
 
+onBeforeMount(() => {
+    getMessageErrorDetails();
+})
+
 onMounted(() => {
   getMessageErrorDetails();
-  const path = currentPath.value.slice(1) || "/";
-  if (
-    path === "/" &&
-    !connectionState.connected &&
-    !connectionState.connectedRecently
-  ) {
-    /* empty */
-  }
 });
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12">
-        <h1>{{ messageErrorDetails.message_type }}</h1>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="tabs">
-          <h5
-            :class="{
-              active: subIsActive('#stack-trace') || subIsActive(''),
-              disabled:
-                !connectionState.connected &&
-                !connectionState.connectedRecently,
-            }"
-          >
-            <a href="#stack-trace"
-              >Stack Trace {{ messageErrorDetails }}</a
-            >
-          </h5>
-          <h5
-            :class="{
-              active: subIsActive('#headers'),
-              disabled:
-                !connectionState.connected &&
-                !connectionState.connectedRecently,
-            }"
-          >
-            <a href="#headers">Headers</a>
-          </h5>
-          <h5
-            :class="{
-              active: subIsActive('#message-body'),
-              disabled:
-                !connectionState.connected &&
-                !connectionState.connectedRecently,
-            }"
-          >
-            <a href="#message-body">Message Body</a>
-          </h5>
-          <h5
-            :class="{
-              active: subIsActive('#flow-diagram'),
-              disabled:
-                !connectionState.connected &&
-                !connectionState.connectedRecently,
-            }"
-          >
-            <a href="#flow-diagram">Flow Diagram</a>
-          </h5>
+    <no-data
+        v-if="fetchError.error"
+        title="message failures"
+        message="Could not find message. This could be because the message URL is invalid or the corresponding message was processed and is no longer tracked by ServiceControl."
+    ></no-data>
+    <div v-show="!fetchError.error" class="container">
+        <div class="row">
+            <div class="col-sm-12">   
+                <div class="active break group-title">
+                    <h1 class="message-type-title">{{ messageErrorDetails.message_type }}</h1>
+                </div>        
+            </div>
         </div>
-      </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="metadata group-title group-message-count message-metadata">
+                    <span v-if="messageErrorDetails.number_of_processing_attempts > 1" tooltip="This message has already failed {{messageErrorDetails.number_of_processing_attempts}} times" class="label sidebar-label label-important metadata-label">{{messageErrorDetails.number_of_processing_attempts}} Retry Failures</span>
+                    <span class="metadata"><i class="fa fa-clock-o"></i> Failed: <time-since :date-utc="messageErrorDetails.time_of_failure"></time-since></span>
+                    <span class="metadata"><i class="fa pa-endpoint"></i> Endpoint: {{ messageErrorDetails.receiving_endpoint.name }}</span>
+                    <span class="metadata"><i class="fa fa-laptop"></i> Machine: {{ messageErrorDetails.receiving_endpoint.host }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="tabs">
+                <h5
+                    :class="{
+                    active: subIsActive('#stack-trace') || subIsActive(''),
+                    disabled:
+                        !connectionState.connected &&
+                        !connectionState.connectedRecently,
+                    }"
+                >
+                    <a href="#stack-trace">Stack Trace</a>
+                </h5>
+                <h5
+                    :class="{
+                    active: subIsActive('#headers'),
+                    disabled:
+                        !connectionState.connected &&
+                        !connectionState.connectedRecently,
+                    }"
+                >
+                    <a href="#headers">Headers</a>
+                </h5>
+                <h5
+                    :class="{
+                    active: subIsActive('#message-body'),
+                    disabled:
+                        !connectionState.connected &&
+                        !connectionState.connectedRecently,
+                    }"
+                >
+                    <a href="#message-body">Message Body</a>
+                </h5>
+                <h5
+                    :class="{
+                    active: subIsActive('#flow-diagram'),
+                    disabled:
+                        !connectionState.connected &&
+                        !connectionState.connectedRecently,
+                    }"
+                >
+                    <a href="#flow-diagram">Flow Diagram</a>
+                </h5>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                {{ messageErrorDetails }}
+            </div>
+        </div>
+        <component :is="currentView" v-on="currentEvents" />
     </div>
-    <component :is="currentView" v-on="currentEvents" />
-  </div>
 </template>
 
 <style></style>
