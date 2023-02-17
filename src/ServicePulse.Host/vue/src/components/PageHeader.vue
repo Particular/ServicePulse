c610885d37911c91460989550b271040dd0ecfa4
williambza <william.brander@gmail.com>
2023-02-17 09:37:37 +0200

    Remove extra close tag

--- src/ServicePulse.Host/vue/src/components/PageHeader.vue
+++ src/ServicePulse.Host/vue/src/components/PageHeader.vue
@@ -107,10 +107,6 @@ const displayDanger = computed(() => {
                 >{{ stats.number_of_failed_messages }}</span
               >
             </RouterLink>
-                class="badge badge-important"
-                >{{ stats.number_of_failed_messages }}</span
-              >
-            </RouterLink>
           </li>
           <li :class="{ active: subIsActive('/a/#/custom-checks') }">
             <a href="/a/#/custom-checks">
