3ca56c7906c1a8d76ff071d013d88b2cd1a6c111
Travis Nickels <travis.nickels@particular.net>
2023-02-22 12:12:59 -0800

    formatting

--- src/ServicePulse.Host/vue/src/views/MessageView.vue
+++ src/ServicePulse.Host/vue/src/views/MessageView.vue
@@ -12,7 +12,6 @@ const failedMessage = ref({});
 function loadFailedMessage() {
   return useFetchFromServiceControl("errors/last/" + id)
     .then((response) => {
-
       if (response.status === 404) {
         failedMessage.value = { notFound: true };
       } else if (response.status !== 200) {
@@ -86,17 +85,19 @@ function togglePanel(panelNum) {
 }
 
 onMounted(() => {
-  loadFailedMessage().then(() => {
-    togglePanel(1);
-  }).then(() => {
-    downloadHeadersAndBody();
-  });  
+  loadFailedMessage()
+    .then(() => {
+      togglePanel(1);
+    })
+    .then(() => {
+      downloadHeadersAndBody();
+    });
 });
 </script>
 
 <template>
   <div class="container">
-    <section>    
+    <section>
       <section name="failed_message">
         <no-data v-if="failedMessage?.notFound" title="message failures" message="Could not find message. This could be because the message URL is invalid or the corresponding message was processed and is no longer tracked by ServiceControl."></no-data>
         <no-data v-if="failedMessage?.error" title="message failures" message="An error occurred while trying to load the message. Please check the ServiceControl logs to learn what the issue is."></no-data>
@@ -139,269 +140,308 @@ onMounted(() => {
               </div>
             </div>
           </div>
-            <div class="row">
-              <div isolate-click class="col-sm-12 no-side-padding">
-                <div class="nav tabs msg-tabs">
-                  <h5 :class="{ active: failedMessage.panel === 1 }" class="nav-item" v-on:click="togglePanel(1)"><a href="#">Stacktrace</a></h5>
-                  <h5 :class="{ active: failedMessage.panel === 2 }" class="nav-item" v-on:click="togglePanel(2)"><a href="#">Headers</a></h5>
-                  <h5 :class="{ active: failedMessage.panel === 3 }" class="nav-item" v-on:click="togglePanel(3)"><a href="#">Message body</a></h5>
-                  <h5 :class="{ active: failedMessage.panel === 4 }" class="nav-item" v-on:click="togglePanel(4)"><a href="#">Flow Diagram</a></h5>
-                </div>
-                <pre isolate-click v-if="failedMessage.panel === 0">{{ failedMessage.exception?.message }}</pre>
-                <pre isolate-click v-if="failedMessage.panel === 1">{{ failedMessage.exception?.stack_trace }}</pre>
-                <table isolate-click class="table" v-if="failedMessage.panel === 2 && !failedMessage.headersNotFound">
-                  <tbody>
-                    <tr class="interactiveList" v-for="(header, index) in failedMessage.headers" :key="index">
-                      <td nowrap="nowrap">{{ header.key }}</td>
-                      <td>
-                        <pre>{{ header.value }}</pre>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                <div isolate-click class="alert alert-info" v-if="failedMessage.panel === 2 && failedMessage?.headersNotFound">Could not find message headers. This could be because the message URL is invalid or the corresponding message was processed and is no longer tracked by ServiceControl.</div>
-                <pre isolate-click v-if="failedMessage.panel === 3 && !failedMessage?.messageBodyNotFound">{{ failedMessage.messageBody }}</pre>
-                <div isolate-click class="alert alert-info" v-if="failedMessage.panel === 3 && failedMessage?.messageBodyNotFound">Could not find message body. This could be because the message URL is invalid or the corresponding message was processed and is no longer tracked by ServiceControl.</div>
-                <flow-diagram v-if="failedMessage.conversationId" conversation-id="{{failedMessage.conversationId}}" v-show="failedMessage.panel === 4"></flow-diagram>
+          <div class="row">
+            <div isolate-click class="col-sm-12 no-side-padding">
+              <div class="nav tabs msg-tabs">
+                <h5 :class="{ active: failedMessage.panel === 1 }" class="nav-item" v-on:click="togglePanel(1)"><a href="#">Stacktrace</a></h5>
+                <h5 :class="{ active: failedMessage.panel === 2 }" class="nav-item" v-on:click="togglePanel(2)"><a href="#">Headers</a></h5>
+                <h5 :class="{ active: failedMessage.panel === 3 }" class="nav-item" v-on:click="togglePanel(3)"><a href="#">Message body</a></h5>
+                <h5 :class="{ active: failedMessage.panel === 4 }" class="nav-item" v-on:click="togglePanel(4)"><a href="#">Flow Diagram</a></h5>
               </div>
+              <pre isolate-click v-if="failedMessage.panel === 0">{{ failedMessage.exception?.message }}</pre>
+              <pre isolate-click v-if="failedMessage.panel === 1">{{ failedMessage.exception?.stack_trace }}</pre>
+              <table isolate-click class="table" v-if="failedMessage.panel === 2 && !failedMessage.headersNotFound">
+                <tbody>
+                  <tr class="interactiveList" v-for="(header, index) in failedMessage.headers" :key="index">
+                    <td nowrap="nowrap">{{ header.key }}</td>
+                    <td>
+                      <pre>{{ header.value }}</pre>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div isolate-click class="alert alert-info" v-if="failedMessage.panel === 2 && failedMessage?.headersNotFound">Could not find message headers. This could be because the message URL is invalid or the corresponding message was processed and is no longer tracked by ServiceControl.</div>
+              <pre isolate-click v-if="failedMessage.panel === 3 && !failedMessage?.messageBodyNotFound">{{ failedMessage.messageBody }}</pre>
+              <div isolate-click class="alert alert-info" v-if="failedMessage.panel === 3 && failedMessage?.messageBodyNotFound">Could not find message body. This could be because the message URL is invalid or the corresponding message was processed and is no longer tracked by ServiceControl.</div>
+              <flow-diagram v-if="failedMessage.conversationId" conversation-id="{{failedMessage.conversationId}}" v-show="failedMessage.panel === 4"></flow-diagram>
             </div>
-        </div> 
+          </div>
+        </div>
       </section>
     </section>
   </div>
 </template>
 
 <style>
-
 /* .row {
     margin-right: 0;
     margin-left: 0;
 } */
 
 .break {
-    -ms-word-wrap: break-word;
-    word-break: break-all;
-    word-wrap: break-word;
+  -ms-word-wrap: break-word;
+  word-break: break-all;
+  word-wrap: break-word;
 }
 
 h1.message-type-title {
-    margin: 0 0 8px;
-    font-size: 24px;
+  margin: 0 0 8px;
+  font-size: 24px;
 }
 
 .group-title {
-    display: block;
-    font-size: 30px;
-    margin: 10px 0 0;
+  display: block;
+  font-size: 30px;
+  margin: 10px 0 0;
 }
 
-h2.group-title, h3.group-title {
-    font-weight: bold;
-    line-height: 28px;
+h2.group-title,
+h3.group-title {
+  font-weight: bold;
+  line-height: 28px;
 }
 
-.group-title.group-message-count sp-moment, .group-title.group-message-count i {
-    font-size: 16px;
-    color: #777f7f;
+.group-title.group-message-count sp-moment,
+.group-title.group-message-count i {
+  font-size: 16px;
+  color: #777f7f;
 }
 
 .group-title {
-    display: block;
-    font-size: 30px;
-    margin: 10px 0 0;
+  display: block;
+  font-size: 30px;
+  margin: 10px 0 0;
 }
 
 .metadata-label {
-    margin-right: 24px;
-    position: relative;
-    top: -1px;
+  margin-right: 24px;
+  position: relative;
+  top: -1px;
 }
 
 .label {
-    display: inline;
-    padding: 0.2em 0.6em 0.3em;
-    font-size: 75%;
-    font-weight: 700;
-    line-height: 1;
-    color: #fff;
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: baseline;
-    border-radius: 0.25em;
+  display: inline;
+  padding: 0.2em 0.6em 0.3em;
+  font-size: 75%;
+  font-weight: 700;
+  line-height: 1;
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+  border-radius: 0.25em;
 }
 
 .label-warning,
 .badge-warning {
-    background-color: #aa6708;
-    border-color: #aa6708;
+  background-color: #aa6708;
+  border-color: #aa6708;
 }
 
 .group-message-count {
-    color: #A8B3B1;
-    font-size: 16px;
-    margin: 4px 0 12px;
-    display: block;
+  color: #a8b3b1;
+  font-size: 16px;
+  margin: 4px 0 12px;
+  display: block;
 }
 
 .label-important,
 .badge-important {
-    background-image: none;
+  background-image: none;
 }
 
 .sidebar-label {
-    box-shadow: none;
-    color: #ffffff;
-    display: inline-block;
-    font-size: 12px;
-    margin-top: 3px;
-    max-width: 100%;
-    padding: 6px 10px;
+  box-shadow: none;
+  color: #ffffff;
+  display: inline-block;
+  font-size: 12px;
+  margin-top: 3px;
+  max-width: 100%;
+  padding: 6px 10px;
 }
 
 .message-metadata {
-    display: inline;
+  display: inline;
 }
 
 .message-metadata {
-    display: inline;
+  display: inline;
 }
 
-span.metadata.danger, i.fa.fa-trash-o.danger, sp-moment.danger {
-    font-weight: normal !important;
+span.metadata.danger,
+i.fa.fa-trash-o.danger,
+sp-moment.danger {
+  font-weight: normal !important;
 }
-
 
 .btn-toolbar {
-    padding: 12px 0 0;
-    margin-left: 0;
+  padding: 12px 0 0;
+  margin-left: 0;
 }
 
 div.btn-toolbar.message-toolbar {
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 }
 .btn-toolbar {
-    padding: 12px 0 0;
-    margin-left: 0;
+  padding: 12px 0 0;
+  margin-left: 0;
 }
 
 button img {
-    position: relative;
-    top: -1px;
-    width: 17px;
+  position: relative;
+  top: -1px;
+  width: 17px;
 }
 
-.btn-toolbar > .btn, .btn-toolbar > .btn-group, .btn-toolbar > .input-group, .action-btns .btn {
-    margin-left: 0;
-    margin-right: 5px;
+.btn-toolbar > .btn,
+.btn-toolbar > .btn-group,
+.btn-toolbar > .input-group,
+.action-btns .btn {
+  margin-left: 0;
+  margin-right: 5px;
 }
 
-.btn-default.disabled:hover, .btn-default[disabled]:hover, fieldset[disabled] .btn-default:hover, .btn-default.disabled:focus, .btn-default[disabled]:focus, fieldset[disabled] .btn-default:focus, .btn-default.disabled.focus, .btn-default[disabled].focus, fieldset[disabled] .btn-default.focus {
-    background-color: #fff;
-    border-color: #ccc;
+.btn-default.disabled:hover,
+.btn-default[disabled]:hover,
+fieldset[disabled] .btn-default:hover,
+.btn-default.disabled:focus,
+.btn-default[disabled]:focus,
+fieldset[disabled] .btn-default:focus,
+.btn-default.disabled.focus,
+.btn-default[disabled].focus,
+fieldset[disabled] .btn-default.focus {
+  background-color: #fff;
+  border-color: #ccc;
 }
 
-.btn-default:active:hover, .btn-default.active:hover, .open > .dropdown-toggle.btn-default:hover, .btn-default:active:focus, .btn-default.active:focus, .open > .dropdown-toggle.btn-default:focus, .btn-default:active.focus, .btn-default.active.focus, .open > .dropdown-toggle.btn-default.focus {
-    color: #333;
-    border-color: #8c8c8c;
+.btn-default:active:hover,
+.btn-default.active:hover,
+.open > .dropdown-toggle.btn-default:hover,
+.btn-default:active:focus,
+.btn-default.active:focus,
+.open > .dropdown-toggle.btn-default:focus,
+.btn-default:active.focus,
+.btn-default.active.focus,
+.open > .dropdown-toggle.btn-default.focus {
+  color: #333;
+  border-color: #8c8c8c;
 }
 
-.btn:focus, .btn:active:focus, .btn.active:focus, .btn.focus, .btn:active.focus, .btn.active.focus {
-    outline: 5px auto -webkit-focus-ring-color;
-    outline-offset: -2px;
+.btn:focus,
+.btn:active:focus,
+.btn.active:focus,
+.btn.focus,
+.btn:active.focus,
+.btn.active.focus {
+  outline: 5px auto -webkit-focus-ring-color;
+  outline-offset: -2px;
 }
 
-.btn-default:active, .btn-default.active, .open > .dropdown-toggle.btn-default {
-    color: #333;
-    background-color: #e6e6e6;
-    background-image: none;
-    border-color: #adadad;
+.btn-default:active,
+.btn-default.active,
+.open > .dropdown-toggle.btn-default {
+  color: #333;
+  background-color: #e6e6e6;
+  background-image: none;
+  border-color: #adadad;
 }
 
 .btn-default:hover {
-    color: #333;
-    background-color: #e6e6e6;
-    border-color: #adadad;
+  color: #333;
+  background-color: #e6e6e6;
+  border-color: #adadad;
 }
 
-.btn-default:focus, .btn-default.focus {
-    color: #333;
-    background-color: #e6e6e6;
-    border-color: #8c8c8c;
+.btn-default:focus,
+.btn-default.focus {
+  color: #333;
+  background-color: #e6e6e6;
+  border-color: #8c8c8c;
 }
 
-.btn.disabled, .btn[disabled], fieldset[disabled] .btn {
-    cursor: not-allowed;
-    pointer-events: unset;
-    border-color: #adadad;
-    background-color: #fff;
-    filter: alpha(opacity=65);
-    opacity: 0.65;
-    -webkit-box-shadow: none;
-    box-shadow: none;
+.btn.disabled,
+.btn[disabled],
+fieldset[disabled] .btn {
+  cursor: not-allowed;
+  pointer-events: unset;
+  border-color: #adadad;
+  background-color: #fff;
+  filter: alpha(opacity=65);
+  opacity: 0.65;
+  -webkit-box-shadow: none;
+  box-shadow: none;
 }
 
-.btn:active, .btn.active {
-    background-image: none;
-    outline: 0;
-    -webkit-box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
-    box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+.btn:active,
+.btn.active {
+  background-image: none;
+  outline: 0;
+  -webkit-box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+  box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
 }
 
-.btn:hover, .btn:focus, .btn.focus {
-    color: #333;
-    text-decoration: none;
+.btn:hover,
+.btn:focus,
+.btn.focus {
+  color: #333;
+  text-decoration: none;
 }
 
-a:focus, button:focus {
-    outline: 0 !important;
+a:focus,
+button:focus {
+  outline: 0 !important;
 }
 
-button[disabled], html input[disabled] {
-    cursor: default;
+button[disabled],
+html input[disabled] {
+  cursor: default;
 }
 
 .btn-default {
-    padding: 8px 16px;
+  padding: 8px 16px;
 }
 
 .btn-default {
-    color: #333;
-    background-color: #fff;
-    border-color: #ccc;
+  color: #333;
+  background-color: #fff;
+  border-color: #ccc;
 }
 
 span.metadata {
-    display: inline-block;
-    padding: 0px 20px 2px 0;
-    color: #777f7f;
+  display: inline-block;
+  padding: 0px 20px 2px 0;
+  color: #777f7f;
 }
 
-code, kbd, pre, samp {
-    font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+code,
+kbd,
+pre,
+samp {
+  font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
 }
 
 pre {
-    margin: 3px 0 2px;
+  margin: 3px 0 2px;
 }
 pre {
-    white-space: pre-wrap;
+  white-space: pre-wrap;
 }
 pre {
-    display: block;
-    padding: 9.5px;
-    margin: 0 0 10px;
-    font-size: 13px;
-    line-height: 1.42857143;
-    color: #333333;
-    word-break: break-all;
-    word-wrap: break-word;
-    background-color: #f5f5f5;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+  display: block;
+  padding: 9.5px;
+  margin: 0 0 10px;
+  font-size: 13px;
+  line-height: 1.42857143;
+  color: #333333;
+  word-break: break-all;
+  word-wrap: break-word;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 
 .msg-tabs {
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 }
 
 /* .label-warning,
