5f67339a5931f4833499645cb0b82a835f5fca95
williambza <william.brander@gmail.com>
2023-02-27 14:49:34 +0200

    Undo pendingRetry setting

--- src/ServicePulse.Host/vue/public/js/app.constants.js
+++ src/ServicePulse.Host/vue/public/js/app.constants.js
@@ -3,5 +3,5 @@ window.defaultConfig = {
     version: '1.2.0',
     service_control_url: 'http://localhost:33333/api/',
     monitoring_urls: ['http://localhost:33633/'],
-    showPendingRetry: false,
+    showPendingRetry: false
 };
