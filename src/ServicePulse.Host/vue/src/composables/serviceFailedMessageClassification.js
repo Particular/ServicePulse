d04718e8e7592f7f4dfb219a66808bca682e24df
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-02-28 22:53:29 -0800

    removed unused function

--- src/ServicePulse.Host/vue/src/composables/serviceFailedMessageClassification.js
+++ src/ServicePulse.Host/vue/src/composables/serviceFailedMessageClassification.js
@@ -31,36 +31,7 @@ export class useFailedMessageGroupClassification {
   }
 }
 
-export class useArchivedMessageGroupClassification {
-    getGroupingClassifiers() {
-        return useFetchFromServiceControl("recoverability/classifiers").then((response) => {
-            return response.json();
-        });
-    }
 
-    loadDefaultGroupingClassifier(router) {
-        let urlGrouping = router.query.groupBy;
-
-        if (urlGrouping) {
-            this.saveDefaultGroupingClassifier(urlGrouping);
-            return urlGrouping;
-        }
-
-        const cookies = useCookies().cookies;
-        let cookieGrouping = cookies.get("archived_groups_classification");
-
-        if (cookieGrouping) {
-            return cookieGrouping;
-        }
-
-        return null;
-    }
-
-    saveDefaultGroupingClassifier(classifier) {
-        const cookies = useCookies().cookies;
-        cookies.set("archived_groups_classification", classifier);
-    }
-}
 
 export class useMessageGroupClassification {
     getGroupingClassifiers() {
