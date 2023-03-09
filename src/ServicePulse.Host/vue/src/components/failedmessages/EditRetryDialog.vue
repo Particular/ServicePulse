4b85ea96649370b8859fa2642ba93d9f26362ae9
Travis Nickels <travis.nickels@particular.net>
2023-03-08 20:09:12 -0800

    update modal display for edit and retry message

--- src/ServicePulse.Host/vue/src/components/failedmessages/EditRetryDialog.vue
+++ src/ServicePulse.Host/vue/src/components/failedmessages/EditRetryDialog.vue
@@ -10,6 +10,8 @@ const settings = defineProps({
 
 let panel = ref();
 let message = ref();
+let showEditAndRetryConfirmation = ref(false);
+//let showCancelConfirmation = ref(false);
 
 function initializeMessageHeaders() {
   message.value = settings.message;
@@ -37,6 +39,10 @@ function retry() {
   emit("retry", settings);
 }
 
+function confirmEditAndRetry() {
+  showEditAndRetryConfirmation.value = true;
+}
+
 function togglePanel(panelNum) {
   panel.value = panelNum;
   return false;
@@ -49,68 +55,95 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container">
-    <section name="failed_message">
+  <section name="failed_message_editor">
+    <div class="model modal-msg-editor modal-open" style="z-index: 1050; display: block">
       <div class="modal-mask">
-        <div class="modal-content">
-          <div class="modal-container">
+        <div class="modal-dialog">
+          <div class="modal-content">
             <div class="modal-header">
               <div class="modal-title">
                 <h3>Edit and retry message</h3>
               </div>
             </div>
             <div class="modal-body">
-              <div class="row msg-editor-content">
-                <div class="col-sm-12 no-side-padding">
-                  <div class="nav tabs msg-tabs">
-                    <h5 :class="{ active: panel === 1 }" class="nav-item" @click="togglePanel(1)"><a href="#">Headers</a></h5>
-                    <h5 :class="{ active: panel === 2 }" class="nav-item" @click="togglePanel(2)"><a href="#">Message body</a></h5>
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="row msg-editor-tabs">
+                    <div class="col-sm-12 no-side-padding">
+                      <div class="tabs msg-tabs">
+                        <h5 :class="{ active: panel === 1 }" class="nav-item" @click="togglePanel(1)"><a href="#">Headers</a></h5>
+                        <h5 :class="{ active: panel === 2 }" class="nav-item" @click="togglePanel(2)"><a href="#">Message body</a></h5>
+                      </div>
+                    </div>
                   </div>
-                  <table class="table" v-if="panel === 1">
-                    <tbody>
-                      <tr class="interactiveList" v-for="(header, index) in message.headers" :key="index">
-                        <td nowrap="nowrap">
-                          {{ header.key }}
-                          &nbsp;
-                          <i class="fa fa-lock" tooltip="Protected system header" v-if="header.isLocked"></i>
-                        </td>
-                        <td>
-                          <input class="form-control" :disabled="settings.configuration.locked_headers.includes(header.key)" v-model="header.value" />
-                        </td>
-                        <td>
-                          <a v-if="!settings.configuration.locked_headers.includes(header.key)" href="#">
-                            <i class="fa fa-trash" tooltip="Protected system header"></i>
-                          </a>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  <div class="row msg-editor-content">
+                    <div class="col-sm-12 no-side-padding">
+                      <table class="table" v-if="panel === 1">
+                        <tbody>
+                          <tr class="interactiveList" v-for="(header, index) in message.headers" :key="index">
+                            <td nowrap="nowrap">
+                              {{ header.key }}
+                              &nbsp;
+                              <i class="fa fa-lock" tooltip="Protected system header" v-if="header.isLocked"></i>
+                            </td>
+                            <td>
+                              <input class="form-control" :disabled="header.isLocked" v-model="header.value" />
+                            </td>
+                            <td>
+                              <a v-if="!settings.configuration.locked_headers.includes(header.key)" href="#">
+                                <i class="fa fa-trash" tooltip="Protected system header"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <textarea v-if="panel === 2" class="form-control" v-model="message.messageBody"></textarea>
+                  </div>
                 </div>
               </div>
-              <pre v-if="panel === 2">{{ settings.message.messageBody }}</pre>
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer" v-if="!showEditAndRetryConfirmation">
               <button class="btn btn-default" @click="close()">Cancel</button>
-              <button class="btn btn-primary" @click="retry()">Retry</button>
+              <button class="btn btn-primary" @click="confirmEditAndRetry()">Retry</button>
+            </div>
+            <div class="modal-footer edit-retry-confirmation" v-if="showEditAndRetryConfirmation">
+              <div>Are you sure you want to continue? If you edited the message, it may cause unexpected consequences in the system.</div>
+              <button class="btn btn-default" @click="retry()">Yes</button>
+              <button class="btn btn-primary" @click="showEditAndRetryConfirmation = false">No</button>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
 
 <style>
-.modal-container {
-  width: 70%;
+.btn-primary:hover {
+  color: #fff;
 }
-.modal-body {
-  overflow-y: scroll;
-  height: 80vh;
+
+.cancel-confirmation,
+.edit-retry-confirmation {
+  background: #181919;
+  color: #fff;
+  border-bottom-right-radius: 6px;
+  border-bottom-left-radius: 6px;
 }
-.msg-editor-content tr td:first-child {
-  padding-top: 15px;
-  padding-left: 0;
-  width: 30%;
+
+.cancel-confirmation div,
+.edit-retry-confirmation div {
+  display: inline-block;
+  font-weight: bold;
+  font-size: 14px;
+  position: relative;
+  top: 1px;
+  margin-right: 20px;
+}
+
+.modal-body textarea {
+  height: 100%;
+  margin-top: 20px;
 }
 </style>
