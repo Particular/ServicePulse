77f1a89d91cb44acb747390ac5d9e35420f3b15a
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-03-16 21:27:50 -0700

    Update src/ServicePulse.Host/vue/src/components/EventItemShort.vue
    Co-authored-by: Travis Nickels <travis.nickels@particular.net>

--- src/ServicePulse.Host/vue/src/components/EventItemShort.vue
+++ src/ServicePulse.Host/vue/src/components/EventItemShort.vue
@@ -52,7 +52,7 @@ function navigateToEvent(eventLogItem) {
     case "MessageFailures":
       var newlocation = "/failed-messages";
       if (eventLogItem.related_to && eventLogItem.related_to.length>0 && eventLogItem.related_to[0].search("message") > 0) {
-      newlocation = "/failed-messages" + eventLogItem.related_to[0];
+        newlocation = "/failed-messages" + eventLogItem.related_to[0];
       }
       router.push(newlocation);
       break;
