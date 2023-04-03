2d166b29de10a7111c5cff0900838f84e18e7e07
williambza <william.brander@gmail.com>
2023-04-03 15:35:02 +0200

    Only show connection icon if monitoring is enabled

--- src/ServicePulse.Host/vue/src/views/ConfigurationView.vue
+++ src/ServicePulse.Host/vue/src/views/ConfigurationView.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from "vue";
 import { licenseStatus } from "../composables/serviceLicense.js";
 import { connectionState, monitoringConnectionState } from "../composables/serviceServiceControl";
+import { useIsMonitoringDisabled } from "../composables/serviceServiceControlUrls";
 import { useRedirects } from "../composables/serviceRedirects.js";
 import ExclamationMark from "../components/ExclamationMark.vue";
 
@@ -45,7 +46,7 @@ onMounted(() => {
           <h5 v-if="!licenseStatus.isExpired" :class="{ active: subIsActive('connections') }" class="nav-item">
             <RouterLink :to="{ name: 'connections' }">
               Connections
-              <template v-if="connectionState.unableToConnect || monitoringConnectionState.unableToConnect">
+              <template v-if="connectionState.unableToConnect || (monitoringConnectionState.unableToConnect && !useIsMonitoringDisabled())">
                 <span><i class="fa fa-exclamation-triangle"></i></span>
               </template>
             </RouterLink>
