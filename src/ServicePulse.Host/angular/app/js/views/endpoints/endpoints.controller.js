7e87e1e742d7f26a7c51ba1d5bfb4e66966b8322
williambza <william.brander@gmail.com>
2023-05-15 09:46:09 +0200

    Allow displaying logical endpoint

--- src/ServicePulse.Host/angular/app/js/views/endpoints/endpoints.controller.js
+++ src/ServicePulse.Host/angular/app/js/views/endpoints/endpoints.controller.js
@@ -22,6 +22,7 @@
         $scope.sortOptions = ['Name', 'Latest heartbeat'];
         $scope.sort = 'Name';
         $scope.sortDir = '';
+        $scope.display = 'Endpoint Instances';
 
         $scope.isInactiveEndpoints = true;
 
@@ -33,75 +34,123 @@
             updateUI();
         }, 5000);
 
+        function mapEndpointsToDisplay(endpoints) {
+            if ($scope.display == 'Logical Endpoints') {
+                var logicalNames = [...new Set(endpoints.data.map(function (endpoint) { return endpoint.name; }))];
+                var logicalEndpoints = logicalNames.map(function (endpointName) {
+                    var aliveList = endpoints.data.filter(function (ep) { 
+                        return ep.name == endpointName && ep.monitor_heartbeat && ep.heartbeat_information.reported_status === 'beating';
+                    });
+
+                    var aliveCount = aliveList ? aliveList.length : 0;
+                    var downCount = endpoints.data.filter(function (ep) { 
+                        return ep.name == endpointName && ep.monitor_heartbeat;
+                    }).length - aliveCount;
+
+                    return {
+                        name: endpointName,
+                        aliveCount: aliveCount,
+                        downCount: downCount,
+                        heartbeat_information: {
+                            reported_status: aliveList && aliveList.length ? 'beating' : 'dead',
+                            last_report_at: aliveList && aliveList.length ? aliveList[0].heartbeat_information.last_report_at : undefined
+                        },
+                        monitor_heartbeat: endpoints.data.filter(function (ep) { 
+                            return ep.name == endpointName
+                        }).some(function (ep) {
+                            return ep.monitor_heartbeat;
+                        })
+                    };
+                });
+
+                return { data: logicalEndpoints };
+            }
+
+            return endpoints;
+        }
+
         function updateUI() {
-            configurationService.getEndpoints().then(function(endpoints) {
-                var endpointList = endpoints.data;
+            configurationService.getEndpoints()
+                .then(mapEndpointsToDisplay)
+                .then(function(endpoints) {
+                    var endpointList = endpoints.data;
 
-                // remove unmonitored
-                var unmonitored = endpointList.filter(function(umi) {
-                    return !umi.monitor_heartbeat;
-                });
+                    // remove unmonitored
+                    var unmonitored = endpointList.filter(function(umi) {
+                        return !umi.monitor_heartbeat;
+                    });
 
-                var i, obj;
+                    var i, obj;
 
-                for (i = 0; i < $scope.model.active.length; i++) {
-                    obj = $scope.model.active[i];
-                    if (unmonitored.find(function (endpoint) { return endpoint.id == obj.id; }) !== -1) {
-                        $scope.model.active.splice(i, 1);
-                        i--; // we just made the array 1 shorter
+                    for (i = 0; i < $scope.model.active.length; i++) {
+                        obj = $scope.model.active[i];
+                        if (unmonitored.find(function (endpoint) { return $scope.display == 'Endpoint Instances' ? endpoint.id == obj.id : endpoint.name == obj.name; }) !== -1) {
+                            $scope.model.active.splice(i, 1);
+                            i--; // we just made the array 1 shorter
+                        }
                     }
-                }
 
-                for (i = 0; i < $scope.model.inactive.length; i++) {
-                    obj = $scope.model.inactive[i];
-                    if (unmonitored.find(function (endpoint) { return endpoint.id == obj.id; }) !== -1) {
-                        $scope.model.inactive.splice(i, 1);
-                        i--;
+                    for (i = 0; i < $scope.model.inactive.length; i++) {
+                        obj = $scope.model.inactive[i];
+                        if (unmonitored.find(function (endpoint) { return $scope.display == 'Endpoint Instances' ? endpoint.id == obj.id : endpoint.name == obj.name; }) !== -1) {
+                            $scope.model.inactive.splice(i, 1);
+                            i--;
+                        }
                     }
-                }
 
-                var monitored = endpointList.filter(function(mi) {
-                    return mi.monitor_heartbeat;
-                });
+                    var monitored = endpointList.filter(function(mi) {
+                        return mi.monitor_heartbeat;
+                    });
 
-                for (var j = 0; j < monitored.length; j++) {
-                    var item = monitored[j];
+                    for (var j = 0; j < monitored.length; j++) {
+                        var item = monitored[j];
 
-                    var activeIndex = $scope.model.active.map(function(bi) { return bi.id; }).indexOf(item.id);
-                    var inactiveIndex = $scope.model.inactive.map(function(bi) { return bi.id; }).indexOf(item.id);
+                        var activeIndex = $scope.model.active.map(function(bi) { return $scope.display == 'Endpoint Instances' ? bi.id : bi.name; }).indexOf($scope.display == 'Endpoint Instances' ? item.id : item.name);
+                        var inactiveIndex = $scope.model.inactive.map(function(bi) { return $scope.display == 'Endpoint Instances' ? bi.id : bi.name; }).indexOf($scope.display == 'Endpoint Instances' ? item.id : item.name);
 
-                    if (Object.prototype.hasOwnProperty.call(item, 'heartbeat_information') && item.heartbeat_information.reported_status === 'beating') {
-                        if (activeIndex === -1) {
-                            $scope.model.active.push(item);
+                        if (Object.prototype.hasOwnProperty.call(item, 'heartbeat_information') && item.heartbeat_information.reported_status === 'beating') {
+                            if (activeIndex === -1) {
+                                $scope.model.active.push(item);
+                            } else {
+                                var activeitem = $scope.model.active[activeIndex];
+                                activeitem.heartbeat_information.last_report_at = item.heartbeat_information.last_report_at;
+                            }
+                            if (inactiveIndex !== -1) {
+                                $scope.model.inactive.splice(inactiveIndex, 1);
+                            }
                         } else {
-                            var activeitem = $scope.model.active[activeIndex];
-                            activeitem.heartbeat_information.last_report_at = item.heartbeat_information.last_report_at;
-                        }
-                        if (inactiveIndex !== -1) {
-                            $scope.model.inactive.splice(inactiveIndex, 1);
-                        }
-                    } else {
-                        if (inactiveIndex === -1) {
-                            $scope.model.inactive.push(item);
-                        }
-                        if (activeIndex !== -1) {
-                            $scope.model.active.splice(activeIndex, 1);
-                        }
+                            if (inactiveIndex === -1) {
+                                $scope.model.inactive.push(item);
+                            }
+                            if (activeIndex !== -1) {
+                                $scope.model.active.splice(activeIndex, 1);
+                            }
 
+                        }
                     }
-                }
 
-                var sort = getSortFunction();
+                    var sort = getSortFunction();
 
-                $scope.model.inactive.sort(sort);
-                $scope.model.active.sort(sort);
-            });
+                    $scope.model.inactive.sort(sort);
+                    $scope.model.active.sort(sort);
+                });
         }
 
         $scope.deleteEndpoint = function(endpoint) {
             configurationService.deleteEndpoint(endpoint.id).then(function() {
                 $scope.model.inactive.splice($scope.model.inactive.indexOf(endpoint), 1);
             });
+        }
+
+        $scope.endpointDisplayName = function(endpoint) {
+            if ($scope.display == 'Logical Endpoints') {
+                if (endpoint.aliveCount > 0) {
+                    return endpoint.name + ' (' + endpoint.aliveCount + ' instance' + (endpoint.aliveCount > 1 ? 's)' : ')');
+                }
+
+                return endpoint.name + ' (0 out of ' + endpoint.downCount + ' previous instance' + (endpoint.downCount > 1 ? 's' : '') + ' reporting)';
+            }
+            return endpoint.name + '@' + endpoint.host_display_name;
         }
 
         function autoGetEndPoints() {
@@ -137,6 +186,11 @@
                 }, function(message) {
                     result.workflow_state = createWorkflowState('error', message);
                 });
+        };
+
+        $scope.changeDisplay = function(display) {
+            $scope.display = display;
+            updateUI();
         };
 
         $scope.changeSort = function(sortBy, sortDir) {
