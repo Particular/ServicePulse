00793e2c442fc1d3854f8dd68c73137b0bf9a0f8
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-06-19 23:25:07 -0700

    fix view licence links

--- src/ServicePulse.Host/vue/src/components/LicenseExpired.vue
+++ src/ServicePulse.Host/vue/src/components/LicenseExpired.vue
@@ -8,7 +8,7 @@ import { licenseStatus } from "./../composables/serviceLicense.js";
       <h1>Platform license expired</h1>
       <p>Please update your license to continue using the Particular Service Platform</p>
       <div class="action-toolbar">
-        <a class="btn btn-default btn-primary" href="/configuration#license">View license details</a>
+        <a class="btn btn-default btn-primary" href="#/configuration">View license details</a>
       </div>
     </div>
   </template>
@@ -18,7 +18,7 @@ import { licenseStatus } from "./../composables/serviceLicense.js";
       <p>To continue using the Particular Service Platform, please extend your license</p>
       <div class="action-toolbar">
         <a class="btn btn-default btn-primary" href="https://particular.net/extend-your-trial?p=servicepulse" target="_blank">Extend your license <i class="fa fa-external-link"></i></a>
-        <a class="btn btn-default btn-secondary" href="/configuration#license">View license details</a>
+        <a class="btn btn-default btn-secondary" href="#/configuration">View license details</a>
       </div>
     </div>
   </template>
@@ -27,7 +27,7 @@ import { licenseStatus } from "./../composables/serviceLicense.js";
       <h1>Platform license expired</h1>
       <p>Your upgrade protection period has elapsed and your license is not valid for this version of ServicePulse.</p>
       <div class="action-toolbar">
-        <a class="btn btn-default btn-primary" href="/configuration#license">View license details</a>
+        <a class="btn btn-default btn-primary" href="#/configuration">View license details</a>
       </div>
     </div>
   </template>
