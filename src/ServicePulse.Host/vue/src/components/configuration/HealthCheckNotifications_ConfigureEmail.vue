2d4d7dcf218a4d1d5a15c22553177ba43b26650e
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-06-26 09:06:20 -0700

    switch to comma separated addresess

--- src/ServicePulse.Host/vue/src/components/configuration/HealthCheckNotifications_ConfigureEmail.vue
+++ src/ServicePulse.Host/vue/src/components/configuration/HealthCheckNotifications_ConfigureEmail.vue
@@ -32,14 +32,14 @@ const fromIsValid = computed(() => {
   return from.value && emailRe.test(from.value) ? true : false;
 });
 const toIsValid = computed(() => {
-  return to.value && validateMultipleEmailsSemicolonSeparated(to.value) ? true : false;
+  return to.value && validateMultipleEmailsCommaSeparated(to.value) ? true : false;
 });
 const formIsValid = computed(() => {
   return smtpServerIsValid.value && smtpPortIsValid.value && fromIsValid.value && toIsValid.value;
 });
 
-function validateMultipleEmailsSemicolonSeparated(value) {
-  var result = value.split(";");
+function validateMultipleEmailsCommaSeparated(value) {
+  var result = value.split(",");
   for (var i = 0; i < result.length; i++)
   if (!emailRe.test(result[i]))
      return false;
@@ -106,7 +106,7 @@ function close() {
               </div>
               <div class="row"></div>
               <div class="form-group" :class="{ 'has-error': !toIsValid }">
-                  <label for="to">To address <br />(Separate multiple email address with a semicolon. E.g. testing@test.com;testing2@test.com)</label>
+                  <label for="to">To address <br />(Separate multiple email address with a comma. E.g. testing@test.com,testing2@test.com)</label>
                 <input type="email" id="to" name="to" v-model="to" class="form-control" required />
               </div>
             </div>
