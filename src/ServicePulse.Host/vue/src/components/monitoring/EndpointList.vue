464a4c2071d684300d6134e5c175e886d46e4664
Jayanthi <jayanthi.sourirajan@particular.net>
2023-10-08 23:41:24 -0700

    Display of endpoints, - from monitored, from sc , display icons

--- src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
@@ -10,51 +10,153 @@ monitoringService.getMonitoredEndpoints()
  monitoringService.createEndpointsSource(selectedPeriod.value, selectedPeriod.refreshInterval)
 \angular\app\modules\monitoring\js\services\services.monitoring.js
   */
-import { ref, onMounted, onUnmounted } from "vue";
-import { useRouter } from "vue-router";
-import { stats } from "../../composables/serviceServiceControl.js";
-import { useShowToast } from "../../composables/toast.js";
-import { useFetchFromMonitoring } from "../../composables/serviceServiceControlUrls";
-import MonitoringNoData from "./MonitoringNoData.vue";
-import MonitoringNotAvailable from "./MonitoringNotAvailable.vue";
+    import { ref, onMounted, onUnmounted } from "vue";
+    import { useRouter } from "vue-router";
+    import { stats } from "../../composables/serviceServiceControl.js";
+    import { useShowToast } from "../../composables/toast.js";
+    import { useFetchFromMonitoring, useIsMonitoringDisabled } from "../../composables/serviceServiceControlUrls";
+    import MonitoringNoData from "./MonitoringNoData.vue";
+    import MonitoringNotAvailable from "./MonitoringNotAvailable.vue";
+    import { useGetExceptionGroups } from "../../composables/serviceMessageGroup.js";
+    import {  monitoringConnectionState } from "../../composables/serviceServiceControl";
 
-const endpoints = ref([]);
-const historyPeriod = 1;
-    function getMonitoredEndpoints() {
-        return useFetchFromMonitoring(`${`monitored-endpoints` }?history=${historyPeriod}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            endpoints.value = [];
-            endpoints.value = data;
+    const endpoints = ref([]);
+    const exceptionGroups = ref([]);
+    const historyPeriod = 1;
+    const router = useRouter();
+    var hasData = ref(false);
+    var supportsEndpointCount = ref();
+    const metricslargenumber = ref();
+    var monitoringNotAvailable = ref(false);
+
+    function getAllMonitoredEndpoints() {
+        if (!useIsMonitoringDisabled() && !monitoringConnectionState.unableToConnect && monitoringConnectionState.connecting) {
+            return useFetchFromMonitoring(`${`monitored-endpoints`}?history=${historyPeriod}`)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    endpoints.value = [];
+                    endpoints.value = data;
+                    updateUI();
+                    getEndpointsFromScSubscription();
+
+                });
+        }
+        else {  getEndpointsFromScSubscription(); }
+
+    }
+
+    function getEndpointsFromScSubscription() {
+        return useGetExceptionGroups('Endpoint Name').then((result) => {
+            exceptionGroups.value = [];
+            exceptionGroups.value = result;
+            //Squash and add to existing monitored endpoints
+            if (exceptionGroups.value.length > 0) {
+                exceptionGroups.value.forEach((failedMessageEndpoint) => {
+                    if (failedMessageEndpoint.operation_status === 'ArchiveCompleted') {
+                        return;
+                    }
+                    var index = endpoints.value.findIndex(function (item) { return item.name === failedMessageEndpoint.title });
+                    if (index >= 0) {
+                        endpoints.value[index].serviceControlId = failedMessageEndpoint.id;
+                        endpoints.value[index].errorCount = failedMessageEndpoint.count;
+                    } else {
+                        endpoints.value.push({ name: failedMessageEndpoint.title, errorCount: failedMessageEndpoint.count, serviceControlId: failedMessageEndpoint.id, isScMonitoringDisconnected: true });
+                    }
+                });
+            }
+
+
         });
-}
+    }
 
-onMounted(() => {
-    getMonitoredEndpoints();
-});
+    function updateUI() {
+        if (endpoints.value.length > 0) {
+            endpoints.value.forEach((endpoint) => {
+
+                hasData = !endpoint.empty;
+                supportsEndpointCount = endpoint.hasOwnProperty('connectedCount');////$scope.supportsEndpointCount = Object.prototype.hasOwnProperty.call(endpoint, 'connectedCount');
+                if (endpoint.empty) {
+                    return;
+                }
+
+                if (endpoint.error) {
+                    // connectivityNotifier.reportFailedConnection();
+                    if (endpoints) {
+                        endpoints.value.forEach((item) => item.isScMonitoringDisconnected = true);
+                    }
+                } else {
+                    // connectivityNotifier.reportSuccessfulConnection();
+                    var index = endpoints.value.findIndex(function (item) { return item.name === endpoint.name; });
+                    endpoint.isScMonitoringDisconnected = false;
+                    fillDisplayValuesForEndpoint(endpoint);
+                    if (index >= 0) {
+                        mergeIn(endpoints.value[index], endpoint);
+                    } else {
+                        endpoints.value.push(endpoint);
+                        endpoints.value.sort(function (first, second) {
+                            if (first.name < second.name) {
+                                return -1;
+                            }
+
+                            if (first.name > second.name) {
+                                return 1;
+                            }
+
+                            return 0;
+                        });
+                    }
+                }
+
+            });
+        }
+
+
+    }
+    function navigateToMessageGroup($event, groupId) {
+        if ($event.target.localName !== "button") {
+            router.push({ name: "message-groups", params: { groupId: groupId } });
+        }
+    }
+    function fillDisplayValuesForEndpoint(endpoint) {
+
+        //$filter('graphduration')(endpoint.metrics.processingTime);
+        //$filter('graphduration')(endpoint.metrics.criticalTime);
+        //$filter('graphdecimal')(endpoint.metrics.queueLength, 0);
+        //$filter('graphdecimal')(endpoint.metrics.throughput, 2);
+        //$filter('graphdecimal')(endpoint.metrics.retries, 2);
+    }
+
+    function mergeIn(destination, source) {
+        for (var propName in source) {
+            if (Object.prototype.hasOwnProperty.call(source, propName)) {
+                destination[propName] = source[propName];
+            }
+        }
+    }
+    onMounted(() => {
+        getAllMonitoredEndpoints();
+    });
 </script>
 
 
 <template>
     <section ng-if="grouping.selectedGrouping == 0">
-
-        <!--<div ng-include="'modules/monitoring/views/monitoring_not_available.html'" ng-show="!endpoints.length && !loading && hasData"></div>
-    <div ng-include="'modules/monitoring/views/monitoring_no_data.html'" ng-show="!endpoints.length && !loading && !hasData"></div>-->
         <div class="row">
             <div class="col-sm-12">
-                <MonitoringNoData v-show="!endpoints" v-if="endpoints.length === 0"></MonitoringNoData>
+                <MonitoringNoData v-if="!endpoints && endpoints.length === 0 && !hasData  "></MonitoringNoData>
             </div>
         </div>
 
         <div class="row">
             <div class="col-sm-12">
-                <MonitoringNotAvailable v-show="!endpoints" v-if="endpoints.length === 0"></MonitoringNotAvailable>
+                <MonitoringNotAvailable  v-if="!endpoints && endpoints.length === 0 && hasData"></MonitoringNotAvailable>
             </div>
         </div>
+
         <!--Table headings-->
-        <div ng-show="endpoints.length" class="row box box-no-click table-head-row">
+        <div v-if="endpoints && endpoints.length >0" class="row box box-no-click table-head-row">
             <div class="col-xs-2 col-xl-7">
                 <sortable-column property="'name'" ref="order">
                     Endpoint name
@@ -98,36 +200,39 @@ onMounted(() => {
         </div>
 
         <!--endpointlist-->
-        <div class="row">
+        <div class="row" v-if="endpoints && endpoints.length >0" >
             <div class="col-xs-12 no-side-padding">
                 <div class="row box endpoint-row" v-for="(endpoint, index) in endpoints" :key="index" v-show="endpoints.length" ng-mouseenter="endpoint.hover1=true" ng-mouseleave="endpoint.hover1=false">
                     <div class="col-xs-12 no-side-padding">
                         <div class="row">
                             <div class="col-xs-2 col-xl-7 endpoint-name name-overview">
                                 <div class="row box-header">
-                                    <div class="col-lg-max-8 no-side-padding lead righ-side-ellipsis endpoint-details-link">
-                                        <a ng-click="endpoint.isExpanded = !endpoint.isExpanded" ng-href="{{getDetailsUrl(endpoint)}}" title="{{endpoint.name}}">{{endpoint.name}}</a>
+                                    <div class="col-lg-max-3 no-side-padding lead righ-side-ellipsis endpoint-details-link">
+                                        <a ng-click="endpoint.isExpanded = !endpoint.isExpanded" ng-href="{{getDetailsUrl(endpoint)}}" class="cursorpointer" :title="`${endpoint.name}`">
+                                            {{endpoint.name}}
+                                        </a>
                                     </div>
-                                    <span class="endpoint-count" v-if="endpoint.connectedCount || endpoint.disconnectedCount" title="Endpoint instance(s): {{endpoint.connectedCount || 0}}">{{endpoint.connectedCount || 0}}</span>
-                                    <div class="col-xs-5 no-side-padding endpoint-status">
-                                        <span class="warning" v-if="endpoint.metrics.criticalTime.displayValue < 0">
-                                            <i class="fa pa-warning" title="Warning: endpoint currently has negative critical time, possibly because of a clock drift."></i>
-                                        </span>
-                                        <span class="warning" v-if="endpoint.isScMonitoringDisconnected">
-                                            <i class="fa pa-monitoring-lost endpoints-overview" title="Unable to connect to monitoring server"></i>
-                                        </span>
-                                        <span class="warning" v-if="endpoint.isStale && (!supportsEndpointCount || !endpoint.connectedCount)" title="No data received from any instance">
-                                            <a class="monitoring-lost-link" ng-href="{{getDetailsUrl(endpoint)}}&tab=instancesBreakdown"><i class="fa pa-endpoint-lost endpoints-overview"></i></a>
-                                        </span>
-                                        <span class="warning" v-if="endpoint.errorCount" title="{{endpoint.errorCount | metricslargenumber}} failed messages associated with this endpoint. Click to see list.">
-                                            <a v-if="endpoint.errorCount" class="warning btn" href="/#/failed-messages/group/{{endpoint.serviceControlId}}">
-                                                <i class="fa fa-envelope"></i>
-                                                <span class="badge badge-important ng-binding">{{endpoint.errorCount | metricslargenumber}}</span>
-                                            </a>
-                                        </span>
-                                    </div>
-                                </div>
+                                    <span class="endpoint-count ng-binding ng-scope" v-if="endpoint.connectedCount || endpoint.disconnectedCount" :title="`Endpoint instance(s): ${endpoint.connectedCount || 0} `">({{endpoint.connectedCount || 0}})</span>
+                                            <div class="col-xs-5 no-side-padding endpoint-status">
+                                                <span class="warning" ng-if="endpoint.metrics.criticalTime.displayValue < 0">
+                                                    <i class="fa pa-warning" :title="`Warning: endpoint currently has negative critical time, possibly because of a clock drift.`"></i>
+                                                </span>
+                                                <span class="warning" v-if="endpoint.isScMonitoringDisconnected">
+                                                    <i class="fa pa-monitoring-lost endpoints-overview" :title="`Unable to connect to monitoring server`"></i>
+                                                </span>
+                                                <span class="warning" v-if="endpoint.isStale && (!supportsEndpointCount || !endpoint.connectedCount)" :title="`No data received from any instance`">
+                                                    <a class="monitoring-lost-link" ng-href="{{getDetailsUrl(endpoint)}}&tab=instancesBreakdown"><i class="fa pa-endpoint-lost endpoints-overview"></i></a>
+                                                </span>
+                                                <span class="warning" v-if="endpoint.errorCount" :title="`${endpoint.errorCount | metricslargenumber} failed messages associated with this endpoint. Click to see list.`">
+                                                    <a v-if="endpoint.errorCount" class="warning  cursorpointer" @click="navigateToMessageGroup($event, endpoint.serviceControlId)">
+                                                        <i class="fa fa-envelope"></i>
+                                                        <span class="badge badge-important ng-binding cursorpointer ">{{endpoint.errorCount | metricslargenumber}}</span>
+                                                    </a>
+                                                </span>
+                                            </div>
+</div>
                             </div>
+                            <!--
                             <div class="col-xs-2 col-xl-1 no-side-padding">
                                 <div class="row box-header">
                                     <div class="no-side-padding">
@@ -190,6 +295,7 @@ onMounted(() => {
                                     </div>
                                 </div>
                             </div>
+                            -->
                         </div>
                     </div>
                 </div>
@@ -200,3 +306,215 @@ onMounted(() => {
 </template>
 
 
+<style>
+
+    .cursorpointer {
+        cursor: pointer;
+    }
+.endpoint-row {
+height: 61px; /*//61*/
+position: relative;
+padding: 2px 0 4px;
+}
+.endpoint-name {
+margin-top: 15px;
+}
+
+.endpoint-name > div > div > a {
+text-overflow: ellipsis;
+overflow: hidden;
+white-space: nowrap;
+color: #00729C;
+border-bottom: 1px dotted lightgrey;
+}
+
+.endpoint-name > div > div > a:first-child:hover {
+border-bottom: 1px solid #00729C;
+text-decoration: none !important;
+}
+
+i.fa.pa-endpoint-lost.endpoints-overview, i.fa.pa-monitoring-lost.endpoints-overview {
+position: relative;
+margin-right: 4px;
+}
+
+i.fa.pa-endpoint-lost.endpoints-overview {
+top: 8px;
+}
+.lead.righ-side-ellipsis.endpoint-details-link {
+    color: #00729C !important;
+    cursor: pointer;
+}
+.righ-side-ellipsis {
+    direction: rtl;
+    text-align: left;
+}
+
+@supports (-ms-ime-align:auto) {
+    .righ-side-ellipsis {
+        direction: ltr;
+    }
+}
+@media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+    .righ-side-ellipsis {
+        direction: ltr;
+    }
+}
+
+.endpoint-count {
+color: #777;
+font-weight: normal;
+margin-left: 3px;
+}
+
+
+
+    .pa-monitoring-lost.endpoints-overview {
+        background-image: url('../../a/img/monitoring-lost.svg');
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+
+.pa-warning {
+background-image: url('../../../a/img/warning.svg');
+background-position: center;
+background-repeat: no-repeat;
+width: 20px;
+height: 20px;
+margin-left: 4px;
+padding-top: 24px;
+}
+
+.warning {
+color: red;
+}
+
+.warning i {
+color: #BE0202;
+}
+.ng-binding {
+margin-right: 4px !important;
+}
+.monitoring-lost-link i {
+    top: 7px;
+}
+.pa-endpoint-lost.endpoints-overview {
+    background-image: url('../../a/img/endpoint-lost.svg');
+    background-position: center;
+    background-repeat: no-repeat;
+}
+.endpoint-status {
+    display: inline-block;
+    position: absolute;
+    top: 1px;
+    margin-left: 7px;
+    padding-left: 0;
+}
+
+    .endpoint-status i.fa-envelope, .endpoint-status i.fa-exclamation-triangle {
+        font-size: 20px;
+        color: #CE4844;
+    }
+
+    h1 .endpoint-status i.fa-envelope, .endpoint-status i.fa-exclamation-triangle {
+        font-size: 24px;
+    }
+
+    .endpoint-status i.fa-envelope {
+        color: #777f7f;
+    }
+
+        .endpoint-status i.fa-envelope:hover {
+            color: #23527c;
+        }
+
+    .endpoint-status .badge {
+        position: relative;
+        top: 8px;
+        font-size: 10px;
+        margin-right: 0;
+        left: -10px;
+    }
+
+    .endpoint-status i.fa-envelope, .endpoint-name i.fa-exclamation-triangle {
+        font-size: 20px;
+        margin-left: 6px;
+    }
+
+.endpoint-status a {
+position: relative;
+top: -8px;
+padding-left: 0;
+}
+
+.endpoint-status a:hover {
+    text-decoration: none;
+}
+
+.endpoint-status a[ng-if="endpoint.errorCount"] {
+    top: -11px;
+}
+
+.monitoring-head .endpoint-status {
+    top: 4px;
+}
+
+    .monitoring-head .endpoint-status a {
+        top: 0;
+    }
+
+        .monitoring-head .endpoint-status a[ng-if="endpoint.errorCount"] {
+            top: -5px;
+        }
+
+.monitoring-head .endpoint-status {
+top: 4px;
+}
+
+.monitoring-head .endpoint-status a {
+    top: 0;
+}
+
+    .monitoring-head .endpoint-status a[ng-if="endpoint.errorCount"] {
+        top: -5px;
+    }
+
+.monitoring-head i.fa.fa-envelope {
+    font-size: 26px;
+    position: relative;
+    top: -4px;
+    left: 1px;
+}
+
+.monitoring-head .endpoint-status .badge {
+    position: relative;
+    top: 4px;
+    left: -12px;
+    font-size: 10px;
+}
+
+.endpoint-status .badge {
+    position: relative;
+    top: 2px;
+    left: -9px;
+    font-size: 10px;
+}
+
+.monitoring-head .endpoint-status .pa-endpoint-lost.endpoint-details, .monitoring-head .endpoint-status .pa-monitoring-lost.endpoint-details {
+    width: 32px;
+    height: 30px;
+}
+
+.endpoint-status .pa-endpoint-lost.endpoint-details, .endpoint-status .pa-monitoring-lost.endpoint-details, .endpoint-status .pa-endpoint-lost.endpoints-overview, .endpoint-status .pa-monitoring-lost.endpoints-overview {
+    width: 26px;
+    height: 26px;
+    left: 6px;
+    position: relative;
+}
+
+.endpoint-message-types .endpoint-status {
+    margin-top: -8px;
+}
+
+</style>
