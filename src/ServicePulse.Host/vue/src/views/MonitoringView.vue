4e0bc9102327c7fb76ca3157a42ba814aa1ef0ab
Travis Nickels <travis.nickels@particular.net>
2023-10-12 16:11:59 -0700

    Lint fixes

--- src/ServicePulse.Host/vue/src/views/MonitoringView.vue
+++ src/ServicePulse.Host/vue/src/views/MonitoringView.vue
@@ -1,58 +1,53 @@
 <script setup>
-    import { ref, onMounted } from "vue";
-    import LicenseExpired from "../components/LicenseExpired.vue";
-    import { licenseStatus } from "../composables/serviceLicense.js";
-    import ServiceControlNotAvailable from "../components/ServiceControlNotAvailable.vue";
-    import { connectionState } from "../composables/serviceServiceControl";
-    import { useRedirects } from "../composables/serviceRedirects.js";
-    import EndpointList from "../components/monitoring/EndpointList.vue";
+import { ref, onMounted } from "vue";
+import LicenseExpired from "../components/LicenseExpired.vue";
+import { licenseStatus } from "../composables/serviceLicense.js";
+import ServiceControlNotAvailable from "../components/ServiceControlNotAvailable.vue";
+import { connectionState } from "../composables/serviceServiceControl";
+import { useRedirects } from "../composables/serviceRedirects.js";
+import EndpointList from "../components/monitoring/EndpointList.vue";
 
-    const redirectCount = ref(0);
+const redirectCount = ref(0);
 
-    onMounted(() => {
-        useRedirects().then((result) => {
-            redirectCount.value = result.total;
-        });
-    });
-
+onMounted(() => {
+  useRedirects().then((result) => {
+    redirectCount.value = result.total;
+  });
+});
 </script>
 
-
 <template>
-
-    <LicenseExpired />
-    <template v-if="!licenseStatus.isExpired">
-        <div class="container">
-            <ServiceControlNotAvailable />
-            <template v-if="connectionState.connected">
-
-                <div class="row">
-                    <div class="col-6 list-section">
-                        <h1>Endpoints Overview</h1>
-                    </div>
-                    <!--filters-->
-                    <div class="col-6 toolbar-menus no-side-padding">
-                        <div class="msg-group-menu dropdown">
-                            <label class="control-label">Group by:</label>
-                            <button type="button" class="btn btn-default dropdown-toggle sp-btn-menu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                selectedClassifier
-                                <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a> classifier1</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <!--List of endpoints-->
-                <EndpointList></EndpointList>
-
-            </template>
+  <LicenseExpired />
+  <template v-if="!licenseStatus.isExpired">
+    <div class="container">
+      <ServiceControlNotAvailable />
+      <template v-if="connectionState.connected">
+        <div class="row">
+          <div class="col-6 list-section">
+            <h1>Endpoints Overview</h1>
+          </div>
+          <!--filters-->
+          <div class="col-6 toolbar-menus no-side-padding">
+            <div class="msg-group-menu dropdown">
+              <label class="control-label">Group by:</label>
+              <button type="button" class="btn btn-default dropdown-toggle sp-btn-menu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                selectedClassifier
+                <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  <a> classifier1</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    </template>
+
+        <!--List of endpoints-->
+        <EndpointList></EndpointList>
+      </template>
+    </div>
+  </template>
 </template>
 <style>
 .form-control-static {
@@ -178,4 +173,3 @@
   margin-left: 72px;
 }
 </style>
-
