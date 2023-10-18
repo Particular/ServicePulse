6610a2ef877d6e936bc5cda3a75f6c210d061b90
Travis Nickels <travis.nickels@particular.net>
2023-10-18 15:18:09 -0700

    Add group by endpoint component for monitoring (#1585)

--- src/ServicePulse.Host/vue/src/views/MonitoringView.vue
+++ src/ServicePulse.Host/vue/src/views/MonitoringView.vue
@@ -1,47 +1,153 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import { licenseStatus } from "./../composables/serviceLicense.js";
 import LicenseExpired from "../components/LicenseExpired.vue";
+import GroupBy from "../components/monitoring/MonitoringGroupBy.vue";
+import EndpointListGrouped from "../components/monitoring/EndpointListGrouped.vue";
+import ServiceControlNotAvailable from "../components/ServiceControlNotAvailable.vue";
+import { connectionState } from "../composables/serviceServiceControl";
+import { useRedirects } from "../composables/serviceRedirects.js";
 import EndpointList from "../components/monitoring/EndpointList.vue";
+import { useFetchFromMonitoring, useIsMonitoringDisabled } from "../composables/serviceServiceControlUrls";
+import { monitoringConnectionState } from "../composables/serviceServiceControl";
+import { useGetExceptionGroups } from "../composables/serviceMessageGroup.js";
+
+const redirectCount = ref(0);
+const endpoints = ref([]);
+const grouping = ref([]);
+const exceptionGroups = ref([]);
+const historyPeriod = 1;
+var hasData = ref(false);
+var supportsEndpointCount = ref();
+
+function getAllMonitoredEndpoints() {
+  if (!useIsMonitoringDisabled() && !monitoringConnectionState.unableToConnect) {
+    return useFetchFromMonitoring(`${`monitored-endpoints`}?history=${historyPeriod}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        endpoints.value = [];
+        endpoints.value = data;
+        updateUI();
+        getEndpointsFromScSubscription();
+      });
+  } else {
+    getEndpointsFromScSubscription();
+  }
+}
+
+function getEndpointsFromScSubscription() {
+  return useGetExceptionGroups("Endpoint Name").then((result) => {
+    exceptionGroups.value = [];
+    exceptionGroups.value = result;
+    //Squash and add to existing monitored endpoints
+
+    if (exceptionGroups.value.length > 0) {
+      //sort the exceptiongroup list by name - case sensitive
+      exceptionGroups.value.sort((a, b) => (a.title > b.title ? 1 : a.title < b.title ? -1 : 0)); //desc
+      exceptionGroups.value.forEach((failedMessageEndpoint) => {
+        if (failedMessageEndpoint.operation_status === "ArchiveCompleted") {
+          return;
+        }
+        var index = endpoints.value.findIndex(function (item) {
+          return item.name === failedMessageEndpoint.title;
+        });
+        if (index >= 0) {
+          endpoints.value[index].serviceControlId = failedMessageEndpoint.id;
+          endpoints.value[index].errorCount = failedMessageEndpoint.count;
+        } else {
+          endpoints.value.push({ name: failedMessageEndpoint.title, errorCount: failedMessageEndpoint.count, serviceControlId: failedMessageEndpoint.id, isScMonitoringDisconnected: true });
+        }
+      });
+    }
+  });
+}
+
+function updateUI() {
+  if (endpoints.value.length > 0) {
+    endpoints.value.forEach((endpoint) => {
+      hasData.value = !endpoint.empty;
+      supportsEndpointCount.value = Object.prototype.hasOwnProperty.call(endpoint, "connectedCount"); ////$scope.supportsEndpointCount = Object.prototype.hasOwnProperty.call(endpoint, 'connectedCount');
+      if (endpoint.empty) {
+        return;
+      }
+
+      if (endpoint.error) {
+        // connectivityNotifier.reportFailedConnection();
+        if (endpoints.value) {
+          endpoints.value.forEach((item) => (item.isScMonitoringDisconnected = true));
+        }
+      } else {
+        // connectivityNotifier.reportSuccessfulConnection();
+        var index = endpoints.value.findIndex(function (item) {
+          return item.name === endpoint.name;
+        });
+        endpoint.isScMonitoringDisconnected = false;
+        if (index >= 0) {
+          mergeIn(endpoints.value[index], endpoint);
+        } else {
+          endpoints.value.push(endpoint);
+        }
+      }
+    });
+
+    //sort the monitored endpoints by name - case sensitive
+    endpoints.value.sort((a, b) => (a.name < b.name ? 1 : a.name > b.name ? -1 : 0));
+  }
+}
+
+function mergeIn(destination, source) {
+  for (var propName in source) {
+    if (Object.prototype.hasOwnProperty.call(source, propName)) {
+      destination[propName] = source[propName];
+    }
+  }
+}
+
+function updateGroupedEndpointList(endpointGrouping) {
+  grouping.value = endpointGrouping;
+}
+
+onMounted(() => {
+  useRedirects().then((result) => {
+    redirectCount.value = result.total;
+  });
+  getAllMonitoredEndpoints();
+});
 </script>
 
 <template>
   <LicenseExpired />
   <template v-if="!licenseStatus.isExpired">
     <div class="container monitoring-view">
-      <div class="row monitoring-head">
-        <div class="col-sm-4 no-side-padding list-section">
-          <h1>Endpoints overview</h1>
-        </div>
-        <div class="col-sm-8 no-side-padding toolbar-menus">
-          <div class="filter-group filter-monitoring">
-            <ul class="nav nav-pills period-selector">
-              <li><a href="#">1m</a></li>
-              <li><a href="#">5m</a></li>
-              <li><a href="#">10m</a></li>
-              <li><a href="#">15m</a></li>
-              <li><a href="#">30m</a></li>
-              <li><a href="#">1h</a></li>
-            </ul>
-            <div class="dropdown">
-              <label class="control-label">Group by:</label>
-              <button type="button" class="btn dropdown-toggle sp-btn-menu" id="dropdownMenu1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                button
-                <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                <li><a href="#">no grouping</a></li>
-                <li role="separator" class="divider"></li>
-                <li>
-                  <a href="#">max.segments</a>
-                </li>
+      <ServiceControlNotAvailable />
+      <template v-if="connectionState.connected">
+        <div class="row monitoring-head">
+          <div class="col-sm-4 no-side-padding list-section">
+            <h1>Endpoints overview</h1>
+          </div>
+          <!--filters-->
+          <div class="col-sm-8 no-side-padding toolbar-menus">
+            <div class="filter-group filter-monitoring">
+              <ul class="nav nav-pills period-selector">
+                <li><a href="#">1m</a></li>
+                <li><a href="#">5m</a></li>
+                <li><a href="#">10m</a></li>
+                <li><a href="#">15m</a></li>
+                <li><a href="#">30m</a></li>
+                <li><a href="#">1h</a></li>
               </ul>
+              <GroupBy v-if="endpoints.length" :endpoints="endpoints" @group-selector="updateGroupedEndpointList" />
+              <input type="text" placeholder="Filter by name..." class="form-control-static filter-input" />
             </div>
-            <input type="text" placeholder="Filter by name..." class="form-control-static filter-input" />
           </div>
         </div>
-      </div>
-      <!--List of endpoints-->
-      <EndpointList></EndpointList>
+        <!--List of endpoints-->
+        <EndpointList v-if="grouping.length === 0"></EndpointList>
+        <!--Grouped list of endpoints-->
+        <EndpointListGrouped v-if="grouping.selectedGrouping !== undefined && grouping.selectedGrouping > 0" :grouping="grouping"></EndpointListGrouped>
+      </template>
     </div>
   </template>
 </template>
