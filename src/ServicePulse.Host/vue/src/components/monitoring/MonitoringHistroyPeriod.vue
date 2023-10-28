1a15a90feaa45b797b4f821b416d25a86e862625
Travis Nickels <travis.nickels@particular.net>
2023-10-27 21:52:49 -0700

    Fix monitoring History period component

--- src/ServicePulse.Host/vue/src/components/monitoring/MonitoringHistroyPeriod.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/MonitoringHistroyPeriod.vue
@@ -1,22 +1,49 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { getAllPeriods, saveSelectedPeriod, useGetDefaultPeriod } from "../../composables/serviceHistoryPeriods.js";
 
 const emit = defineEmits(["period-selected"]);
 const allPeriods = getAllPeriods();
 const defaultPeriod = ref(useGetDefaultPeriod());
+const router = useRouter();
+const route = useRoute();
+
+watch(defaultPeriod, () => {
+  const queryParameters = { ...route.query };
+  router.push({ query: { ...queryParameters, historyPeriod: defaultPeriod.value.value } });
+});
 
 function selectHistoryPeriod(period) {
   saveSelectedPeriod(period);
   defaultPeriod.value = useGetDefaultPeriod();
-  emit("period-selected");
+  emit("period-selected", period);
 }
+
+function checkPermaLink() {
+  if (route.query.historyPeriod !== undefined) {
+    const historyPeriodParam = parseInt(route.query.historyPeriod);
+    const historyPeriod = allPeriods.find((period) => {
+      return period.value === historyPeriodParam;
+    });
+    if (historyPeriod !== undefined) {
+      selectHistoryPeriod(historyPeriod);
+    } else {
+      router.push({ query: { historyPeriod: 1 } });
+      selectHistoryPeriod(allPeriods.indexOf(0));
+    }
+  }
+}
+
+onMounted(() => {
+  checkPermaLink();
+});
 </script>
 
 <template>
   <ul class="nav nav-pills period-selector" v-for="period in allPeriods" :key="period.value">
     <li role="presentation" :class="{ active: period.value === defaultPeriod.value, notselected: period.value !== defaultPeriod.value }">
-      <a :href="`#/monitoring?historyPeriod=` + period.value" @click="selectHistoryPeriod(period)">{{ period.text }}</a>
+      <a :href="`#`" @click.prevent="selectHistoryPeriod(period)">{{ period.text }}</a>
     </li>
   </ul>
 </template>
