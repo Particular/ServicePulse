fa52d0af9b177355865e0a699377bf9d9d17c30c
Travis Nickels <travis.nickels@particular.net>
2023-10-27 21:53:47 -0700

    Fix the monitoring groupBy component

--- src/ServicePulse.Host/vue/src/components/monitoring/MonitoringGroupBy.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/MonitoringGroupBy.vue
@@ -1,91 +1,57 @@
 <script setup>
-import { ref, watch } from "vue";
+import { ref, onMounted } from "vue";
+import { useCookies } from "vue3-cookies";
+import { useFindEndpointSegments, useGroupEndpoints } from "../../composables/serviceMonitoringEndpoints";
+
+const cookies = useCookies().cookies;
 
 const emit = defineEmits(["group-selector"]);
 const settings = defineProps({
   endpoints: Object,
 });
+
 const endpoints = ref(settings.endpoints);
 
 const grouping = ref({
   groupedEndpoints: [],
-  groupSegments: findSegments(endpoints),
+  groupSegments: useFindEndpointSegments(endpoints),
   selectedGrouping: 0,
-});
-
-watch(endpoints, async () => {
-  grouping.value.groupSegments = findSegments(endpoints);
-  grouping.value.groupedEndpoints = groupEndpoints(endpoints, grouping.value.selectedGrouping);
 });
 
 function selectGroup(groupSize) {
   grouping.value.selectedGrouping = groupSize;
-  grouping.value.groupedEndpoints = groupEndpoints(endpoints, groupSize);
-  emit("group-selector", grouping.value);
+  saveSelectedGroup(groupSize);
+  grouping.value.groupedEndpoints = useGroupEndpoints(endpoints, groupSize);
+  emit("group-selector", grouping);
 }
 
-function groupEndpoints(endpoints, numberOfSegments) {
-  let groups = new Map();
-  if (endpoints.value === undefined) return;
-  endpoints.value.forEach(function (element) {
-    const grouping = parseEndpoint(element, numberOfSegments);
-
-    let resultGroup = groups.get(grouping.groupName);
-    if (!resultGroup) {
-      resultGroup = {
-        group: grouping.groupName,
-        endpoints: [],
-      };
-      groups.set(grouping.groupName, resultGroup);
-    }
-    resultGroup.endpoints.push(grouping);
-  });
-  return [...groups.values()];
+function saveSelectedGroup(groupSize) {
+  cookies.set(`selected_group_size`, groupSize);
 }
 
-function findSegments(endpoints) {
-  if (endpoints.value !== undefined) {
-    return endpoints.value.reduce(function (acc, cur) {
-      return Math.max(acc, cur.name.split(".").length - 1);
-    }, 0);
+function getDefaultSelectedGroup() {
+  const storedGroupSelection = cookies.get("selected_group_size");
+  if (storedGroupSelection !== "undefined") {
+    selectGroup(parseInt(storedGroupSelection));
+  } else {
+    selectGroup(0);
   }
-  return 0;
 }
 
-function parseEndpoint(endpoint, maxGroupSegments) {
-  if (maxGroupSegments === 0) {
-    return {
-      groupName: "Ungrouped",
-      shortName: endpoint.name,
-      endpoint: endpoint,
-    };
-  }
-
-  const segments = endpoint.name.split(".");
-  const groupSegments = segments.slice(0, maxGroupSegments);
-  const endpointSegments = segments.slice(maxGroupSegments);
-  if (endpointSegments.length === 0) {
-    // the endpoint's name is shorter than the group size
-    return parseEndpoint(endpoint, maxGroupSegments - 1);
-  }
-
-  return {
-    groupName: groupSegments.join("."),
-    shortName: endpointSegments.join("."),
-    endpoint: endpoint,
-  };
-}
+onMounted(() => {
+  getDefaultSelectedGroup();
+});
 </script>
 
 <template>
   <div class="dropdown">
     <label class="control-label">Group by:</label>
     <button type="button" class="btn dropdown-toggle sp-btn-menu" id="dropdownMenu1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      {{ grouping.selectedGrouping == 0 ? "no grouping" : "max. " + grouping.selectedGrouping + " segments" }}
+      {{ grouping.selectedGrouping === 0 ? "no grouping" : "max. " + grouping.selectedGrouping + " segments" }}
       <span class="caret"></span>
     </button>
     <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-      <li><a href="#">no grouping</a></li>
+      <li><a href="#" @click.prevent="selectGroup(0)">no grouping</a></li>
       <li role="separator" class="divider"></li>
       <li v-for="segment in grouping.groupSegments" :key="segment">
         <a href="#" @click.prevent="selectGroup(segment)">max. {{ segment }} segments</a>
