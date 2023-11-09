882501b9077ab430e987be816c7fcaf1d5f6db5f
Christian <christian.quiros@particular.net>
2023-11-09 21:08:54 +0000

    Setup storybook (#1604)
    * Setting up storybook

---
+++ src/ServicePulse.Host/vue/.storybook/preview.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/vue3').Preview } */
+
+import '../src/assets/main.css';
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
