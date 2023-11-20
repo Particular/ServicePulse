47bde068b9efb790b05c5edb59153a1310c08bc5
Jayanthi <jayanthi.sourirajan@particular.net>
2023-11-20 15:37:16 -0800

    comment out code for average label

--- src/ServicePulse.Host/vue/src/components/monitoring/D3LargeGraph.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/D3LargeGraph.vue
@@ -28,141 +28,138 @@ const root = ref(null);
 //var averageLabelToTheLeft = ArrowLabel({ pointToTheLeft: true, caption: 'AVG' });
 
 function displayGraphValues() {
-    var avgDecimals = avgDecimals || averageDecimalsDefault;
-    var metricSuffix = metricSuffix || avgLabelSuffixDefault;
-    //attrs.metricSuffix = attrs.metricSuffix || avgLabelSuffixDefault;
+var avgDecimals = avgDecimals || averageDecimalsDefault;
+var metricSuffix = metricSuffix || avgLabelSuffixDefault;
+
+    var svg = root.value.getElementsByTagName("svg")[0];
+
+    d3.select(svg).selectAll('*').remove();
+
+    var topMargin = 10;
+    var bottomMargin = 5;
+    var leftMargin = 60;
+
+    var chart = d3.select(svg)
+        .attr('width', props.plotwidth)
+        .attr('height', props.plotheight);
+
+    var width = svg.clientWidth;
+    var height = svg.clientHeight;
+
+    //HINT: This is workaround for Firefox
+    if (width === 0) {
+        var box = svg.getBoundingClientRect();
+
+        width = box.right - box.left;
+        height = box.bottom - box.top;
+    }
+
+    var firstSeries = props.firstdataseries;
+    var secondSeries = props.seconddataseries;
+    var firstSeriesColor = props.firstseriescolor;
+    var firstSeriesFillColor = props.firstseriesfillcolor;
+    var secondSeriesColor = props.secondseriescolor;
+    var secondSeriesFillColor = props.secondseriesfillcolor;
+
+    var amountOfValues = Math.max(firstSeries.points.length, secondSeries ? secondSeries.points.length : 0) || 10;
+    if (firstSeries.points.length === 0) {
+        firstSeries.points = new Array(amountOfValues).fill(0);
+    }
+    var scaleX = d3.scaleLinear()
+        .domain([0, amountOfValues - 1])
+        .range([leftMargin, width]);
+
+    chart.append('rect')
+        .attr('width', width - leftMargin)
+        .attr('height', height - topMargin - bottomMargin)
+        .attr('transform', 'translate(' + leftMargin + ',' + topMargin + ')')
+        .attr('fill', '#F2F6F7');
+
+    var minimumYaxis = !isNaN(props.minimumyaxis) ? Number(props.minimumyaxis) : 10;
+    var max = Math.max(firstSeries.average, firstSeries.points.length > 0 ? d3.max(firstSeries.points) : 0, minimumYaxis);
+
+    if (secondSeries && secondSeries.points.length > 0) {
+        max = Math.max(max, secondSeries.average, d3.max(secondSeries.points));
+    }
+
+    max = padToWholeValue(max);
+
+    var scaleY = d3.scaleLinear()
+        .domain([0, max])
+        .range([height - bottomMargin, topMargin]);
+
+    var yAxis = d3.axisLeft(scaleY)
+        .tickValues([0, max * 1 / 4, max * 1 / 2, max * 3 / 4, max]);
+
+    if (props.isdurationgraph) {
+        yAxis = yAxis.tickFormat(function (v) {
+            var formattedTime = useFormatTime(v);
+            return formattedTime.value + '  ' + formattedTime.unit;
+        });
+    }
+
+    chart.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(' + leftMargin + ', 0)')
+        .call(function (g) {
+            g.call(yAxis);
+            g.select('.domain').remove();
+            g.selectAll('.tick line').attr('stroke', 'black').attr('stroke-width', '1.75').attr('opacity', 0.1).attr('x', 0).attr('x2', width - leftMargin);
+            g.selectAll('.tick text').attr('x', -4).attr('fill', '#828282');
+        });
+
+    var drawSeries = function (data, lineColor, fillColor) {
+        drawDataSeries(chart, data, lineColor, fillColor, scaleX, scaleY);
+    }
+
+    var drawAverage = function (data, lineColor, fillColor) {
+        return drawAverageLine(chart, data, lineColor, fillColor, scaleX, scaleY);
+    }
 
 
 
-        var svg = root.value.getElementsByTagName("svg")[0];
+    drawSeries(firstSeries, firstSeriesColor, firstSeriesFillColor);
 
-        d3.select(svg).selectAll('*').remove();
+    if (secondSeries) {
+        drawSeries(secondSeries, secondSeriesColor, secondSeriesFillColor);
+    }
 
-        var topMargin = 10;
-        var bottomMargin = 5;
-        var leftMargin = 60;
+    var firstAverageLine = drawAverage(firstSeries, firstSeriesColor, firstSeriesFillColor);
 
-        var chart = d3.select(svg)
-            .attr('width', props.plotwidth)
-            .attr('height', props.plotheight);
+    var secondAverageLine = null;
 
-        var width = svg.clientWidth;
-        var height = svg.clientHeight;
+    if (secondSeries) {
+        secondAverageLine = drawAverage(secondSeries, secondSeriesColor, secondSeriesFillColor);
+    }
 
-        //HINT: This is workaround for Firefox
-        if (width === 0) {
-            var box = svg.getBoundingClientRect();
-
-            width = box.right - box.left;
-            height = box.bottom - box.top;
-        }
-
-        var firstSeries = props.firstdataseries;
-        var secondSeries = props.seconddataseries;
-        var firstSeriesColor = props.firstseriescolor;
-        var firstSeriesFillColor = props.firstseriesfillcolor;
-        var secondSeriesColor = props.secondseriescolor;
-        var secondSeriesFillColor = props.secondseriesfillcolor;
-
-        var amountOfValues = Math.max(firstSeries.points.length, secondSeries ? secondSeries.points.length : 0) || 10;
-        if (firstSeries.points.length === 0) {
-            firstSeries.points = new Array(amountOfValues).fill(0);
-        }
-        var scaleX = d3.scaleLinear()
-            .domain([0, amountOfValues - 1])
-            .range([leftMargin, width]);
-
-        chart.append('rect')
-            .attr('width', width - leftMargin)
-            .attr('height', height - topMargin - bottomMargin)
-            .attr('transform', 'translate(' + leftMargin + ',' + topMargin + ')')
-            .attr('fill', '#F2F6F7');
-
-        var minimumYaxis = !isNaN(props.minimumyaxis) ? Number(props.minimumyaxis) : 10;
-        var max = Math.max(firstSeries.average, firstSeries.points.length > 0 ? d3.max(firstSeries.points) : 0, minimumYaxis);
-
-        if (secondSeries && secondSeries.points.length > 0) {
-            max = Math.max(max, secondSeries.average, d3.max(secondSeries.points));
-        }
-
-        max = padToWholeValue(max);
-
-        var scaleY = d3.scaleLinear()
-            .domain([0, max])
-            .range([height - bottomMargin, topMargin]);
-
-        var yAxis = d3.axisLeft(scaleY)
-            .tickValues([0, max * 1 / 4, max * 1 / 2, max * 3 / 4, max]);
+    chart.on("mouseover", function () {
+        var value = useFormatLargeNumber(firstSeries.average, avgDecimals);
+        var suffix = metricSuffix;
 
         if (props.isdurationgraph) {
-            yAxis = yAxis.tickFormat(function (v) {
-                var formattedTime = useFormatTime(v);
-                return formattedTime.value + '  ' + formattedTime.unit;
-            });
+            value = useFormatTime(firstSeries.average).value;
+            suffix = useFormatTime(firstSeries.average).unit.toUpperCase();
         }
 
-        chart.append('g')
-            .attr('class', 'y axis')
-            .attr('transform', 'translate(' + leftMargin + ', 0)')
-            .call(function (g) {
-                g.call(yAxis);
-                g.select('.domain').remove();
-                g.selectAll('.tick line').attr('stroke', 'black').attr('stroke-width', '1.75').attr('opacity', 0.1).attr('x', 0).attr('x2', width - leftMargin);
-                g.selectAll('.tick text').attr('x', -4).attr('fill', '#828282');
-            });
+       // displayAverageLabel(firstAverageLine, averageLabelToTheRight, value, firstSeriesColor, suffix);
 
-        var drawSeries = function (data, lineColor, fillColor) {
-            drawDataSeries(chart, data, lineColor, fillColor, scaleX, scaleY);
-        }
-
-        var drawAverage = function (data, lineColor, fillColor) {
-            return drawAverageLine(chart, data, lineColor, fillColor, scaleX, scaleY);
-        }
-
-
-
-        drawSeries(firstSeries, firstSeriesColor, firstSeriesFillColor);
-
-        if (secondSeries) {
-            drawSeries(secondSeries, secondSeriesColor, secondSeriesFillColor);
-        }
-
-        var firstAverageLine = drawAverage(firstSeries, firstSeriesColor, firstSeriesFillColor);
-
-        var secondAverageLine = null;
-
-        if (secondSeries) {
-            secondAverageLine = drawAverage(secondSeries, secondSeriesColor, secondSeriesFillColor);
-        }
-
-        chart.on("mouseover", function () {
-            var value = useFormatLargeNumber(firstSeries.average, avgDecimals);
-            var suffix = metricSuffix;
-
+        if (secondAverageLine && secondSeries.points.length > 0) {
+            value = useFormatLargeNumber(secondSeries.average, avgDecimals);
             if (props.isdurationgraph) {
-                value = useFormatTime(firstSeries.average).value;
-                suffix = useFormatTime(firstSeries.average).unit.toUpperCase();
+                value = useFormatTime(secondSeries.average).value;
+                suffix = useFormatTime(secondSeries.average).unit.toUpperCase();
             }
 
-            displayAverageLabel(firstAverageLine, averageLabelToTheRight, value, attrs.firstSeriesColor, suffix);
+            //displayAverageLabel(secondAverageLine, averageLabelToTheLeft, value, secondSeriesColor, suffix);
+        }
+    })
+        .on("mouseout", function () {
+            //averageLabelToTheRight.hide();
+            //averageLabelToTheLeft.hide();
+        });
+};
 
-            if (secondAverageLine && secondSeries.points.length > 0) {
-                value = useFormatLargeNumber(secondSeries.average, avgDecimals);
-                if (props.isdurationgraph) {
-                    value = useFormatTime(secondSeries.average).value;
-                    suffix = useFormatTime(secondSeries.average).unit.toUpperCase();
-                }
-
-                displayAverageLabel(secondAverageLine, averageLabelToTheLeft, value, attrs.secondSeriesColor, suffix);
-            }
-        })
-            .on("mouseout", function () {
-                //averageLabelToTheRight.hide();
-                //averageLabelToTheLeft.hide();
-            });
-    };
-
-function displayAverageLabel(averageLine, label, value, color, unit) {
+//function displayAverageLabel(averageLine, label, value, color, unit) {
     //var { x, y, width } = averageLine.node().getBoundingClientRect();
     //label.value(value, unit);
 
@@ -171,7 +168,7 @@ function displayAverageLabel(averageLine, label, value, color, unit) {
     //} else {
     //    label.displayAt({ x: x + window.pageXOffset, y: y + window.pageYOffset, color });
     //}
-}
+//}
 
 function drawDataSeries(chart, data, color, fillColor, scaleX, scaleY) {
 
