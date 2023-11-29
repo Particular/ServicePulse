094720340650d6096615b18b816570b9bcadccf7
Jayanthi <jayanthi.sourirajan@particular.net>
2023-11-29 08:07:52 -0800

    align the buttons to the endpoint name

--- src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
@@ -152,13 +152,13 @@ function formatGraphDecimal(input, deci) {
         <div class="endpoint-row" v-for="(endpoint, index) in endpoints" :key="index" v-show="endpoints.length" v-on:mouseenter="endpoint.hover1 = true" v-on:mouseleave="endpoint.hover1 = false">
               <div class="table-first-col endpoint-name name-overview">
                 <div class="box-header">
-                  <div class="col-lg-max-8 no-side-padding lead righ-side-ellipsis endpoint-details-link">
-                    <a @click="navigateToEndpointDetails($event, endpoint.name)" class="cursorpointer" v-tooltip :title="endpoint.name">
-                      {{ endpoint.name }}
-                    </a>
-                  </div>
-                  <span class="endpoint-count ng-binding ng-scope" v-if="endpoint.connectedCount || endpoint.disconnectedCount" v-tooltip :title="`Endpoint instance(s):` + endpoint.connectedCount || 0">({{ endpoint.connectedCount || 0 }})</span>
-                  <div class="col-xs-5 no-side-padding endpoint-status">
+                    <div class="col-lg-max-8 no-side-padding lead righ-side-ellipsis endpoint-details-link">
+                        <a @click="navigateToEndpointDetails($event, endpoint.name)" class="cursorpointer" v-tooltip :title="endpoint.name">
+                            {{ endpoint.name }}
+                        </a>
+                        <span class="endpoint-count" v-if="endpoint.connectedCount || endpoint.disconnectedCount" v-tooltip :title="`Endpoint instance(s):` + endpoint.connectedCount || 0">({{ endpoint.connectedCount || 0 }})</span>
+                    </div>
+                  <div class="col-xs-5 no-side-padding endpoint-status" >
                     <span class="warning" v-if="endpoint.metrics != null && formatGraphDuration(endpoint.metrics.criticalTime).value < 0">
                       <i class="fa pa-warning" v-tooltip title="Warning: endpoint currently has negative critical time, possibly because of a clock drift."></i>
                     </span>
