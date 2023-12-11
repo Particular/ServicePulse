897f90d10744f64a7a4ea55d669373b35ac22744
Travis Nickels <travis.nickels@particular.net>
2023-12-11 12:23:33 -0800

    Add Pinia store to monitoring view
    Add comment back to v-tooltip directive
    
    Remove commented code

--- src/ServicePulse.Host/vue/src/App.vue
+++ src/ServicePulse.Host/vue/src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { RouterView } from "vue-router";
-import PageFooter from "./components/PageFooter.vue";
-import PageHeader from "./components/PageHeader.vue";
 import { useServiceControlUrls } from "./composables/serviceServiceControlUrls.js";
 import { useServiceControlVersion, useServiceControl } from "./composables/serviceServiceControl.js";
 import { useLicense } from "./composables/serviceLicense.js";
+import PageFooter from "./components/PageFooter.vue";
+import PageHeader from "./components/PageHeader.vue";
 import "@popperjs/core";
 import "bootstrap";
 
