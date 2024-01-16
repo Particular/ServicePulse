1391783a857174d961b59e3c71f47db17b6f3e0b
Travis Nickels <travis.nickels@particular.net>
2024-01-16 15:34:48 -0800

    Fix hydration mismatch

--- src/ServicePulse.Host/vue/vite.config.js
+++ src/ServicePulse.Host/vue/vite.config.js
@@ -8,6 +8,11 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_OPTIONS_API__: "false",
+    __VUE_PROD_DEVTOOLS__: "false",
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
+  },
   css: {
     devSourcemap: true,
   },
