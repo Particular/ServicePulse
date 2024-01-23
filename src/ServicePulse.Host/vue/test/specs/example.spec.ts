c8fbf79bacda72a56937917c83ff10730e8c3b6a
Christian <christian.quiros+github@gmail.com>
2024-01-22 20:50:31 -0600

    Dummy test to check faking API works with vitest and playwright

--- src/ServicePulse.Host/vue/test/specs/example.spec.ts
+++ src/ServicePulse.Host/vue/test/specs/example.spec.ts
@@ -3,3 +3,11 @@ it("should work with both vitest and playwright", async ({ driver }) => {
 	await driver.goTo("checking-testing-drivers-implementations");
 	await driver.findByText("Working!").shouldBeVisible();
 });
+
+it("should do something useful", async ({ driver }) => {
+	driver.mockEndpoint("/todos/1", {
+		body: { userId: 1, id: 1, title: "Todo 1", completed: false },
+	});
+	await driver.goTo("/todos/1");
+	// ...
+});
