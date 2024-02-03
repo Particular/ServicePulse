8379e011a1ce05bd19d210f34830d3c33ce8e2ca
Travis Nickels <travis.nickels@particular.net>
2024-02-03 01:50:10 -0800

    Added sort icons change image direction on click
    # Conflicts:
    #	src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue

--- src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
@@ -7,6 +7,11 @@ import { useMonitoringStore } from "../../stores/MonitoringStore";
 const monitoringStore = useMonitoringStore();
 const endpoints = ref();
 const isGrouped = computed(() => monitoringStore.endpointListIsGrouped);
+const activeColumn = ref("");
+
+function toggleSort(name) {
+  activeColumn.value = name;
+}
 
 watch([() => monitoringStore.endpointList, () => monitoringStore.filterString], async () => {
   if (monitoringStore.endpointListIsFiltered) {
@@ -30,24 +35,22 @@ onMounted(async () => {
     <!--Table headings-->
     <div class="table-head-row">
       <div class="table-first-col">
-        <endpoint-list-sortable-column>Endpoint name</endpoint-list-sortable-column>
+        <endpoint-list-sortable-column :is-active="activeColumn === 'endpoints'" @click="toggleSort('endpoints')">Endpoint name</endpoint-list-sortable-column>
       </div>
       <div class="table-col">
-        <endpoint-list-sortable-column v-tooltip title="Queue length: The number of messages waiting to be processed in the input queue(s) of the endpoint.">Queue Length<template #unit>(MSGS)</template></endpoint-list-sortable-column>
+        <endpoint-list-sortable-column :is-active="activeColumn === 'queue'" @click="toggleSort('queue')" v-tooltip title="Queue length: The number of messages waiting to be processed in the input queue(s) of the endpoint.">Queue Length<template #unit>(MSGS)</template></endpoint-list-sortable-column>
       </div>
       <div class="table-col">
-        <endpoint-list-sortable-column v-tooltip title="Throughput: The number of messages per second successfully processed by a receiving endpoint.">Throughput<template #unit>(msgs/s)</template></endpoint-list-sortable-column>
+        <endpoint-list-sortable-column :is-active="activeColumn === 'throughput'" @click="toggleSort('throughput')" v-tooltip title="Throughput: The number of messages per second successfully processed by a receiving endpoint.">Throughput<template #unit>(msgs/s)</template></endpoint-list-sortable-column>
       </div>
       <div class="table-col">
-        <endpoint-list-sortable-column v-tooltip title="Scheduled retries: The number of messages per second scheduled for retries (immediate or delayed).">Scheduled retries <template #unit>(msgs/s)</template></endpoint-list-sortable-column>
+        <endpoint-list-sortable-column :is-active="activeColumn === 'scheduled'" @click="toggleSort('scheduled')" v-tooltip title="Scheduled retries: The number of messages per second scheduled for retries (immediate or delayed).">Scheduled retries <template #unit>(msgs/s)</template></endpoint-list-sortable-column>
       </div>
       <div class="table-col">
-        <endpoint-list-sortable-column v-tooltip title="Processing time: The time taken for a receiving endpoint to successfully process a message."> Processing Time <template #unit>(t)</template></endpoint-list-sortable-column>
+        <endpoint-list-sortable-column :is-active="activeColumn === 'processing'" @click="toggleSort('processing')" v-tooltip title="Processing time: The time taken for a receiving endpoint to successfully process a message."> Processing Time <template #unit>(t)</template></endpoint-list-sortable-column>
       </div>
       <div class="table-col">
-        <endpoint-list-sortable-column v-tooltip title="Critical time: The elapsed time from when a message was sent, until it was successfully processed by a receiving endpoint.">
-          Critical Time <template #unit>(t)</template></endpoint-list-sortable-column
-        >
+        <endpoint-list-sortable-column :is-active="activeColumn === 'critical'" @click="toggleSort('critical')" v-tooltip title="Critical time: The elapsed time from when a message was sent, until it was successfully processed by a receiving endpoint."> Critical Time <template #unit>(t)</template></endpoint-list-sortable-column>
       </div>
     </div>
     <div>
--- src/ServicePulse.Host/vue/src/components/monitoring/EndpointListSortableColumn.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/EndpointListSortableColumn.vue
@@ -1,19 +1,71 @@
 <script setup>
-defineProps({
+import { ref, computed } from "vue";
+const props = defineProps({
   unit: String,
+  isActive: Boolean,
 });
+
+const isActive = computed(() => props.isActive);
+const isAscending = ref(false);
+const sortIcon = ref("sort-down");
+
+function toggleSort() {
+  isAscending.value = isActive.value ? !isAscending.value : false;
+  sortIcon.value = isAscending.value ? "sort-up" : "sort-down";
+}
 </script>
 <template>
   <div class="box-header">
-    <button ng-click="vm.toggleSort()">
+    <button @click="toggleSort" class="column-header-button">
       <span>
-        <slot></slot> &nbsp;
+        <slot></slot>
         <span class="table-header-unit"><slot name="unit"></slot></span>
       </span>
       <div>
-        <!-- <i class="fa fa-sort-down" aria-hidden="true"></i> -->
-        <!-- <i class="bi bi-sort-down"></i> -->
+        <i v-if="isActive" :class="sortIcon"></i>
       </div>
     </button>
   </div>
 </template>
+
+<style scoped>
+.column-header-button {
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+}
+
+.column-header-button span {
+  text-transform: uppercase;
+}
+
+.column-header-button:hover span {
+  text-decoration: underline;
+}
+
+.column-header-button div {
+  display: inline-block;
+  margin-top: -1px;
+}
+
+.sort-up,
+.sort-down {
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 8px;
+  height: 14px;
+  display: inline-block;
+  padding: 0;
+  margin-left: 10px;
+  vertical-align: middle;
+}
+
+.sort-up {
+  background-image: url("../../assets/sort-up.svg");
+}
+
+.sort-down {
+  background: url("../../assets/sort-down.svg");
+}
+</style>
