67db9f8a481f85965988af7c2f94879b9cc0399f
John Simons <john.simons@particular.net>
2024-02-05 11:57:39 +1000

    Use vite-plugin-checker
    So that eslint and ts checks are performed as part of build

--- src/ServicePulse.Host/vue/vite.config.ts
+++ src/ServicePulse.Host/vue/vite.config.ts
@@ -2,19 +2,41 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import checker from "vite-plugin-checker";
+
+function createCSPOverrides(hostPort: number, configuredDestinations: string[]) {
+  const destinations = configuredDestinations.join(" ");
+
+  return (
+    "default-src 'none';" +
+    `connect-src ws://localhost:${hostPort} https://platformupdate.particular.net ${destinations} 'self';` +
+    "font-src 'self' data:;" +
+    `img-src data: 'self';` +
+    `script-src eval: inline: https://platformupdate.particular.net ${destinations} 'self' 'unsafe-eval' 'unsafe-inline';` +
+    `style-src inline: 'self' 'unsafe-inline';` +
+    "worker-src 'self';" +
+    "block-all-mixed-content;" +
+    "sandbox allow-same-origin allow-scripts allow-forms allow-popups allow-modals allow-downloads;"
+  );
+}
+
+const port = 5173;
+const defaultUrls = [
+  "http://10.211.55.3:*", // The default Parallels url to access Windows VM
+  "http://localhost:33333", // Default SC Error instance
+  "http://localhost:33633"  // Default SC Monitoring instance
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    checker({ overlay: { initialIsOpen: "error" }, vueTsc: { tsconfigPath: "tsconfig.app.json" }, eslint: { lintCommand: "eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --ignore-path .gitignore" } }),
     {
       name: "app-constants-js-cache-busting",
       transformIndexHtml(html) {
         const cachebuster = Math.round(new Date().getTime() / 1000);
-        return html.replace(
-          /app.constants.js/,
-          "app.constants.js?" + cachebuster,
-        );
+        return html.replace(/app.constants.js/, "app.constants.js?" + cachebuster);
       },
     },
   ],
@@ -44,7 +66,11 @@ export default defineConfig({
     },
   },
   server: {
+    headers: {
+      "Content-Security-Policy": createCSPOverrides(port, defaultUrls),
+    },
     host: true,
+    port: port,
     fs: {
       // Allow serving files from one level up to the project root
       allow: [".."],
