ab49c73a51202a22f33f8d2d1f065d8efe507dbf
Phil Bastian <phil.bastian@particular.net>
2024-02-06 11:39:25 +0800

    clarify where hard coded value comes from

--- src/ServicePulse.Host/vue/src/components/monitoring/SmallGraph.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/SmallGraph.vue
@@ -36,8 +36,8 @@ const averageLabelValue = computed(() => {
   return props.isdurationgraph ? useFormatTime(average.value).value : useFormatLargeNumber(average.value, 2);
 });
 const averageLabelSuffix = computed(() => (props.isdurationgraph ? useFormatTime(average.value).unit.toUpperCase() : props.metricsuffix ?? ""));
-//38 is an arbitrary 'tested' number that ends up with the label in approximately the correct position.
-//To get it exact, we would need to perform measurement on the rendered element, which we want to avoid since this is close enough
+//38 is 50 (height of parent) - 6 - 6 for padding.
+//To get it exact without hard coding a height value, we would need to perform measurement on the rendered SVG element, which we want to avoid
 const averageLabelPosition = computed(() => `calc(${(average.value / maxYaxis.value) * 38}px - 1em)`);
 </script>
 
