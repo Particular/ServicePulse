300725af2503b27f3fc8e95e81b13031e6dff5d6
Phil Bastian <phil.bastian@particular.net>
2024-02-07 15:30:50 +0800

    extract pathing functions to class

--- src/ServicePulse.Host/vue/src/components/monitoring/graphLines.js
+++ src/ServicePulse.Host/vue/src/components/monitoring/graphLines.js
@@ -18,15 +18,16 @@ export function useGraph(plotdata, minimumyaxis) {
     })();
     const xTick = 100 / (values.length - 1);
     const coordinates = values.reduce((points, yValue, i) => [...points, [i * xTick, yValue]], []);
-    valuesPath.value = `${coordinates.map((c, i) => (i ? lineTo(c) : moveTo(c))).join(" ")}`;
-    valuesArea.value = `${moveTo([0, 0])} ${coordinates.map((c) => lineTo(c)).join(" ")} ${lineTo([100, 0])} Z`;
+    valuesPath.value = new Path().startAt(coordinates[0]).followCoordinates(coordinates.slice(1)).toString();
+    valuesArea.value = new Path().startAt([0, 0]).followCoordinates(coordinates).lineTo([100, 0]).close().toString();
 
     average.value = plotData.average;
     //TODO: why is this called minimumYaxis when it's only used to determine the maxYaxis?
+    // should the graph actually set the min y value rather than leave it at 0?
     const minimumYaxis = !isNaN(toValue(minimumyaxis)) ? Number(toValue(minimumyaxis)) : 10;
     maxYaxis.value = Math.max(...[...values, average.value * 1.5, minimumYaxis]);
 
-    averageLine.value = `M0 ${average.value} L100 ${average.value}`;
+    averageLine.value = new Path().startAt([0, average.value]).lineTo([100, average.value]).toString();
   };
 
   watchEffect(() => createGraph());
@@ -34,10 +35,42 @@ export function useGraph(plotdata, minimumyaxis) {
   return { valuesPath, valuesArea, maxYaxis, average, averageLine };
 }
 
-function moveTo([x, y]) {
-  return `M${x} ${y}`;
-}
+class Path {
+  #pathElements = [];
+  #complete = false;
 
-function lineTo([x, y]) {
-  return `L${x} ${y}`;
+  startAt([x, y]) {
+    if (this.#pathElements.length > 0) throw new Error("startAt must be the first call on a path");
+    return this.moveTo([x, y]);
+  }
+
+  moveTo([x, y]) {
+    if (this.#complete) throw new Error("Path is already closed");
+    this.#pathElements.push(`M${x} ${y}`);
+    return this;
+  }
+
+  lineTo([x, y]) {
+    if (this.#complete) throw new Error("Path is already closed");
+    this.#pathElements.push(`L${x} ${y}`);
+    return this;
+  }
+
+  followCoordinates(coordinates) {
+    for (const c of coordinates) {
+      this.lineTo(c);
+    }
+    return this;
+  }
+
+  close() {
+    if (this.#pathElements.length === 0) throw new Error("Cannot close an empty path");
+    this.#pathElements.push("Z");
+    this.#complete = true;
+    return this;
+  }
+
+  toString() {
+    return this.#pathElements.join(" ");
+  }
 }
