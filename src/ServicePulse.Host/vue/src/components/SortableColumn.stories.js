1ac3ebf679e442dd125fb37339beabf74be50b9b
Travis Nickels <travis.nickels@particular.net>
2024-02-08 18:34:07 -0800

    Fix Storybook for SortableColumn

--- src/ServicePulse.Host/vue/src/components/SortableColumn.stories.js
+++ src/ServicePulse.Host/vue/src/components/SortableColumn.stories.js
@@ -1,22 +1,22 @@
-import EndpointListSortableColumn from "./EndpointListSortableColumn.vue";
+import SortableColumn from "./SortableColumn.vue";
 
 export default {
-  component: EndpointListSortableColumn,
-  title: "Monitoring/EndpointList/Sortable Column",
+  component: SortableColumn,
+  title: "Generic/Sortable Column",
   tags: ["autodocs"],
   decorators: [() => ({ template: '<div style="position: fixed;top: 0; left: 0; bottom: 0; right: 0;display: flex; alignItems:center; justifyContent:center;"><story/></div>' })],
   render: (args) => ({
-    components: { EndpointListSortableColumn },
+    components: { SortableColumn },
     setup() {
       return { args };
     },
     template: `
-        <EndpointListSortableColumn v-bind="args">
+        <SortableColumn v-bind="args">
           {{args.default}}
           <template #unit>
             {{args.unit}}
           </template>
-        </EndpointListSortableColumn>
+        </SortableColumn>
       `,
   }),
 };
