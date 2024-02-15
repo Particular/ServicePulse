e5af361a3897fe6391060a9ccce575f7dd40a537
Christian <christian.quiros+github@gmail.com>
2024-02-14 18:30:31 -0600

    Revert "Extracting mounting of the app into a function"
    This reverts commit 558775fd479be91234b70b90a887bb1d43627f24.

--- src/ServicePulse.Host/vue/src/main.ts
+++ src/ServicePulse.Host/vue/src/main.ts
@@ -1,7 +1,43 @@
-import makeRouter from "./router";
-import { mount } from "./mount";
+import { createApp } from "vue";
+import { Tooltip } from "bootstrap";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import SimpleTypeahead from "vue3-simple-typeahead";
+import Toast, { type PluginOptions, POSITION } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import "vue3-simple-typeahead/dist/vue3-simple-typeahead.css"; //Optional default CSS
 import "./assets/main.css";
 
-mount({ router: makeRouter() });
+const app = createApp(App);
+
+const toastOptions: PluginOptions = {
+  position: POSITION.BOTTOM_RIGHT,
+  timeout: 5000,
+  transition: "Vue-Toastification__fade",
+  hideProgressBar: true,
+  containerClassName: "toast-container",
+  toastClassName: "vue-toast",
+  closeButtonClassName: "toast-close-button",
+};
+
+app.config.errorHandler = (err, instance, info) => {
+  console.error(instance, err);
+};
+
+app
+  .use(router)
+  .use(Toast, toastOptions)
+  .use(SimpleTypeahead)
+  .use(createPinia())
+  // make v-tooltip available in all components
+  .directive("tooltip", {
+    mounted: (el) => {
+      const tooltip = new Tooltip(el, { trigger: "hover" });
+      el.tooltip = tooltip;
+    },
+    beforeUnmount: (el) => {
+      el.tooltip.hide();
+    },
+  })
+  .mount("#app");
