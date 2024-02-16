0c99de4d5c1cfad4f093e3be8e87af92d9c6c99e
Mike Minutillo <mike.minutillo@particular.net>
2024-02-16 08:50:50 +0800

    Extract Endpoint Timings component

--- src/ServicePulse.Host/vue/src/components/monitoring/EndpointDetails.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/EndpointDetails.vue
@@ -18,9 +18,9 @@ import PeriodSelector from "./MonitoringHistoryPeriod.vue";
 import NoData from "../NoData.vue";
 import SmallGraph from "./SmallGraph.vue";
 import PaginationStrip from "@/components/PaginationStrip.vue";
-import LargeGraph from "./LargeGraph.vue";
 import EndpointBacklog from "./EndpointBacklog.vue";
 import EndpointWorkload from "./EndpointWorkload.vue";
+import EndpointTimings from "./EndpointTimings.vue";
 
 const route = useRoute();
 const router = useRouter();
@@ -381,61 +381,7 @@ onMounted(async () => {
             <div class="row">
               <EndpointBacklog v-model="endpoint" :minimum-y-axis="largeGraphsMinimumYAxis.queueLength" />
               <EndpointWorkload v-model="endpoint" :minimum-y-axis="largeGraphsMinimumYAxis.throughputRetries" />
-              <!--ProcessingTime and Critical Time-->
-              <div class="col-xs-4 no-side-padding list-section graph-area graph-critical-processing-times">
-                <!-- large graph -->
-                <LargeGraph
-                  v-if="endpoint.metricDetails.metrics.criticalTime"
-                  :isdurationgraph="true"
-                  :firstdataseries="endpoint.metricDetails.metrics.criticalTime"
-                  :seconddataseries="endpoint.metricDetails.metrics.processingTime"
-                  :minimumyaxis="largeGraphsMinimumYAxis.processingCritical"
-                  :firstseriestype="'critical-time'"
-                  :secondseriestype="'processing-time'"
-                  :avgdecimals="0"
-                />
-                <div class="no-side-padding graph-values">
-                  <div class="no-side-padding processing-time-values">
-                    <div class="">
-                      <span class="metric-digest-header" v-tooltip :title="`Processing time: The time taken for a receiving endpoint to successfully process a message.`"> Processing Time </span>
-                    </div>
-                    <div class="metric-digest-value current">
-                      <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
-                        {{ formatGraphDuration(endpoint.digest.metrics.processingTime.latest).value }}
-                        <span class="metric-digest-value-suffix"> {{ formatGraphDuration(endpoint.digest.metrics.processingTime.latest).unit }}</span>
-                      </div>
-                      <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
-                    </div>
-                    <div class="metric-digest-value average">
-                      <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
-                        {{ formatGraphDuration(endpoint.digest.metrics.processingTime.average).value }}
-                        <span class="metric-digest-value-suffix"> {{ formatGraphDuration(endpoint.digest.metrics.processingTime.average).unit }} AVG</span>
-                      </div>
-                      <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
-                    </div>
-                  </div>
-
-                  <div class="no-side-padding critical-time-values">
-                    <div class="">
-                      <span class="metric-digest-header" v-tooltip :title="`Critical time: The elapsed time from when a message was sent, until it was successfully processed by a receiving endpoint.`"> Critical Time </span>
-                    </div>
-                    <div class="metric-digest-value current">
-                      <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
-                        <span :class="{ negative: formatGraphDuration(endpoint.digest.metrics.criticalTime.latest).value < 0 }"> {{ formatGraphDuration(endpoint.digest.metrics.criticalTime.latest).value }}</span>
-                        <span class="metric-digest-value-suffix"> &nbsp;{{ formatGraphDuration(endpoint.digest.metrics.criticalTime.latest).unit }}</span>
-                      </div>
-                      <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
-                    </div>
-                    <div class="metric-digest-value average">
-                      <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
-                        <span :class="{ negative: formatGraphDuration(endpoint.digest.metrics.criticalTime.average).value < 0 }"> {{ formatGraphDuration(endpoint.digest.metrics.criticalTime.average).value }}</span>
-                        <span class="metric-digest-value-suffix"> &nbsp;{{ formatGraphDuration(endpoint.digest.metrics.criticalTime.average).unit }} AVG </span>
-                      </div>
-                      <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <EndpointTimings v-model="endpoint" :minimum-y-axis="largeGraphsMinimumYAxis.processingCritical" />
             </div>
           </div>
         </div>
---
+++ src/ServicePulse.Host/vue/src/components/monitoring/EndpointTimings.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { formatGraphDuration } from "./formatGraph";
+import LargeGraph from "./LargeGraph.vue";
+
+const endpoint = defineModel({});
+defineProps({
+  minimumYAxis: {
+    default: 10,
+  },
+});
+</script>
+
+<template>
+  <!--ProcessingTime and Critical Time-->
+  <div class="col-xs-4 no-side-padding list-section graph-area graph-critical-processing-times">
+    <!-- large graph -->
+    <LargeGraph
+      v-if="endpoint.metricDetails.metrics.criticalTime"
+      :isdurationgraph="true"
+      :firstdataseries="endpoint.metricDetails.metrics.criticalTime"
+      :seconddataseries="endpoint.metricDetails.metrics.processingTime"
+      :minimumyaxis="minimumYAxis"
+      :firstseriestype="'critical-time'"
+      :secondseriestype="'processing-time'"
+      :avgdecimals="0"
+    />
+    <div class="no-side-padding graph-values">
+      <div class="no-side-padding processing-time-values">
+        <div class="">
+          <span class="metric-digest-header" v-tooltip :title="`Processing time: The time taken for a receiving endpoint to successfully process a message.`"> Processing Time </span>
+        </div>
+        <div class="metric-digest-value current">
+          <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
+            {{ formatGraphDuration(endpoint.digest.metrics.processingTime.latest).value }}
+            <span class="metric-digest-value-suffix"> {{ formatGraphDuration(endpoint.digest.metrics.processingTime.latest).unit }}</span>
+          </div>
+          <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
+        </div>
+        <div class="metric-digest-value average">
+          <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
+            {{ formatGraphDuration(endpoint.digest.metrics.processingTime.average).value }}
+            <span class="metric-digest-value-suffix"> {{ formatGraphDuration(endpoint.digest.metrics.processingTime.average).unit }} AVG</span>
+          </div>
+          <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
+        </div>
+      </div>
+
+      <div class="no-side-padding critical-time-values">
+        <div class="">
+          <span class="metric-digest-header" v-tooltip :title="`Critical time: The elapsed time from when a message was sent, until it was successfully processed by a receiving endpoint.`"> Critical Time </span>
+        </div>
+        <div class="metric-digest-value current">
+          <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
+            <span :class="{ negative: formatGraphDuration(endpoint.digest.metrics.criticalTime.latest).value < 0 }"> {{ formatGraphDuration(endpoint.digest.metrics.criticalTime.latest).value }}</span>
+            <span class="metric-digest-value-suffix"> &nbsp;{{ formatGraphDuration(endpoint.digest.metrics.criticalTime.latest).unit }}</span>
+          </div>
+          <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
+        </div>
+        <div class="metric-digest-value average">
+          <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
+            <span :class="{ negative: formatGraphDuration(endpoint.digest.metrics.criticalTime.average).value < 0 }"> {{ formatGraphDuration(endpoint.digest.metrics.criticalTime.average).value }}</span>
+            <span class="metric-digest-value-suffix"> &nbsp;{{ formatGraphDuration(endpoint.digest.metrics.criticalTime.average).unit }} AVG </span>
+          </div>
+          <strong v-if="endpoint.isStale || endpoint.isScMonitoringDisconnected">?</strong>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "./monitoring.css";
+@import "./largeGraphs.css";
+
+.critical-time-values span.metric-digest-header {
+  color: var(--monitoring-critical-time);
+}
+
+.critical-time-values .current,
+.critical-time-values .average {
+  border-color: var(--monitoring-critical-time);
+}
+
+.processing-time-values span.metric-digest-header {
+  color: var(--monitoring-processing-time);
+}
+
+.processing-time-values .current,
+.processing-time-values .average {
+  border-color: var(--monitoring-processing-time);
+}
+</style>
