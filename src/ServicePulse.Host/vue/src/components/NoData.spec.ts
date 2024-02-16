2430b50de90b13117dcf7d7fdda81c215f02ae76
Christian <christian.quiros+github@gmail.com>
2024-02-16 14:24:47 -0600

    tidying sample component test

--- src/ServicePulse.Host/vue/src/components/NoData.spec.ts
+++ src/ServicePulse.Host/vue/src/components/NoData.spec.ts
@@ -2,7 +2,7 @@ import { expect, it, render, screen } from "@component-test-utils";
 
 import HelloWorld from "./NoData.vue";
 
-it('should work',async()=>{
-    render(HelloWorld, { props: { message:'Hello!'} });
-    expect(await screen.findByText('Hello!')).toBeVisible();
+it("should displayed assigned message", async () => {
+  render(HelloWorld, { props: { message: "No messages processed in this period of time" } });
+  expect(await screen.findByText("No messages processed in this period of time")).toBeVisible();
 });
