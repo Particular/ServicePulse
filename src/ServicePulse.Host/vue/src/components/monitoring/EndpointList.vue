4c610ad17cb67dc69b4c36a31237c16516d33067
Christian <christian.quiros+github@gmail.com>
2024-02-21 16:30:55 -0600

    Revert "updating endpointlist to use the store reference directly"
    This reverts commit c515c8091f525fa0f70eff2a9828ec6ed4221269.

--- src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
+++ src/ServicePulse.Host/vue/src/components/monitoring/EndpointList.vue
@@ -1,10 +1,13 @@
 ﻿<script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import SortableColumn from "../../components/SortableColumn.vue";
 import EndpointListRow from "./EndpointListRow.vue";
 import { useMonitoringStore } from "../../stores/MonitoringStore";
 
 const monitoringStore = useMonitoringStore();
+const endpoints = computed(() => monitoringStore.getEndpointList);
+const isGrouped = computed(() => monitoringStore.endpointListIsGrouped);
+const groupedEndpoints = computed(() => monitoringStore.grouping.groupedEndpoints);
 const activeColumn = ref("name");
 
 const sortByColumn = Object.freeze({
@@ -29,48 +32,28 @@ function updateSorting(isAscending) {
         <SortableColumn :sort-by="sortByColumn.ENDPOINTNAME" v-model="activeColumn" @isAscending="updateSorting">Endpoint name</SortableColumn>
       </div>
       <div class="table-col">
-        <SortableColumn
-          :sort-by="monitoringStore.endpointListIsGrouped ? '' : sortByColumn.QUEUELENGTH"
-          v-model="activeColumn"
-          @isAscending="updateSorting"
-          v-tooltip
-          title="Queue length: The number of messages waiting to be processed in the input queue(s) of the endpoint."
+        <SortableColumn :sort-by="isGrouped ? '' : sortByColumn.QUEUELENGTH" v-model="activeColumn" @isAscending="updateSorting" v-tooltip title="Queue length: The number of messages waiting to be processed in the input queue(s) of the endpoint."
           >Queue Length<template #unit>(MSGS)</template>
         </SortableColumn>
       </div>
       <div class="table-col">
-        <SortableColumn
-          :sort-by="monitoringStore.endpointListIsGrouped ? '' : sortByColumn.THROUGHPUT"
-          v-model="activeColumn"
-          @isAscending="updateSorting"
-          v-tooltip
-          title="Throughput: The number of messages per second successfully processed by a receiving endpoint."
+        <SortableColumn :sort-by="isGrouped ? '' : sortByColumn.THROUGHPUT" v-model="activeColumn" @isAscending="updateSorting" v-tooltip title="Throughput: The number of messages per second successfully processed by a receiving endpoint."
           >Throughput<template #unit>(msgs/s)</template>
         </SortableColumn>
       </div>
       <div class="table-col">
-        <SortableColumn
-          :sort-by="monitoringStore.endpointListIsGrouped ? '' : sortByColumn.SCHEDULEDRETRIES"
-          v-model="activeColumn"
-          @isAscending="updateSorting"
-          v-tooltip
-          title="Scheduled retries: The number of messages per second scheduled for retries (immediate or delayed)."
+        <SortableColumn :sort-by="isGrouped ? '' : sortByColumn.SCHEDULEDRETRIES" v-model="activeColumn" @isAscending="updateSorting" v-tooltip title="Scheduled retries: The number of messages per second scheduled for retries (immediate or delayed)."
           >Scheduled retries <template #unit>(msgs/s)</template>
         </SortableColumn>
       </div>
       <div class="table-col">
-        <SortableColumn
-          :sort-by="monitoringStore.endpointListIsGrouped ? '' : sortByColumn.PROCESSINGTIME"
-          v-model="activeColumn"
-          @isAscending="updateSorting"
-          v-tooltip
-          title="Processing time: The time taken for a receiving endpoint to successfully process a message."
+        <SortableColumn :sort-by="isGrouped ? '' : sortByColumn.PROCESSINGTIME" v-model="activeColumn" @isAscending="updateSorting" v-tooltip title="Processing time: The time taken for a receiving endpoint to successfully process a message."
           >Processing Time <template #unit>(t)</template>
         </SortableColumn>
       </div>
       <div class="table-col">
         <SortableColumn
-          :sort-by="monitoringStore.endpointListIsGrouped ? '' : sortByColumn.CRITICALTIME"
+          :sort-by="isGrouped ? '' : sortByColumn.CRITICALTIME"
           v-model="activeColumn"
           @isAscending="updateSorting"
           v-tooltip
@@ -80,8 +63,8 @@ function updateSorting(isAscending) {
       </div>
     </div>
     <div>
-      <div v-if="monitoringStore.endpointListIsGrouped">
-        <div class="row" v-for="(endpointGroup, index) in monitoringStore.grouping.groupedEndpoints" :key="index">
+      <div v-if="isGrouped">
+        <div class="row" v-for="(endpointGroup, index) in groupedEndpoints" :key="index">
           <div class="endpoint-group-title">
             {{ endpointGroup.group }}
           </div>
@@ -91,7 +74,7 @@ function updateSorting(isAscending) {
         </div>
       </div>
       <div v-else>
-        <div class="endpoint-row" v-for="(endpoint, index) in monitoringStore.getEndpointList" :key="index">
+        <div class="endpoint-row" v-for="(endpoint, index) in endpoints" :key="index">
           <EndpointListRow :endpoint="endpoint" />
         </div>
       </div>
