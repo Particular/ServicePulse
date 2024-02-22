b1183e94cb139f7121694a38bb872b828d9f4508
Christian <christian.quiros+github@gmail.com>
2024-02-21 22:13:43 -0600

    moving assertions to its own interface

--- src/ServicePulse.Host/vue/src/components/PaginationStrip.spec.ts
+++ src/ServicePulse.Host/vue/src/components/PaginationStrip.spec.ts
@@ -2,126 +2,118 @@ import { expect, it as example, render, screen, describe, userEvent } from "@com
 import paginationStrip from "./PaginationStrip.vue";
 
 interface PaginationStripDSL {
-  activePageIs(value: string): void;
+  assert: PaginationStripDSLAssertions;
   clickPrevious(): Promise<void>;
   clickNext(): Promise<void>;
+}
 
+interface PaginationStripDSLAssertions {
+  activePageIs(value: string): void;
   previousIsEnabled(): void;
   previousIsDisabled(): void;
-
   nextIsEnabled(): void;
   nextIsDisabled(): void;
-
-  skipPagesBackIsPresent(): void;
-  skipPagesForwardIsPresent(): void;
-
-  skipPagesBackIsNotPresent(): void;
-  skipPagesForwardIsNotPresent(): void;
+  skipPagesBackIsPresent(value?: boolean): void;
+  skipPagesForwardIsPresent(value?: boolean): void;
 }
 
 describe("Previous page behavior", () => {
   example("Disables navigating to 'Previous' page while first rendered on the first page", async () => {
-    
     const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 1 });
 
-    dsl.previousIsDisabled();
+    dsl.assert.previousIsDisabled();
   });
 
   example("Disables navigating to 'Previous' after navigating to the first page", async () => {
-    
     const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 2 });
 
-    dsl.previousIsEnabled();
+    dsl.assert.previousIsEnabled();
 
     await dsl.clickPrevious();
 
-    dsl.previousIsDisabled();
-    dsl.activePageIs("1");
+    dsl.assert.previousIsDisabled();
+    dsl.assert.activePageIs("1");
   });
 
   example("Enables navigating to 'Previous' page while not first rendered on the first page", async () => {
     const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 2 });
-    dsl.previousIsEnabled();
+    dsl.assert.previousIsEnabled();
   });
 
   example("Enables navigating to 'Previous' page after navigating one page next", async () => {
-    
     const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 1 });
 
-    dsl.previousIsDisabled();
+    dsl.assert.previousIsDisabled();
 
     await dsl.clickNext();
 
-    dsl.activePageIs("2")
-    dsl.previousIsEnabled();
+    dsl.assert.activePageIs("2");
+    dsl.assert.previousIsEnabled();
   });
 });
 
 describe("Next page behavior", () => {
   example("Disables navigating to 'Next' page when first rendered on the last page", async () => {
     const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 10 });
-    dsl.nextIsDisabled();
+    dsl.assert.nextIsDisabled();
   });
 
   example("Disables navigating to 'Next' after navigating to the last page", async () => {
     const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 9 });
 
-    dsl.nextIsEnabled();
+    dsl.assert.nextIsEnabled();
 
     await dsl.clickNext();
 
-    dsl.nextIsDisabled();
-    dsl.activePageIs("10")
+    dsl.assert.nextIsDisabled();
+    dsl.assert.activePageIs("10");
   });
 
   example("Enables navigating to 'Next' page when NOT first rendered on the last page", async () => {
     const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 9 });
 
-    dsl.nextIsEnabled();
-    
+    dsl.assert.nextIsEnabled();
   });
 
   example("Enables navigating to 'Next' page after navigating one page back", async () => {
     const dls = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 10 });
 
-    dls.nextIsDisabled()
+    dls.assert.nextIsDisabled();
 
     dls.clickPrevious();
-    dls.previousIsEnabled();
-    dls.activePageIs("8");
+    dls.assert.previousIsEnabled();
+    dls.assert.activePageIs("8");
   });
 });
 
 describe("Feature: Skipping a number of pages forward or backward must be possible", () => {
-
   describe("Rule: Buttons for skpping back or forward should be available only when enough pages ahead or back are available", () => {
-    
     example("Example: Enough pages to skip forward and backward", async () => {
       const dsl = rederPaginationStripWith({ records: 500, itemsPerPage: 10, selectedPage: 10, allowToJumpPagesBy: 5 });
 
-      dsl.skipPagesBackIsPresent();
-      dsl.skipPagesForwardIsPresent();
+      dsl.assert.skipPagesBackIsPresent();
+      dsl.assert.skipPagesForwardIsPresent();
     });
 
     example("Example: Enough pages to skip foward only", async () => {
       const dsl = rederPaginationStripWith({ records: 500, itemsPerPage: 10, selectedPage: 6, allowToJumpPagesBy: 5 });
 
-      dsl.skipPagesBackIsNotPresent();
-      dsl.skipPagesForwardIsPresent();
+      dsl.assert.skipPagesBackIsPresent(false);
+      dsl.assert.skipPagesForwardIsPresent();
     });
 
     example("Example: Enough pages to skip backward only", async () => {
       const dsl = rederPaginationStripWith({ records: 500, itemsPerPage: 10, selectedPage: 500, allowToJumpPagesBy: 5 });
 
-      dsl.skipPagesBackIsPresent();
-      dsl.skipPagesForwardIsNotPresent();
+      dsl.assert.skipPagesBackIsPresent();
+      dsl.assert.skipPagesForwardIsPresent(false);
     });
 
-    example("Example: No enough pages forward nor backward", async () => {
+    example("Example: No enough pages forward or backward", async () => {
       const dsl = rederPaginationStripWith({ records: 100, itemsPerPage: 10, selectedPage: 1, allowToJumpPagesBy: 5 });
 
-      dsl.skipPagesBackIsNotPresent();
-      dsl.skipPagesForwardIsNotPresent();
+      dsl.assert.skipPagesBackIsPresent(false);
+      dsl.assert.skipPagesForwardIsPresent(false);
     });
   });
 });
@@ -136,21 +128,38 @@ function rederPaginationStripWith({ records, itemsPerPage, selectedPage, allowTo
     },
   });
 
-  return {
-    previousIsDisabled: function () {
-      expect(screen.queryByLabelText("Previous Page")).toBeDisabled();
-    },
-    previousIsEnabled: function () {
-      expect(screen.queryByLabelText("Previous Page")).not.toBeDisabled();
-    },
+  let dslAPI: PaginationStripDSL = {
+    assert: {
+      previousIsDisabled: function () {
+        expect(screen.queryByLabelText("Previous Page")).toBeDisabled();
+      },
+      previousIsEnabled: function () {
+        expect(screen.queryByLabelText("Previous Page")).not.toBeDisabled();
+      },
 
-    nextIsDisabled: function () {
-      expect(screen.queryByLabelText("Next Page")).toBeDisabled();
+      nextIsDisabled: function () {
+        expect(screen.queryByLabelText("Next Page")).toBeDisabled();
+      },
+      nextIsEnabled: function () {
+        expect(screen.queryByLabelText("Next Page")).not.toBeDisabled();
+      },
+      activePageIs: function (value) {
+        expect(screen.getByRole("button", { pressed: true })).toContainHTML(value);
+      },
+      skipPagesBackIsPresent: function (truthy = true) {
+        if (truthy) {
+          expect(screen.queryByLabelText(`Back ${allowToJumpPagesBy}`)).toBeInTheDocument();
+        } else {
+        }
+      },
+      skipPagesForwardIsPresent: function (truthy = true) {
+        if (truthy) {
+          expect(screen.queryByLabelText(`Forward ${allowToJumpPagesBy}`)).toBeInTheDocument();
+        } else {
+          expect(screen.queryByLabelText(`Forward ${allowToJumpPagesBy}`)).not.toBeInTheDocument();
+        }
+      },
     },
-    nextIsEnabled: function () {
-      expect(screen.queryByLabelText("Next Page")).not.toBeDisabled();
-    },
-
     clickPrevious: async function () {
       await userEvent.click(await screen.findByLabelText("Previous Page"));
     },
@@ -158,20 +167,7 @@ function rederPaginationStripWith({ records, itemsPerPage, selectedPage, allowTo
     clickNext: async function () {
       await userEvent.click(await screen.findByLabelText("Next Page"));
     },
-    activePageIs: function (value) {
-      expect(screen.getByRole("button", { pressed: true })).toContainHTML(value);
-    },
-    skipPagesBackIsPresent: function() {
-      expect(screen.queryByLabelText(`Back ${allowToJumpPagesBy}`)).toBeInTheDocument();
-    },
-    skipPagesForwardIsPresent: function() {
-      expect(screen.queryByLabelText(`Forward ${allowToJumpPagesBy}`)).toBeInTheDocument();
-    },
-    skipPagesBackIsNotPresent: function() {
-      expect(screen.queryByLabelText(`Back ${allowToJumpPagesBy}`)).not.toBeInTheDocument();
-    },
-    skipPagesForwardIsNotPresent: function() {
-      expect(screen.queryByLabelText(`Forward ${allowToJumpPagesBy}`)).not.toBeInTheDocument();
-    }
-  } as PaginationStripDSL;
+  };
+
+  return dslAPI;
 }
