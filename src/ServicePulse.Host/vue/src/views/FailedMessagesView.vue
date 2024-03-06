09f5bb174c26b67b7790cfe3d6576787e4177625
Christian <christian.quiros+github@gmail.com>
2024-03-06 15:10:20 -0600

    FailedMessagesView.vue

--- src/ServicePulse.Host/vue/src/views/FailedMessagesView.vue
+++ src/ServicePulse.Host/vue/src/views/FailedMessagesView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import { licenseStatus } from "../composables/serviceLicense";
 import { connectionState, stats } from "../composables/serviceServiceControl";
