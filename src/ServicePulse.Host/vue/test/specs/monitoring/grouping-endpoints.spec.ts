376a5258cb955c4006281edc5e4af0daa16d33fd
Travis Nickels <travis.nickels@particular.net>
2024-03-25 15:45:32 -0700

    Refactoring tests

--- src/ServicePulse.Host/vue/test/specs/monitoring/grouping-endpoints.spec.ts
+++ src/ServicePulse.Host/vue/test/specs/monitoring/grouping-endpoints.spec.ts
@@ -6,6 +6,9 @@ import { numberOfGroupingSegments } from "./questions/numberOfGroupingSegments";
 import { groupingOptionWithName } from "./questions/groupingOptionWithName";
 import { openGroupingOptions } from "./actions/openGroupingOptions";
 import { groupingOptions } from "./questions/groupingOptions";
+import { groupEndpointsBy } from "./actions/groupEndpointsBy";
+import { endpointsGroupsNames } from "./questions/endpointsGroupsNames";
+import { endpointGroup } from "./questions/endpointGroup";
 
 describe("FEATURE: Grouping endpoints", () => {
   describe("RULE: The number of grouping segments is determined by the number of periods in the endpoint name ", () => {
@@ -81,6 +84,34 @@ describe("FEATURE: Grouping endpoints", () => {
       expect(await groupingOptionWithName(/no grouping/i)).toBeInTheDocument();
       expect(groupingOptions().length).toBe(1);
       //screen.logTestingPlaygroundURL();
+    });
+  });
+  describe("RULE: Allow the user to group endpoints by the number of segments in the endpoint name", () => {
+    it("Example: Grouping by ONE segment", async ({ driver }) => {
+      //Arrange
+      await driver.setUp(precondition.serviceControlWithMonitoring);
+      await driver.setUp(precondition.monitoredEndpointsNamed(["Universe.Alphacentauri.Proximacentauri.Endpoint1", "Universe.Solarsystem.Earth.Endpoint1", "Universe.Solarsystem.Earth.Endpoint2"]));
+
+      //Act
+      await driver.goTo("monitoring");
+      await groupEndpointsBy({ numberOfSegments: 1 });
+
+      expect(endpointsGroupsNames()).toEqual(["Universe"]);
+      expect(endpointGroup("Universe").Endpoints).toEqual(["Alphacentauri.Proximacentauri.Endpoint1", "Solarsystem.Earth.Endpoint1", "Solarsystem.Earth.Endpoint2"]);
+    });
+
+    it("Example: Grouping by TWO segments", async ({ driver }) => {
+      //Arrange
+      await driver.setUp(precondition.serviceControlWithMonitoring);
+      await driver.setUp(precondition.monitoredEndpointsNamed(["Universe.Alphacentauri.Proximacentauri.Endpoint1", "Universe.Solarsystem.Earth.Endpoint1", "Universe.Solarsystem.Earth.Endpoint2"]));
+
+      //Act
+      await driver.goTo("monitoring");
+      await groupEndpointsBy({ numberOfSegments: 2 });
+
+      expect(endpointsGroupsNames()).toEqual(["Universe.Alphacentauri", "Universe.Solarsystem"]);
+      expect(endpointGroup("Universe.Alphacentauri").Endpoints).toEqual(["Proximacentauri.Endpoint1"]);
+      expect(endpointGroup("Universe.Solarsystem").Endpoints).toEqual(["Earth.Endpoint1", "Earth.Endpoint2"]);
     });
   });
 });
