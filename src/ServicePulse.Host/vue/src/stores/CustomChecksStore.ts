f45957a55401727ecda8e9b65b2bf2be91e2f387
Mike Minutillo <mike.minutillo@particular.net>
2024-03-27 14:47:29 +0800

    Extract auto-refresh functionality

--- src/ServicePulse.Host/vue/src/stores/CustomChecksStore.ts
+++ src/ServicePulse.Host/vue/src/stores/CustomChecksStore.ts
@@ -2,54 +2,41 @@ import { useDeleteFromServiceControl, useTypedFetchFromServiceControl } from "@/
 import CustomCheck from "@/resources/CustomCheck";
 import { acceptHMRUpdate, defineStore } from "pinia";
 import { ref, watch } from "vue";
+import useAutoRefresh from "@/composables/autoRefresh";
 
 export const useCustomChecksStore = defineStore("CustomChecksStore", () => {
-  const REFRESH_EVERY = 5000;
-  let refreshInterval: number | null = null;
-
   const prefix = "customchecks/";
 
   const pageNumber = ref(1);
   const failingCount = ref(0);
-
   const failedChecks = ref<CustomCheck[]>([]);
 
-  watch(pageNumber, () => loadData());
-
-  async function loadData() {
+  const dataRetriever = useAutoRefresh(async () => {
     try {
-      if (refreshInterval != null) {
-        window.clearTimeout(refreshInterval);
-      }
       const [response, data] = await useTypedFetchFromServiceControl<CustomCheck[]>(`customchecks?status=fail&page=${pageNumber.value}`);
-      if (response.ok) {
-        failedChecks.value = data;
-        failingCount.value = parseInt(response.headers.get("Total-Count") ?? "0");
-      }
-    } finally {
-      refreshInterval = window.setTimeout(() => loadData(), REFRESH_EVERY);
+      failedChecks.value = data;
+      failingCount.value = parseInt(response.headers.get("Total-Count") ?? "0");
+    } catch (e) {
+      failedChecks.value = [];
+      failingCount.value = 0;
+      throw e;
     }
-  }
+  }, 5000);
+
+  watch(pageNumber, () => dataRetriever.executeAndResetTimer());
 
   async function dismissCustomCheck(id: string) {
-    try {
-      if (refreshInterval != null) {
-        window.clearTimeout(refreshInterval);
-      }
-
-      console.log("updating");
+    dataRetriever.executeAndResetTimer(async () => {
       failedChecks.value = failedChecks.value.filter((x) => x.id !== id);
       failingCount.value--;
 
       // HINT: This is required to handle the difference between ServiceControl 4 and 5
       const guid = id.toLocaleLowerCase().startsWith(prefix) ? id.substring(prefix.length) : id;
       await useDeleteFromServiceControl(`${prefix}${guid}`);
-    } finally {
-      refreshInterval = window.setTimeout(() => loadData(), REFRESH_EVERY);
-    }
+    });
   }
 
-  loadData();
+  dataRetriever.executeAndResetTimer();
 
   return {
     dismissCustomCheck,
@@ -63,4 +50,4 @@ if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useCustomChecksStore, import.meta.hot));
 }
 
-export type MonitoringHistoryPeriodStore = ReturnType<typeof useCustomChecksStore>;
+export type CustomChecksStore = ReturnType<typeof useCustomChecksStore>;
