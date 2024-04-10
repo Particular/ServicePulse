8984e24c8f1598c1a7273c66e624341fb7846fb4
Mike Minutillo <mike.minutillo@particular.net>
2024-04-10 12:59:16 +0800

    Use built-in Vue Router active class

--- src/ServicePulse.Host/vue/src/components/PageHeader.vue
+++ src/ServicePulse.Host/vue/src/components/PageHeader.vue
@@ -8,7 +8,6 @@ import ExclamationMark from "./ExclamationMark.vue";
 import { LicenseWarningLevel } from "@/composables/LicenseStatus";
 import { WarningLevel } from "@/components/WarningLevel";
 import routeLinks from "@/router/routeLinks";
-import isRouteSelected from "@/composables/isRouteSelected";
 import CustomChecksMenuItem from "@/components/customchecks/CustomChecksMenuItem.vue";
 
 const displayWarn = computed(() => {
@@ -30,27 +29,27 @@ const displayDanger = computed(() => {
 
       <div id="navbar" class="navbar navbar-expand-lg">
         <ul class="nav navbar-nav navbar-inverse">
-          <li :class="{ active: isRouteSelected(routeLinks.dashboard) }">
+          <li>
             <RouterLink :to="routeLinks.dashboard">
               <i class="fa fa-dashboard icon-white" title="Dashboard"></i>
               <span class="navbar-label">Dashboard</span>
             </RouterLink>
           </li>
-          <li :class="{ active: isRouteSelected(routeLinks.heartbeats.root) }">
+          <li>
             <RouterLink :to="routeLinks.heartbeats.root">
               <i class="fa fa-heartbeat icon-white" title="Heartbeats"></i>
               <span class="navbar-label">Heartbeats</span>
               <span v-if="stats.number_of_failed_heartbeats > 0" class="badge badge-important">{{ stats.number_of_failed_heartbeats }}</span>
             </RouterLink>
           </li>
-          <li v-if="useIsMonitoringEnabled()" :class="{ active: isRouteSelected(routeLinks.monitoring.root) || isRouteSelected(routeLinks.monitoring.endpointDetails.link('endpointName', 1)) }">
+          <li v-if="useIsMonitoringEnabled()">
             <RouterLink :to="routeLinks.monitoring.root">
               <i class="fa pa-monitoring icon-white" title="Monitoring"></i>
               <span class="navbar-label">Monitoring</span>
               <span v-if="stats.number_of_disconnected_endpoints > 0" class="badge badge-important">{{ stats.number_of_disconnected_endpoints }}</span>
             </RouterLink>
           </li>
-          <li :class="{ active: isRouteSelected(routeLinks.failedMessage.root) }">
+          <li>
             <RouterLink :to="routeLinks.failedMessage.root">
               <i class="fa fa-envelope icon-white" title="Failed Messages"></i>
               <span class="navbar-label">Failed Messages</span>
@@ -60,13 +59,13 @@ const displayDanger = computed(() => {
           <li>
             <CustomChecksMenuItem />
           </li>
-          <li :class="{ active: isRouteSelected(routeLinks.events) }">
-            <RouterLink :to="routeLinks.events" exact>
+          <li>
+            <RouterLink :to="routeLinks.events">
               <i class="fa fa-list-ul icon-white" title="Events"></i>
               <span class="navbar-label">Events</span>
             </RouterLink>
           </li>
-          <li :class="{ active: isRouteSelected(routeLinks.configuration.root) }">
+          <li>
             <RouterLink :to="routeLinks.configuration.root" exact>
               <i class="fa fa-cog icon-white" title="Configuration"></i>
               <span class="navbar-label">Configuration</span>
