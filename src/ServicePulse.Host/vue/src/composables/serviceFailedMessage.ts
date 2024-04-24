f714b50078b0c022419ed63557c24373ffbdb60f
Mauro Servienti <mauro.servienti@gmail.com>
2024-04-24 12:01:25 +0200

    Post a different payload depending on the ServiceControl version

--- src/ServicePulse.Host/vue/src/composables/serviceFailedMessage.ts
+++ src/ServicePulse.Host/vue/src/composables/serviceFailedMessage.ts
@@ -1,5 +1,7 @@
 import { usePatchToServiceControl, usePostToServiceControl } from "./serviceServiceControlUrls";
 import type { Ref } from "vue";
+import { useIsSupported } from "@/composables/serviceSemVer";
+import { environment } from "@/composables/serviceServiceControl";
 
 export async function useUnarchiveMessage(ids: string[]) {
   const response = await usePatchToServiceControl("errors/unarchive/", ids);
@@ -26,13 +28,19 @@ export async function useRetryEditedMessage(
     headers: any[];
   }>
 ) {
-  const payload = {
-    message_body: editedMessage.value.messageBody,
-    message_headers: editedMessage.value.headers.reduce((result, header) => {
+  let headers = editedMessage.value.headers;
+  if (useIsSupported(environment.sc_version, "5.2.0")) {
+    headers = editedMessage.value.headers.reduce((result, header) => {
       const { key, value } = header as { key: string; value: string };
       result[key] = value;
       return result;
-    }, {} as { [key: string]: string }) };
+    }, {} as { [key: string]: string });
+  }
+ 
+  const payload = {
+    message_body: editedMessage.value.messageBody,
+    message_headers: headers
+  }
   const response = await usePostToServiceControl(`edit/${id}`, payload);
   if (!response.ok) {
     throw new Error(response.statusText);
