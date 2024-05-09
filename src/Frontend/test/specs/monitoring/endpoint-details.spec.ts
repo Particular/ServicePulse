9cdc288587414da9f977271ba3a796c58b02a13b
Travis Nickels <travis.nickels@particular.net>
2024-05-08 17:53:27 -0700

    Endpoint details tests

--- src/Frontend/test/specs/monitoring/endpoint-details.spec.ts
+++ src/Frontend/test/specs/monitoring/endpoint-details.spec.ts
@@ -1,29 +1,32 @@
 import { it, describe } from "../../drivers/vitest/driver";
 
 describe("FEATURE: Endpoint details", () => {
-  describe("RULE: Endpoint details should be able to be viewable", () => {
+  describe("RULE: The details of an endpoint should be viewable on a dedicated page", () => {
     it("Example: The endpoint name is clicked from the list of endpoints", async ({ driver }) => {});
   });
-  describe("RULE: Endpoint details should show all the message types for the endpoint", () => {
-    it("Example: The endpoint sends a message of type 'Message1'", async ({ driver }) => {});
-  });
-  describe("RULE: Endpoint details should show correct counts for message types", () => {
-    it("Example: The endpoint has messages of type 'Message1'", async ({ driver }) => {});
-    it("Example: The endpoint has messages of type 'Message1' and 'Message2'", async ({ driver }) => {});
-  });
-  describe("RULE: Endpoint details should show all the instances for the endpoint", () => {
-    it("Example: The endpoint has 1 instance", async ({ driver }) => {});
-    it("Example: The endpoint has been scaled out to 3 instances", async ({ driver }) => {});
+  describe("RULE: Endpoint details should hide if license has expired", () => {
+    it("Example: The endpoint has expired license", async ({ driver }) => {});
+    it("Example: The endpoint has valid license", async ({ driver }) => {});
   });
   describe("RULE: Endpoint details should display endpoint name correctly", () => {
-    it("Example: Clicking the endpoint name 'endpoint1", async ({ driver }) => {});
+    it("Example: Clicking an endpoint name from the endpoint monitoring list", async ({ driver }) => {});
   });
-  describe("RULE: Endpoint details should display endpoint status correctly", () => {
-    it("Example: The endpoint connection status is 'connecnted'", async ({ driver }) => {});
-    it("Example: The endpoint connection status is 'disconnecnted'", async ({ driver }) => {});
+  describe("RULE: An indication should be be displayed for the status of an endpoint", () => {
+    it("Example: An endpoint has a negative critical time", async ({ driver }) => {});
+    it("Example: An endpoint is stale", async ({ driver }) => {});
+    it("Example: An endpoint is disconnected from ServiceControl monitoring", async ({ driver }) => {});
+    it("Example: An endpoint has failed messages", async ({ driver }) => {});
   });
-  describe("RULE: Endpoint details should update on period selector change", () => {
+  describe("RULE: Endpoint details should show all message types for the endpoint", () => {
+    it("Example: The endpoint sends messages of type 'Message1,' 'Message2,' and 'Message3'", async ({ driver }) => {});
+    it("Example: Endpoint details should show correct counts for message types", async ({ driver }) => {});
+  });
+  describe("RULE: Endpoint details should show all instances of the endpoint", () => {
+    it("Example: The endpoint has 1 instance running", async ({ driver }) => {});
+    it("Example: The endpoint has 3 instances running", async ({ driver }) => {});
+  });
+  describe("RULE: Endpoint detail graphs should update on period selector change", () => {
     it("Example: One period is selected from the period selector", async ({ driver }) => {});
-    it("Example: Two periods are selected from the period selector", async ({ driver }) => {});
+    it("Example: Two different periods are selected from the period selector", async ({ driver }) => {});
   });
 });
