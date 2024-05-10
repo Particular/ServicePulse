eee5bb51c4ed9bfbea6533db958608ffb5b496e5
Travis Nickels <travis.nickels@particular.net>
2024-05-09 19:15:10 -0700

    Convert heartbeat mocks to typescript

---
+++ src/Frontend/test/mocks/heartbeats-five-active-one-failing.ts
@@ -0,0 +1,6 @@
+import EndpointMonitoringStats from "@/resources/EndpointMonitoringStats";
+
+export const heartbeatsFiveActiveOneFailing = <EndpointMonitoringStats>{
+  active: 5,
+  failing: 1,
+};
---
+++ src/Frontend/test/mocks/heartbeats-four-active-two-failing.ts
@@ -0,0 +1,6 @@
+import EndpointMonitoringStats from "@/resources/EndpointMonitoringStats";
+
+export const heartbeatsFourActiveTwoFailing = <EndpointMonitoringStats>{
+  active: 4,
+  failing: 1,
+};
