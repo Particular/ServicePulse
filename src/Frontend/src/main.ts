648e7df8b774fc32984cef5a36d3ac1e70a41086
Christian <christian.quiros+github@gmail.com>
2024-05-15 15:55:47 -0600

    making running with mocks conditional

--- src/Frontend/src/main.ts
+++ src/Frontend/src/main.ts
@@ -4,9 +4,18 @@ import "vue-toastification/dist/index.css";
 import "vue3-simple-typeahead/dist/vue3-simple-typeahead.css"; //Optional default CSS
 import "./assets/main.css";
 
-async function enableApiMocking() {
-    const { worker } = await import('./mocks/browser');
-    await worker.start();
-}
-
-enableApiMocking().then(() => mount({ router: makeRouter() }));
+async function conditionallyEnableMocking() {
+    if (process.env.NODE_ENV !== 'dev-mocks') {
+      return
+    }
+   
+    const { worker } = await import('./mocks/browser')
+   
+    // `worker.start()` returns a Promise that resolves
+    // once the Service Worker is up and ready to intercept requests.
+    return worker.start()
+  }
+   
+  conditionallyEnableMocking().then(() => {
+    mount({ router: makeRouter() });
+  })
