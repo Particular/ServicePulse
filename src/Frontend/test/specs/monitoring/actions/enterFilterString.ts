114381d121de598a15246b8de0c5e7ef1245d6ca
Christian <christian.quiros+github@gmail.com>
2024-05-16 18:23:06 -0600

    Removing not needed element.focus()

--- src/Frontend/test/specs/monitoring/actions/enterFilterString.ts
+++ src/Frontend/test/specs/monitoring/actions/enterFilterString.ts
@@ -2,9 +2,7 @@ import { screen } from "@testing-library/vue";
 import UserEvent from '@testing-library/user-event';
 
 export async function enterFilterString(filterString: string) {
-  var filterByNameInput = await screen.findByLabelText("filter by name");
-  filterByNameInput.focus();
-
+  var filterByNameInput = await screen.findByLabelText("filter by name");  
   if (filterString.length > 0) {
     await UserEvent.type(filterByNameInput, filterString);
   } else {
