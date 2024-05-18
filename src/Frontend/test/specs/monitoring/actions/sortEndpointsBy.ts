2373a93a6281b2d319aad6264b2abc1016e26841
Christian <christian.quiros@particular.net>
2024-05-17 19:46:23 -0600

    Update src/Frontend/test/specs/monitoring/actions/sortEndpointsBy.ts
    Co-authored-by: Travis Nickels <travis.nickels@particular.net>

--- src/Frontend/test/specs/monitoring/actions/sortEndpointsBy.ts
+++ src/Frontend/test/specs/monitoring/actions/sortEndpointsBy.ts
@@ -4,5 +4,5 @@ import { columnName } from "@/components/monitoring/EndpointListRow.vue";
 
 export async function sortEndpointsBy({ column }: { column: columnName | string }) {
   const filterRegEx = new RegExp(column, "i");
-  await UserEvent.click(await endpointSortingColumnWithName(filterRegEx));  
+  await UserEvent.click(await endpointSortingColumnWithName(filterRegEx));
 }
