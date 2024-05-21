7bdc91b0abddfcd120b96765696cb8a60fbc2880
Travis Nickels <travis.nickels@particular.net>
2024-05-21 14:43:12 -0700

    Refactoring

--- src/Frontend/src/mocks/browser.ts
+++ src/Frontend/src/mocks/browser.ts
@@ -1,4 +1,4 @@
-import { setupWorker } from 'msw/browser';
+import { setupWorker } from "msw/browser";
 import { Driver } from "../../test/driver";
 import { makeMockEndpoint } from "../../test/mock-endpoint";
 import { serviceControlWithMonitoring,monitoredEndpointsNamed } from "../../test/preconditions"
@@ -6,20 +6,16 @@ export const worker = setupWorker();
 const mockEndpoint = makeMockEndpoint({ mockServer: worker });
 
 const makeDriver = (): Driver => ({
-    async goTo() {
-      
-    },
-    mockEndpoint,
-    setUp(factory) {
-        return factory({ driver: this });
-      },
+  async goTo() {},
+  mockEndpoint,
+  setUp(factory) {
+    return factory({ driver: this });
+  },
 });
 
 const driver = makeDriver();
 
-driver.setUp(serviceControlWithMonitoring).then(async ()  => {
+driver.setUp(serviceControlWithMonitoring).then(async () => {
   //override the default mocked endpoints with a custom list
   await driver.setUp(monitoredEndpointsNamed(["Endpoint1","Endpoint2","Endpoint3"]));
 });
-
-
