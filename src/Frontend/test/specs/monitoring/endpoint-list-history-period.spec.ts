5a83bd0cc865ba2ee9200328ca5592e6865c550d
Travis Nickels <travis.nickels@particular.net>
2024-05-21 19:19:22 -0700

    Change history period tests

--- src/Frontend/test/specs/monitoring/endpoint-list-history-period.spec.ts
+++ src/Frontend/test/specs/monitoring/endpoint-list-history-period.spec.ts
@@ -7,8 +7,8 @@ import { selectHistoryPeriod } from "./actions/selectHistoryPeriod";
 import { endpointSparklineValues } from "./questions/endpointSparklineValues";
 import { generatedProcessingTimePoints } from "../../mocks/history-period-template";
 
-describe("FEATURE: Viewing different endpoint history periods", () => {
-  describe("RULE: Endpoint list should display the correct history data from the permalink history query parameter", () => {
+describe("FEATURE: Endpoint history periods", () => {
+  describe("RULE: The history period can get and set the permalink history period query parameter", () => {
     it("EXAMPLE: No history query parameter set", async ({ driver }) => {
       //Arrange
       await driver.setUp(precondition.serviceControlWithMonitoring);
@@ -19,12 +19,30 @@ describe("FEATURE: Viewing different endpoint history periods", () => {
       //expect(await screen.findByText(/the monitoring service is active but no data is being returned\./i)).toBeInTheDocument();
     });
     [
-      { description: "History query parameter set to 1", historyPeriod: 1 },
-      { description: "History query parameter set to 5", historyPeriod: 5 },
-      { description: "History query parameter set to 10", historyPeriod: 10 },
-      { description: "History query parameter set to 15", historyPeriod: 15 },
-      { description: "History query parameter set to 30", historyPeriod: 30 },
-      { description: "History query parameter set to 60", historyPeriod: 60 },
+      { description: "History period query parameter set to 1", historyPeriod: 1 },
+      { description: "History period query parameter set to 5", historyPeriod: 5 },
+      { description: "History period query parameter set to 10", historyPeriod: 10 },
+      { description: "History period query parameter set to 15", historyPeriod: 15 },
+      { description: "History period query parameter set to 30", historyPeriod: 30 },
+      { description: "History period query parameter set to 60", historyPeriod: 60 },
+    ].forEach(({ description, historyPeriod }) => {
+      it(`EXAMPLE: ${description}`, async ({ driver }) => {
+        //Arrange
+        await driver.setUp(precondition.serviceControlWithMonitoring);
+        await driver.setUp(precondition.hasHistoryPeriodDataForOneMinute);
+        //Act
+        await driver.goTo(`monitoring?historyPeriod=${historyPeriod}`);
+
+        //expect(await screen.findByText(/the monitoring service is active but no data is being returned\./i)).toBeInTheDocument();
+      });
+    });
+    [
+      { description: "History period is set to 1 minute and changed to 5 minutes", historyPeriod: 5 },
+      { description: "History period is set to 1 minute and changed to 10 minutes", historyPeriod: 10 },
+      { description: "History period is set to 1 minute and changed to 15 minutes", historyPeriod: 15 },
+      { description: "History period is set to 1 minute and changed to 30 minutes", historyPeriod: 30 },
+      { description: "History period is set to 1 minute and changed to 60 minutes", historyPeriod: 60 },
+      { description: "History period is set to 60 minutes and changed to 1 minute", historyPeriod: 1 },
     ].forEach(({ description, historyPeriod }) => {
       it(`EXAMPLE: ${description}`, async ({ driver }) => {
         //Arrange
@@ -37,7 +55,7 @@ describe("FEATURE: Viewing different endpoint history periods", () => {
       });
     });
   });
-  describe("RULE: When history period is changed the endpoint list should display the correct history data", () => {
+  describe("RULE: Endpoint history period data is fetched immediately after the history period is updated", () => {
     [
       { description: "History period is set to 1 minute and changed to 5 minutes", historyPeriod: 5 },
       { description: "History period is set to 1 minute and changed to 10 minutes", historyPeriod: 10 },
@@ -57,14 +75,14 @@ describe("FEATURE: Viewing different endpoint history periods", () => {
       });
     });
   });
-  describe("RULE: Selecting a history period should update the historyPeriod parameter in the permalink", () => {
+  describe("RULE: Endpoint history period data should be fetched at the interval selected by the history period", () => {
     [
-      { description: "History period is set to 1 minute and changed to 5 minutes", historyPeriod: 5 },
-      //{ description: "History period is set to 1 minute and changed to 10 minutes", historyPeriod: 10 },
-      //{ description: "History period is set to 1 minute and changed to 15 minutes", historyPeriod: 15 },
-      //{ description: "History period is set to 1 minute and changed to 30 minutes", historyPeriod: 30 },
-      //{ description: "History period is set to 1 minute and changed to 60 minutes", historyPeriod: 60 },
-      //{ description: "History period is set to 60 minutes and changed to 1 minute", historyPeriod: 1 },
+      { description: "History period is set to 1 minute", historyPeriod: 1 },
+      { description: "History period is set to 5 minutes", historyPeriod: 5 },
+      { description: "History period is set to 10 minutes", historyPeriod: 10 },
+      { description: "History period is set to 15 minutes", historyPeriod: 15 },
+      { description: "History period is set to 30 minutes", historyPeriod: 30 },
+      { description: "History period is set to 60 minutes", historyPeriod: 60 },
     ].forEach(({ description, historyPeriod }) => {
       it.only(`EXAMPLE: ${description}`, async ({ driver }) => {
         //Arrange
@@ -75,15 +93,15 @@ describe("FEATURE: Viewing different endpoint history periods", () => {
         //Act
         await driver.goTo(`monitoring`);
         //expect(await endpointSparklineValues("Endpoint1")).toEqual(["14", "9.28", "13.8", "76", "217"]);
-        console.log(await endpointSparklineValues("Endpoint1"));
+        //console.log(await endpointSparklineValues("Endpoint1"));
         //await selectHistoryPeriod(historyPeriod);
         await driver.setUp(precondition.hasOneEndpointWithHistoryPeriodDataFor(historyPeriod));
-        await driver.goTo(`monitoring?historyPeriod=${historyPeriod}`);
-        var foo = generatedProcessingTimePoints(historyPeriod);
+        //await driver.goTo(`monitoring?historyPeriod=${historyPeriod}`);
+        //var foo = generatedProcessingTimePoints(historyPeriod);
         //console.log(foo);
         //expect(await endpointSparklineValues("Endpoint1")).toEqual(["14", "9.28", "13.8", "76", "217"]);
-        screen.logTestingPlaygroundURL();
-        console.log(await endpointSparklineValues("Endpoint1"));
+        //screen.logTestingPlaygroundURL();
+        //console.log(await endpointSparklineValues("Endpoint1"));
         expect(await endpointSparklineValues("Endpoint1")).toEqual(["2.96", "2.26", "2.1", "36", "147"]);
         //expect(await groupingOptionWithName(/no grouping/i)).toBeInTheDocument();
       });
