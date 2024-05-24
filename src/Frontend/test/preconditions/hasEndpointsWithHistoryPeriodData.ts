218bc2a994727f9ee74cb29fd1e1347f09a6611b
Travis Nickels <travis.nickels@particular.net>
2024-05-23 21:41:24 -0700

    refactor

--- src/Frontend/test/preconditions/hasEndpointsWithHistoryPeriodData.ts
+++ src/Frontend/test/preconditions/hasEndpointsWithHistoryPeriodData.ts
@@ -1,11 +1,9 @@
-import { historyPeriodForOneMinute, historyPeriodFor, oneEndpointWithHistoryPeriodFor } from "../mocks/history-period-template";
+import { historyPeriodForOneMinute, oneEndpointWithHistoryPeriodFor } from "../mocks/history-period-template";
 import { SetupFactoryOptions } from "../driver";
 
 const monitoringInstanceUrl = window.defaultConfig.monitoring_urls[0];
 
 export const hasHistoryPeriodDataForOneMinute = ({ driver }: SetupFactoryOptions) => {
-  //console.log("Setting up history period data for one minute");
-  //console.log(historyPeriodForOneMinute()[0].metrics.processingTime.points[0]);
   driver.mockEndpoint(`${monitoringInstanceUrl}monitored-endpoints?history=1`, {
     body: historyPeriodForOneMinute(),
   });
