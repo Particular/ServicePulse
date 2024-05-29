435ea0da4ad4250b6a42c36a54a78aecbd4032d1
Travis Nickels <travis.nickels@particular.net>
2024-05-28 21:37:00 -0700

    Update history period template

--- src/Frontend/test/mocks/history-period-template.ts
+++ src/Frontend/test/mocks/history-period-template.ts
@@ -1,8 +1,43 @@
 import { Endpoint } from "@/resources/MonitoringEndpoint";
 
-export const historyPeriodForOneMinute: Endpoint[] = [
-  <Endpoint>{
-    name: "A happy endpoint",
+const linearCongruentialGenerator = (seed: number, bottomValueRange: number, topValueRange: number, length: number): number[] => {
+  const modulus = 13367; // Prime number (0 < modulus)
+  const multiplier = 6863; // Prime number (0 < multiplier < modulus)
+  const increment = 12689; // Prime number (0 <= increment < modulus)
+  const points: number[] = [];
+
+  for (let i = 1; i < length; i++) {
+    seed = (seed * multiplier + increment) % modulus; // Linear congruential generator algorithm
+    const normalizeSeed = seed / modulus; // Normalize seed to be in the range 0 <= seed < 1
+    const value = bottomValueRange + normalizeSeed * (topValueRange - bottomValueRange); // Normalize seed to be in the range bottomValueRange <= seed < topValueRange
+    points.push(value);
+  }
+  return points;
+};
+
+export const generatedProcessingTimePoints = (historyPeriod: number, bottomValueRange: number = 30, topValueRange: number = 80, length: number = 60): number[] => {
+  return linearCongruentialGenerator(historyPeriod, bottomValueRange, topValueRange, length);
+};
+
+const generatedCriticalTimePoints = (historyPeriod: number, bottomValueRange: number = 135, topValueRange: number = 225, length: number = 60): number[] => {
+  return linearCongruentialGenerator(historyPeriod, bottomValueRange, topValueRange, length);
+};
+
+const generatedRetriesPoints = (historyPeriod: number, bottomValueRange: number = 0, topValueRange: number = 15, length: number = 60): number[] => {
+  return linearCongruentialGenerator(historyPeriod, bottomValueRange, topValueRange, length);
+};
+
+const generatedThroughputPoints = (historyPeriod: number, bottomValueRange: number = 1, topValueRange: number = 10, length: number = 60): number[] => {
+  return linearCongruentialGenerator(historyPeriod, bottomValueRange, topValueRange, length);
+};
+
+const generatedQueueLengthPoints = (historyPeriod: number, bottomValueRange: number = 1, topValueRange: number = 15, length: number = 60): number[] => {
+  return linearCongruentialGenerator(historyPeriod, bottomValueRange, topValueRange, length);
+};
+
+export const oneEndpointWithHistoryPeriodFor = (historyPeriod: number): Endpoint => {
+  return <Endpoint>{
+    name: "Endpoint1",
     isStale: false,
     errorCount: 411,
     serviceControlId: "voluptatibus",
@@ -10,140 +45,29 @@ export const historyPeriodForOneMinute: Endpoint[] = [
     endpointInstanceIds: ["c62841c1e8abe36415eb7ec412cedf58"],
     metrics: {
       processingTime: {
-        average: 0.0,
-        points: [],
+        average: 55.2,
+        points: [...generatedProcessingTimePoints(historyPeriod)],
         timeAxisValues: [],
       },
       criticalTime: {
-        average: 0.0,
-        points: [],
+        average: 180,
+        points: [...generatedCriticalTimePoints(historyPeriod)],
         timeAxisValues: [],
       },
       retries: {
-        average: 0.0,
-        points: [],
+        average: 7,
+        points: [...generatedRetriesPoints(historyPeriod)],
       },
       throughput: {
-        average: 0.0,
-        points: [],
+        average: 5.5,
+        points: [...generatedThroughputPoints(historyPeriod)],
       },
       queueLength: {
-        average: 0.0,
-        points: [],
+        average: 7,
+        points: [...generatedQueueLengthPoints(historyPeriod)],
       },
     },
     disconnectedCount: 0,
     connectedCount: 1,
-  },
-  <Endpoint>{
-    name: "A.C.Sales",
-    isStale: false,
-    endpointInstanceIds: ["6b40b6b994899339d03772d23d5c5f19"],
-    metrics: {
-      processingTime: {
-        average: 0.0,
-        points: [],
-        timeAxisValues: [],
-      },
-      criticalTime: {
-        average: 0.0,
-        points: [],
-        timeAxisValues: [],
-      },
-      retries: {
-        average: 0.0,
-        points: [],
-      },
-      throughput: {
-        average: 0.0,
-        points: [],
-      },
-      queueLength: {
-        average: 0.0,
-        points: [
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        ],
-      },
-    },
-    disconnectedCount: 0,
-    connectedCount: 1,
-    errorCount: 0,
-    serviceControlId: "",
-    isScMonitoringDisconnected: false,
-  },
-  <Endpoint>{
-    name: "A.C.ClientUI",
-    isStale: false,
-    endpointInstanceIds: ["cce2f6add5189ee34de8af0e2cc9da34"],
-    metrics: {
-      processingTime: {
-        average: 0.0,
-        points: [],
-        timeAxisValues: [],
-      },
-      criticalTime: {
-        average: 0.0,
-        points: [],
-        timeAxisValues: [],
-      },
-      retries: {
-        average: 0.0,
-        points: [],
-      },
-      throughput: {
-        average: 0.0,
-        points: [],
-      },
-      queueLength: {
-        average: 0.0,
-        points: [
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        ],
-      },
-    },
-    disconnectedCount: 0,
-    connectedCount: 1,
-    errorCount: 0,
-    serviceControlId: "",
-    isScMonitoringDisconnected: false,
-  },
-  <Endpoint>{
-    name: "A.C.Billing",
-    isStale: false,
-    endpointInstanceIds: ["af940336eb7c92f0687af81fe94a0673"],
-    metrics: {
-      processingTime: {
-        average: 0.0,
-        points: [],
-        timeAxisValues: [],
-      },
-      criticalTime: {
-        average: 0.0,
-        points: [],
-        timeAxisValues: [],
-      },
-      retries: {
-        average: 0.0,
-        points: [],
-      },
-      throughput: {
-        average: 0.0,
-        points: [],
-      },
-      queueLength: {
-        average: 0.0,
-        points: [
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        ],
-      },
-    },
-    disconnectedCount: 0,
-    connectedCount: 1,
-    errorCount: 0,
-    serviceControlId: "",
-    isScMonitoringDisconnected: false,
-  },
-];
+  };
+};
