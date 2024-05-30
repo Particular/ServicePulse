034956b0fd460b59e1bafb0d390878957fe6621a
Travis Nickels <travis.nickels@particular.net>
2024-05-29 17:55:23 -0700

    Add History period test for detail links

--- src/Frontend/test/specs/monitoring/actions/selectHistoryPeriod.ts
+++ src/Frontend/test/specs/monitoring/actions/selectHistoryPeriod.ts
@@ -2,7 +2,6 @@ import userEvent from "@testing-library/user-event";
 import { screen, within } from "@testing-library/vue";
 
 export async function selectHistoryPeriod(historyPeriod: number) {
-  screen.logTestingPlaygroundURL();
   const historyPeriodList = await screen.findByRole("list", { name: "history-period-list" });
   const historyPeriodListItem = await within(historyPeriodList).findByRole("listitem", { name: `${historyPeriod}` });
   const historyPeriodLink = await within(historyPeriodListItem).findByRole("link");
