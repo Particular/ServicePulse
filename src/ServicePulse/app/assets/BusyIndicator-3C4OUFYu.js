6378af77889412fa413b4ab53f3cb95d88368f15
Brandon Ording <bording@gmail.com>
2024-05-31 16:12:23 -0400

    Add copy of app

---
+++ src/ServicePulse/app/assets/BusyIndicator-3C4OUFYu.js
@@ -0,0 +1,2 @@
+import{j as c,o,c as e,f as s}from"./index-4lAjyy1Q.js";const t={},a={class:"row"},n=s("div",{class:"col-sm-12"},[s("div",{class:"row box"},[s("div",{class:"col-sm-12"},[s("div",{class:"loading"})])])],-1),r=[n];function _(d,i){return o(),e("div",a,r)}const f=c(t,[["render",_]]);export{f as B};
+//# sourceMappingURL=BusyIndicator-3C4OUFYu.js.map
