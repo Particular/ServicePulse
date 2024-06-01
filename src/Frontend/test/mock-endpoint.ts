8ca41765552d8f9cf6aa8a9af3ca1113007b3c31
Travis Nickels <travis.nickels@particular.net>
2024-06-01 03:42:35 -0700

    Add support for api params

--- src/Frontend/test/mock-endpoint.ts
+++ src/Frontend/test/mock-endpoint.ts
@@ -20,12 +20,30 @@ export const makeMockEndpoint =
       callback?: () => void;
     }
   ) => {
+    const url = new URL(endpoint);
+    const path = url.origin + url.pathname;
+    const expectedParams = Object.fromEntries(url.searchParams.entries()); // The expected params that the mock endpoint should receive extracted from the endpoint API URL
+
     mockServer.use(
-      http[method](endpoint, () => {
+      http[method](path, ({ request }) => {
         if (callback) {
           callback();
         }
-        return HttpResponse.json(body, { status: status });
+
+        const actualParams = Object.fromEntries(new URL(request.url).searchParams.entries()); // Actual params from the request that was intercepted
+
+        //console.log(`Mocked Endpoint: ${url.pathname}, \n Expect Params: ${JSON.stringify(expectedParams)}, \n Actual Params: ${JSON.stringify(actualParams)}`);
+
+        if (expectedParams) {
+          for (const key in expectedParams) {
+            if (expectedParams[key] !== actualParams[key]) {
+              console.error(`Expected param "${key}" to be "${expectedParams[key]}" but got "${actualParams[key]}"`);
+              throw new Error(`Parameter mismatch: expected "${expectedParams[key]}" for "${key}" but got "${actualParams[key]}"`);
+            }
+          }
+        }
+
+        return HttpResponse.json(body, { status, headers });
       })
     );
   };
