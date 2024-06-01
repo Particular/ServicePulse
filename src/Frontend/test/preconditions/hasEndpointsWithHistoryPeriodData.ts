4059b7f3d1a4108c15793f17d31990b3bb6fc7a4
Travis Nickels <travis.nickels@particular.net>
2024-06-01 03:43:07 -0700

    Refactoring removed unused function

--- src/Frontend/test/preconditions/hasEndpointsWithHistoryPeriodData.ts
+++ src/Frontend/test/preconditions/hasEndpointsWithHistoryPeriodData.ts
@@ -1,55 +1,12 @@
 import * as historyPeriodTemplate from "../mocks/history-period-template";
 import { SetupFactoryOptions } from "../driver";
 
-export const hasHistoryPeriodDataForOneMinute = ({ driver }: SetupFactoryOptions) => {
-  driver.mockEndpoint(`${window.defaultConfig.monitoring_urls[0]}monitored-endpoints?history=1`, {
-    body: historyPeriodTemplate.historyPeriodForOneMinute(),
-  });
-  return historyPeriodTemplate.historyPeriodForOneMinute();
-};
-
-export const hasOneEndpointWithHistoryPeriodDataFor =
-  (historyPeriod: number) =>
+export const hasEndpointWithMetricsPoints =
+  (historyPeriod: number, queueLength: number | number[], throughput: number | number[], retries: number | number[], processingTime: number | number[], criticalTime: number | number[]) =>
   ({ driver }: SetupFactoryOptions) => {
+    const body = historyPeriodTemplate.oneEndpointWithMetricsPoints(queueLength, throughput, retries, processingTime, criticalTime);
     driver.mockEndpoint(`${window.defaultConfig.monitoring_urls[0]}monitored-endpoints?history=${historyPeriod}`, {
-      body: [historyPeriodTemplate.oneEndpointWithHistoryPeriodFor(historyPeriod)],
+      body,
     });
-    return [historyPeriodTemplate.oneEndpointWithHistoryPeriodFor(historyPeriod)];
-  };
-
-export const hasOneEndpointWithHistoryPeriodDataForOneMinute =
-  () =>
-  ({ driver }: SetupFactoryOptions) => {
-    driver.mockEndpoint(`${window.defaultConfig.monitoring_urls[0]}monitored-endpoints?history=1`, {
-      body: historyPeriodTemplate.oneEndpointWithHistoryPeriodForOneMinute(),
-    });
-    return historyPeriodTemplate.oneEndpointWithHistoryPeriodForOneMinute();
-  };
-
-const getEndpointSparklineData = (historyPeriod: number) => {
-  switch (historyPeriod) {
-    case 1:
-      return historyPeriodTemplate.oneEndpointWithSparklineForOneMinute();
-    case 5:
-      return historyPeriodTemplate.oneEndpointWithSparklineForFiveMinutes();
-    case 10:
-      return historyPeriodTemplate.oneEndpointWithSparklineForTenMinutes();
-    case 15:
-      return historyPeriodTemplate.oneEndpointWithSparklineForFifteenMinutes();
-    case 30:
-      return historyPeriodTemplate.oneEndpointWithSparklineForThirtyMinutes();
-    case 60:
-      return historyPeriodTemplate.oneEndpointWithSparklineForSixtyMinutes();
-    default:
-      return [];
-  }
-};
-
-export const hasOneEndpointWithSparklineDataFor =
-  (historyPeriod: number) =>
-  ({ driver }: SetupFactoryOptions) => {
-    driver.mockEndpoint(`${window.defaultConfig.monitoring_urls[0]}monitored-endpoints?history=${historyPeriod}`, {
-      body: getEndpointSparklineData(historyPeriod),
-    });
-    return [getEndpointSparklineData(historyPeriod)];
+    return [body];
   };
