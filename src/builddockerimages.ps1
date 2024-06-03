2ee693f584a9d930f6076d089a2efb9b600a4633
Bob Langley <bob.langley@particular.net>
2024-06-03 14:56:38 +0000

    Move container src to Container subfolder

--- src/builddockerimages.ps1
+++
@@ -1,8 +0,0 @@
-cd ServicePulse.Host
-npm install
-node ./node_modules/webpack/bin/webpack.js --config app/modules/modules.webpackconfig.builder.js
-
-cd ..
-
-docker build -f .\dockerfile.iis -t particular/servicepulse.iis .
-docker build -f .\dockerfile.nginx -t particular/servicepulse.nginx .
