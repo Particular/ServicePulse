40cbbc7df10606105f155d01765789d028519a07
Travis Nickels <travis.nickels@particular.net>
2024-06-05 21:14:06 -0700

    Update history period tests

--- src/Frontend/test/preconditions/hasEndpointsWithHistoryPeriodData.ts
+++ src/Frontend/test/preconditions/hasEndpointsWithHistoryPeriodData.ts
@@ -2,19 +2,12 @@ import * as historyPeriodTemplate from "../mocks/history-period-template";
 import { SetupFactoryOptions } from "../driver";
 
 export const hasEndpointWithMetricsPoints =
-  (historyPeriod: number, queueLength: number | number[], throughput: number | number[], retries: number | number[], processingTime: number | number[], criticalTime: number | number[]) =>
+  (queueLength: number | number[], throughput: number | number[], retries: number | number[], processingTime: number | number[], criticalTime: number | number[]) =>
   ({ driver }: SetupFactoryOptions) => {
     const body = historyPeriodTemplate.oneEndpointWithMetricsPoints(queueLength, throughput, retries, processingTime, criticalTime);
     //console.log(`Mocked Endpoint: ${window.defaultConfig.monitoring_urls[0]}monitored-endpoints?history=${historyPeriod}`);
     driver.mockEndpoint(`${window.defaultConfig.monitoring_urls[0]}monitored-endpoints`, {
-      body: foo(body, historyPeriod, queueLength, throughput, retries, processingTime, criticalTime),
+      body: body,
     });
     return [body];
   };
-
-const foo = (body: any, historyPeriod: number, queueLength: number | number[], throughput: number | number[], retries: number | number[], processingTime: number | number[], criticalTime: number | number[]) => {
-  console.log(
-    `Mocked Endpoint: ${window.defaultConfig.monitoring_urls[0]}monitored-endpoints?history=${historyPeriod}, queueLength: ${queueLength}, throughput: ${throughput}, retries: ${retries}, processingTime: ${processingTime}, criticalTime: ${criticalTime}`
-  );
-  return body;
-};
