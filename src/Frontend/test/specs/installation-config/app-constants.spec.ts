65c1639076396b1f2cf2c92ca69b8bee9b18b5f4
Christian <christian.quiros+github@gmail.com>
2024-06-07 14:28:09 -0600

    more tests

--- src/Frontend/test/specs/installation-config/app-constants.spec.ts
+++ src/Frontend/test/specs/installation-config/app-constants.spec.ts
@@ -1,7 +1,5 @@
-import { expect, vi } from "vitest";
-import { screen } from "@testing-library/vue"; 
+import { expect} from "vitest";
 import { it, describe } from "../../drivers/vitest/driver";
-import { waitFor } from "@testing-library/vue";
 import * as precondition from "../../preconditions";
 import { endpointsNames } from "../monitoring/questions/endpointsNames";
 
@@ -10,43 +8,50 @@ describe("FEATURE: app.constants.js", () => {
     it("EXAMPLE: default route is set to /dashboard", async ({ driver }) => {
       //Arrange
       await driver.setUp(precondition.serviceControlWithMonitoring);
-
       window.defaultConfig.default_route = "/dashboard";
-      await driver.goTo("/");
 
-      await waitFor(async () => {
-        expect(window.location.href).toBe("http://localhost:3000/#/dashboard");
-      });
+      //act      
+      await driver.goTo("/");
+      
+      expect(window.location.href).toBe("http://localhost:3000/#/dashboard");
     });
 
-    it("EXAMPLE: default route is set to /monitoring", async ({ driver }) => {      
-      
+    it("EXAMPLE: default route is set to /monitoring", async ({ driver }) => {
       //Arrange
       await driver.setUp(precondition.serviceControlWithMonitoring);
       await driver.setUp(precondition.monitoredEndpointsNamed(["Endpoint1"]));
-
       window.defaultConfig.default_route = "/monitoring";
-      
-      await driver.goTo("/");
-      expect(await endpointsNames()).toEqual(["Endpoint1"]);
 
+      //act
+      await driver.goTo("/");
+
+      //Although the navigation was not explicitly made to /monitoring, the default route should be applied and therefore:
+      // - the monitoring page should be displayed
+      expect(await endpointsNames()).toEqual(["Endpoint1"]);
+      //- the monitoring address should be in the URL
       expect(window.location.href).toBe("http://localhost:3000/#/monitoring");
-      
     });
 
-    it("EXAMPLE: default route is not set", async ({ driver }) => {      
-      
+    it("EXAMPLE: default route is set to a empty value", async ({ driver }) => {
       //Arrange
       await driver.setUp(precondition.serviceControlWithMonitoring);
-            
       window.defaultConfig.default_route = "";
-      
-      await driver.goTo("");
 
-      await waitFor(async () => {
-        expect(window.location.href).toBe("http://localhost:3000/#/");
-      });
+      //act
+      await driver.goTo("");
+      
+      expect(window.location.href).toBe("http://localhost:3000/#/");      
     });
 
+    it("EXAMPLE: default route is set to /", async ({ driver }) => {
+      //Arrange
+      await driver.setUp(precondition.serviceControlWithMonitoring);  
+      window.defaultConfig.default_route = "";
+  
+      //act
+      await driver.goTo("");  
+      
+      expect(window.location.href).toBe("http://localhost:3000/#/");      
+    });
   });
 });
