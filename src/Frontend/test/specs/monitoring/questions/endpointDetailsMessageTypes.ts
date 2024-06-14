d310f87b5ad8b51b386b7ac7827e800f6f1d7c10
Travis Nickels <travis.nickels@particular.net>
2024-06-13 17:29:20 -0700

    Add question to get message type count from the tab

--- src/Frontend/test/specs/monitoring/questions/endpointDetailsMessageTypes.ts
+++ src/Frontend/test/specs/monitoring/questions/endpointDetailsMessageTypes.ts
@@ -4,3 +4,8 @@ export async function endpointMessageNames() {
   const messageName = await screen.findAllByRole("message-type-name", { name: "message-type-name" });
   return messageName.map((name) => name.textContent);
 }
+
+export async function endpointMessageTypesCount() {
+  const messageTypesCount = await screen.findByLabelText("message-types-count");
+  return messageTypesCount.textContent;
+}
