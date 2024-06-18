51319ffe44943e7b1aa5e8f3ef34c8c60eba81f2
Travis Nickels <travis.nickels@particular.net>
2024-06-17 19:26:40 -0700

    Add a precondition for endpoint details with metric digest values

--- src/Frontend/test/preconditions/hasMonitoredEndpointDetails.ts
+++ src/Frontend/test/preconditions/hasMonitoredEndpointDetails.ts
@@ -1,5 +1,5 @@
 import { EndpointDetails } from "@/resources/MonitoringEndpoint";
-import { monitoredEndpointDetails, instanceForEndpoint } from "../mocks/monitored-endpoint-template";
+import { monitoredEndpointDetails, instanceForEndpoint, metricDigestForEndpoint } from "../mocks/monitored-endpoint-template";
 import { SetupFactoryOptions } from "../driver";
 import { messageTypeForEndpoint } from "../mocks/monitored-endpoint-template";
 
@@ -34,6 +34,56 @@ export const hasEndpointInstancesNamed =
     const endpointDetails = structuredClone(monitoredEndpointDetails);
     endpointDetails.instances = [];
     endpointDetails.instances.push(...instanceNames.map((name) => ({ ...instanceForEndpoint, name: name })));
+
+    const monitoringInstanceUrl = window.defaultConfig.monitoring_urls[0];
+    driver.mockEndpoint(`${monitoringInstanceUrl}monitored-endpoints/${endpointDetails.instances[0].name}`, {
+      body: endpointDetails,
+    });
+
+    return endpointDetails;
+  };
+
+export const hasEndpointWithMetricValues =
+  (
+    queueLengthLatest: number,
+    queueLengthAverage: number,
+    throughputLatest: number,
+    throughputAverage: number,
+    retriesLatest: number,
+    retriesAverage: number,
+    processingTimeLatest: number,
+    processingTimeAverage: number,
+    criticalTimeLatest: number,
+    criticalTimeAverage: number
+  ) =>
+  ({ driver }: SetupFactoryOptions) => {
+    const endpointDetails = structuredClone(monitoredEndpointDetails);
+    if (endpointDetails.digest.metrics.queueLength === undefined) {
+      return undefined;
+    }
+    if (endpointDetails.digest.metrics.throughput === undefined) {
+      return undefined;
+    }
+    if (endpointDetails.digest.metrics.retries === undefined) {
+      return undefined;
+    }
+    if (endpointDetails.digest.metrics.processingTime === undefined) {
+      return undefined;
+    }
+    if (endpointDetails.digest.metrics.criticalTime === undefined) {
+      return undefined;
+    }
+
+    endpointDetails.digest.metrics.queueLength.latest = queueLengthLatest;
+    endpointDetails.digest.metrics.queueLength.average = queueLengthAverage;
+    endpointDetails.digest.metrics.throughput.latest = throughputLatest;
+    endpointDetails.digest.metrics.throughput.average = throughputAverage;
+    endpointDetails.digest.metrics.retries.latest = retriesLatest;
+    endpointDetails.digest.metrics.retries.average = retriesAverage;
+    endpointDetails.digest.metrics.processingTime.latest = processingTimeLatest;
+    endpointDetails.digest.metrics.processingTime.average = processingTimeAverage;
+    endpointDetails.digest.metrics.criticalTime.latest = criticalTimeLatest;
+    endpointDetails.digest.metrics.criticalTime.average = criticalTimeAverage;
 
     const monitoringInstanceUrl = window.defaultConfig.monitoring_urls[0];
     driver.mockEndpoint(`${monitoringInstanceUrl}monitored-endpoints/${endpointDetails.instances[0].name}`, {
