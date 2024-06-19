f955a4c9714df17305add67182f3222aedb5097f
Travis Nickels <travis.nickels@particular.net>
2024-06-19 00:08:46 -0700

    Change role to aria-label for endpoint links

--- src/Frontend/test/specs/monitoring/questions/endpointDetailLinks.ts
+++ src/Frontend/test/specs/monitoring/questions/endpointDetailLinks.ts
@@ -2,7 +2,7 @@ import { screen, within } from "@testing-library/vue";
 
 export async function endpointDetailsLinks() {
   const endpointList = await screen.findByRole("treegrid", { name: "endpoint-list" });
-  const detailLinks = await within(endpointList).queryAllByRole("details-link");
+  const detailLinks = await within(endpointList).queryAllByLabelText("details-link");
   const endpointDetailLinks = detailLinks.map((item) => item.getAttribute("href"));
   return endpointDetailLinks;
 }
