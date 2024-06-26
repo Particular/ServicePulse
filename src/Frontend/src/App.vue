cc8214874af32920a984842f79718688f2f92b55
Christian <christian.quiros+github@gmail.com>
2024-06-25 20:49:58 -0600

    Extracting toast notifications to component

--- src/Frontend/src/App.vue
+++ src/Frontend/src/App.vue
@@ -4,16 +4,16 @@ import { RouterView, useRouter } from "vue-router";
 import PageFooter from "./components/PageFooter.vue";
 import PageHeader from "./components/PageHeader.vue";
 import "bootstrap";
-import { useLicense } from "@/composables/serviceLicense";
 import { useServiceControlUrls } from "@/composables/serviceServiceControlUrls";
-import { useServiceControl } from "@/composables/serviceServiceControl";
 import "highlight.js/styles/github-dark.css";
+import { useServiceControl } from "@/composables/serviceServiceControl";
+import LicenseNotifications from "./components/LicenseNotifications.vue";
 
 useServiceControlUrls();
 
 onMounted(async () => {
   const router = useRouter();
-  await Promise.all([useLicense(router), useServiceControl(router)]);
+  await useServiceControl(router);
 });
 </script>
 
@@ -22,5 +22,6 @@ onMounted(async () => {
   <div class="container-fluid" id="main-content">
     <RouterView />
   </div>
+  <LicenseNotifications />
   <page-footer />
 </template>
