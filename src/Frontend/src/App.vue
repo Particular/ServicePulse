6f2c83a815a3664fb8e92fdc283a8a69847da728
Christian <christian.quiros+github@gmail.com>
2024-06-25 22:10:32 -0600

    Moving notifications from app to component

--- src/Frontend/src/App.vue
+++ src/Frontend/src/App.vue
@@ -1,64 +1,20 @@
 <script setup lang="ts">
-import { onMounted, computed, watch } from "vue";
-import { RouterView, useRouter } from "vue-router";
+import { onMounted } from "vue";
+import { RouterView } from "vue-router";
 import PageFooter from "./components/PageFooter.vue";
 import PageHeader from "./components/PageHeader.vue";
 import "bootstrap";
-import { useServiceControlUrls, monitoringUrl, serviceControlUrl, useIsMonitoringDisabled } from "@/composables/serviceServiceControlUrls";
+import { useServiceControlUrls } from "@/composables/serviceServiceControlUrls";
 import "highlight.js/styles/github-dark.css";
-import { useServiceControl, monitoringConnectionState, connectionState, environment } from "@/composables/serviceServiceControl";
-import LicenseNotifications from "./components/LicenseNotifications.vue";
-import routeLinks from "@/router/routeLinks";
-import { useShowToast } from "@/composables/toast";
-import { TYPE } from "vue-toastification";
+import { useServiceControl } from "@/composables/serviceServiceControl";
+import LicenseNotifications from "@/components/LicenseNotifications.vue";
+import BackendChecksNotifications from "@/components/BackendChecksNotifications.vue";
 
 useServiceControlUrls();
-const router = useRouter();
 
 onMounted(async () => {
   await useServiceControl();
 });
-
-const primaryConnectionFailure = computed(() => connectionState.unableToConnect);
-const monitoringConnectionFailure = computed(() => monitoringConnectionState.unableToConnect);
-
-watch(primaryConnectionFailure, (newValue, oldValue) => {
-  //NOTE to eliminate success msg showing everytime the screen is refreshed
-  if (newValue !== oldValue && !(oldValue === null && newValue === false)) {
-    const connectionUrl = router.resolve(routeLinks.configuration.connections.link).href;
-    if (newValue) {
-      useShowToast(TYPE.ERROR, "Error", `Could not connect to ServiceControl at ${serviceControlUrl.value}. <a class="btn btn-default" href="${connectionUrl}">View connection settings</a>`);
-    } else {
-      useShowToast(TYPE.SUCCESS, "Success", `Connection to ServiceControl was successful at ${serviceControlUrl.value}.`);
-    }
-  }
-});
-
-watch(monitoringConnectionFailure, (newValue, oldValue) => {
-  // Only watch the state change if monitoring is enabled
-  if (useIsMonitoringDisabled()) {
-    return;
-  }
-
-  //NOTE to eliminate success msg showing everytime the screen is refreshed
-  if (newValue !== oldValue && !(oldValue === null && newValue === false)) {
-    const connectionUrl = router.resolve(routeLinks.configuration.connections.link).href;
-    if (newValue) {
-      useShowToast(TYPE.ERROR, "Error", `Could not connect to the ServiceControl Monitoring service at ${monitoringUrl.value}. <a class="btn btn-default" href="${connectionUrl}">View connection settings</a>`);
-    } else {
-      useShowToast(TYPE.SUCCESS, "Success", `Connection to ServiceControl Monitoring service was successful at ${monitoringUrl.value}.`);
-    }
-  }
-});
-
-watch(
-  () => environment.is_compatible_with_sc,
-  (newValue) => {
-    if (newValue === false) {
-      useShowToast(TYPE.ERROR, "Error", `You are using Service Control version ${environment.sc_version}. Please, upgrade to version ${environment.minimum_supported_sc_version} or higher to unlock new functionality in ServicePulse.`);
-    }
-  }
-);
 </script>
 
 <template>
@@ -67,5 +23,6 @@ watch(
     <RouterView />
   </div>
   <LicenseNotifications />
+  <BackendChecksNotifications />
   <page-footer />
 </template>
