08b1f01133f3b893a53853155af19c2c3f47f5c5
Brandon Ording <bording@gmail.com>
2024-07-15 16:15:06 -0400

    Add YARP

--- src/ServicePulse/Program.cs
+++ src/ServicePulse/Program.cs
@@ -1,6 +1,61 @@
 using Microsoft.Extensions.FileProviders;
+using Yarp.ReverseProxy.Configuration;
+using Yarp.ReverseProxy.Transforms;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var serviceControlInstance = new ClusterConfig
+{
+    ClusterId = "serviceControlInstance",
+    Destinations = new Dictionary<string, DestinationConfig>
+    {
+        { "instance", new DestinationConfig { Address = "http://localhost:33333" } }
+    }
+};
+
+var monitoringInstance = new ClusterConfig
+{
+    ClusterId = "monitoringInstance",
+    Destinations = new Dictionary<string, DestinationConfig>
+    {
+        { "instance", new DestinationConfig { Address = "http://localhost:33633/" } }
+    }
+};
+
+var serviceControlRoute = new RouteConfig()
+{
+    RouteId = "serviceControlRoute",
+    ClusterId = nameof(serviceControlInstance),
+    Match = new RouteMatch
+    {
+        Path = "/api/{**catch-all}"
+    }
+};
+
+var monitoringRoute = new RouteConfig()
+{
+    RouteId = "monitoringRoute",
+    ClusterId = nameof(monitoringInstance),
+    Match = new RouteMatch
+    {
+        Path = "/monitoring-api/{**catch-all}"
+    }
+}.WithTransformPathRemovePrefix("/monitoring-api");
+
+var routes = new List<RouteConfig>
+{
+    serviceControlRoute,
+    monitoringRoute
+};
+
+var clusters = new List<ClusterConfig>
+{
+    serviceControlInstance,
+    monitoringInstance
+};
+
+builder.Services.AddReverseProxy().LoadFromMemory(routes, clusters);
+
 var app = builder.Build();
 
 var manifestEmbeddedFileProvider = new ManifestEmbeddedFileProvider(typeof(Program).Assembly, "wwwroot");
@@ -11,5 +66,7 @@ app.UseDefaultFiles(defaultFilesOptions);
 
 var staticFileOptions = new StaticFileOptions { FileProvider = fileProvider };
 app.UseStaticFiles(staticFileOptions);
+
+app.MapReverseProxy();
 
 app.Run();
--- src/ServicePulse/wwwroot/js/app.constants.js
+++ src/ServicePulse/wwwroot/js/app.constants.js
@@ -2,7 +2,7 @@ window.defaultConfig = {
   default_route: '/dashboard',
   base_url: '/',
   version: '1.2.0',
-  service_control_url: 'http://localhost:33333/api/',
-  monitoring_urls: ['http://localhost:33633/'],
+  service_control_url: 'http://localhost:5039/api/',
+  monitoring_urls: ['http://localhost:5039/monitoring-api/'],
   showPendingRetry: false,
 };
