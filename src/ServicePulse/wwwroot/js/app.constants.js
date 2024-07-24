9dfaddfe71e2ff2ab5f8a4dcfcecf68b1aeabe2d
Brandon Ording <bording@gmail.com>
2024-07-24 13:36:08 -0400

    Make relative

--- src/ServicePulse/wwwroot/js/app.constants.js
+++ src/ServicePulse/wwwroot/js/app.constants.js
@@ -2,7 +2,7 @@ window.defaultConfig = {
   default_route: '/dashboard',
   base_url: '/',
   version: '1.2.0',
-  service_control_url: 'http://localhost:5039/api/',
-  monitoring_urls: ['http://localhost:5039/monitoring-api/'],
+  service_control_url: '/api/',
+  monitoring_urls: ['/monitoring-api/'],
   showPendingRetry: false,
 };
