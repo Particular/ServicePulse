8db5967e521df9c4bec1af9c0c579ad10a472d4b
Brandon Ording <bording@gmail.com>
2024-07-24 15:45:04 -0400

    Remove unused files (#1971)

--- src/run-dev.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-pushd .\ServicePulse.Host
-start "Proxy" docker run -it --rm -p 1331:1331 -v %cd%/nginx.conf:/etc/nginx/nginx.conf:ro --name service-pulse-dev nginx
-pushd .\vue
-start "Vue" npm run dev
-popd
-pushd .\angular
-start "Angular" npm run dev
