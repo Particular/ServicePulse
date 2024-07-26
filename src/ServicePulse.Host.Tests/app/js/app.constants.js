14223977f88b230bbe626e11c70a5764fa8d9416
Brandon Ording <bording@gmail.com>
2024-07-26 17:23:36 -0400

    Update ServicePulse.Host.Tests to react to changes

--- src/ServicePulse.Host.Tests/app/js/app.constants.js
+++
@@ -1,7 +0,0 @@
-window.defaultConfig = {
-    default_route: '/dashboard',
-    version: '1.2.0',
-    service_control_url: 'http://some.host.com:33333/api/',
-    monitoring_urls: ['http://some.host.com:33633/'],
-    showPendingRetry: true,
-};
