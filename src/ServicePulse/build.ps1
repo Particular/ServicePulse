5659abd08f82ca7ea776d94b6758eea051de1eca
Brandon Ording <bording@gmail.com>
2024-08-06 17:05:39 -0400

    Stop deleting file that doesn't need to be removed

--- src/ServicePulse/build.ps1
+++ src/ServicePulse/build.ps1
@@ -4,7 +4,6 @@ $FrontendSourceFolder = $ScriptPath + "/../Frontend"
 Push-Location $FrontendSourceFolder
 npm install
 npm run build
-Remove-Item -Path "./dist/mockServiceWorker.js"
 Pop-Location
 
 if ( $? -eq $false ) {
