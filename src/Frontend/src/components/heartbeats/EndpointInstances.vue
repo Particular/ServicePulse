d9f1b03e2a5a14224cd5ca05b000a14e38242125
John Simons <john.simons@particular.net>
2024-08-22 08:59:01 +1000

    small adjustments to layout

--- src/Frontend/src/components/heartbeats/EndpointInstances.vue
+++ src/Frontend/src/components/heartbeats/EndpointInstances.vue
@@ -34,54 +34,63 @@ async function toggleAlerts(instance: Endpoint) {
 </script>
 
 <template>
-  <div class="row">
-    <div class="col-8 instances-heading">
-      <RouterLink :to="routeLinks.heartbeats.root">&#9664; Back to list</RouterLink>
-      <h1>{{ endpointName }} Instances</h1>
-    </div>
-    <div class="col-4 align-content-center justify-content-end">
-      <div role="search" aria-label="filter" class="filter-input">
-        <input type="text" placeholder="Filter by name..." aria-label="filter by name" class="form-control-static filter-input" v-model="instanceFilterString" />
+  <div class="container">
+    <div class="row">
+      <div class="col-8 instances-heading">
+        <RouterLink :to="routeLinks.heartbeats.root">&#9664; Back to list</RouterLink>
+        <h1>{{ endpointName }} Instances</h1>
+      </div>
+      <div class="col-4 align-content-center">
+        <div class="searchContainer">
+          <div role="search" aria-label="filter" class="filter-input">
+            <input type="text" placeholder="Filter by name..." aria-label="filter by name" class="form-control-static filter-input" v-model="instanceFilterString" />
+          </div>
+        </div>
       </div>
     </div>
+    <section role="table" aria-label="endpoint-instances">
+      <!--Table headings-->
+      <div role="row" aria-label="column-headers" class="row table-head-row">
+        <div role="columnheader" :aria-label="columnName.HostName" class="col-6">
+          <SortableColumn :sort-by="columnName.HostName" v-model="sortByInstances" :default-ascending="true">Host name</SortableColumn>
+        </div>
+        <div role="columnheader" :aria-label="columnName.LastHeartbeat" class="col-2">
+          <SortableColumn :sort-by="columnName.LastHeartbeat" v-model="sortByInstances">Last Heartbeat</SortableColumn>
+        </div>
+        <div role="columnheader" aria-label="actions" class="col-4">
+          <div>Actions</div>
+        </div>
+      </div>
+      <no-data v-if="instances.length === 0" message="No endpoint instances found"></no-data>
+      <div role="rowgroup" aria-label="endpoints">
+        <div role="row" :aria-label="instance.name" class="row instance-row" v-for="instance in instances" :key="instance.id">
+          <div role="cell" aria-label="instance-name" class="col-6 host-name">
+            <span role="status" class="logo">
+              <i v-if="instance.heartbeat_information?.reported_status !== EndpointStatus.Alive" aria-label="instance dead" class="fa fa-heartbeat text-danger" />
+              <i v-else aria-label="instance alive" class="fa fa-heartbeat text-success" />
+            </span>
+            <span class="lead">{{ instance.host_display_name }}</span>
+          </div>
+          <div role="cell" aria-label="last-heartbeat" class="col-2 last-heartbeat"><time-since :date-utc="instance.heartbeat_information?.last_report_at" default-text-on-failure="Unknown" /></div>
+          <div role="cell" aria-label="last-heartbeat" class="col-4 actions">
+            <button v-if="instance.heartbeat_information?.reported_status !== EndpointStatus.Alive" type="button" @click="deleteInstance(instance)" class="btn btn-danger btn-sm"><i class="fa fa-trash text-white" /> Delete</button>&nbsp;
+            <button v-if="instance.monitor_heartbeat" type="button" @click="toggleAlerts(instance)" class="btn btn-info btn-sm"><i class="fa fa-bell-slash text-white" /> Mute Alerts</button>
+            <button v-else type="button" @click="toggleAlerts(instance)" class="btn btn-warning btn-sm"><i class="fa fa-bell text-white" /> Unmute Alerts</button>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
-  <section role="table" aria-label="endpoint-instances">
-    <!--Table headings-->
-    <div role="row" aria-label="column-headers" class="row table-head-row">
-      <div role="columnheader" :aria-label="columnName.HostName" class="col-6">
-        <SortableColumn :sort-by="columnName.HostName" v-model="sortByInstances" :default-ascending="true">Host name</SortableColumn>
-      </div>
-      <div role="columnheader" :aria-label="columnName.LastHeartbeat" class="col-2">
-        <SortableColumn :sort-by="columnName.LastHeartbeat" v-model="sortByInstances">Last Heartbeat</SortableColumn>
-      </div>
-      <div role="columnheader" aria-label="actions" class="col-4">
-        <div>Actions</div>
-      </div>
-    </div>
-    <no-data v-if="instances.length === 0" message="No endpoint instances found"></no-data>
-    <div role="rowgroup" aria-label="endpoints">
-      <div role="row" :aria-label="instance.name" class="row instance-row" v-for="instance in instances" :key="instance.id">
-        <div role="cell" aria-label="instance-name" class="col-6 host-name">
-          <span role="status" class="logo">
-            <i v-if="instance.heartbeat_information?.reported_status !== EndpointStatus.Alive" aria-label="instance dead" class="fa fa-heartbeat text-danger" />
-            <i v-else aria-label="instance alive" class="fa fa-heartbeat text-success" />
-          </span>
-          <span class="lead">{{ instance.host_display_name }}</span>
-        </div>
-        <div role="cell" aria-label="last-heartbeat" class="col-2 last-heartbeat"><time-since :date-utc="instance.heartbeat_information?.last_report_at" default-text-on-failure="Unknown" /></div>
-        <div role="cell" aria-label="last-heartbeat" class="col-4 actions">
-          <button v-if="instance.heartbeat_information?.reported_status !== EndpointStatus.Alive" type="button" @click="deleteInstance(instance)" class="btn btn-danger btn-sm"><i class="fa fa-trash text-white" /> Delete</button>&nbsp;
-          <button v-if="instance.monitor_heartbeat" type="button" @click="toggleAlerts(instance)" class="btn btn-info btn-sm"><i class="fa fa-bell-slash text-white" /> Mute Alerts</button>
-          <button v-else type="button" @click="toggleAlerts(instance)" class="btn btn-warning btn-sm"><i class="fa fa-bell text-white" /> Unmute Alerts</button>
-        </div>
-      </div>
-    </div>
-  </section>
 </template>
 
 <style scoped>
 @import "../list.css";
 @import "./heartbeats.css";
+
+.searchContainer {
+  display: flex;
+  justify-content: flex-end;
+}
 
 .instances-heading h1 {
   margin-bottom: 10px;
@@ -89,6 +98,8 @@ async function toggleAlerts(instance: Endpoint) {
 
 .logo {
   width: 20px;
+  font-size: 1.4em;
+  margin-right: 4px;
 }
 
 .actions {
