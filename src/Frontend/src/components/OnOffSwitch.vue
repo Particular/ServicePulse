837928bd4b380206e090b520558d98728123fb1f
Phil Bastian <phil.bastian@particular.net>
2024-09-03 13:52:34 +0800

    convert onoffswitch to use valuetoggle

--- src/Frontend/src/components/OnOffSwitch.vue
+++ src/Frontend/src/components/OnOffSwitch.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ValueToggle from "./ValueToggle.vue";
 defineProps<{
   id: string;
 }>();
@@ -8,94 +9,5 @@ const emit = defineEmits<{ toggle: [] }>();
 </script>
 
 <template>
-  <div class="onoffswitch">
-    <input type="checkbox" :id="`onoffswitch${id}`" :name="`onoffswitch${id}`" class="onoffswitch-checkbox" @click="emit('toggle')" v-model="model" />
-    <label class="onoffswitch-label" :for="`onoffswitch${id}`">
-      <span class="onoffswitch-inner"></span>
-      <span class="onoffswitch-switch"></span>
-    </label>
-  </div>
+  <ValueToggle :id="id" value1="OFF" value2="ON" width="76px" @toggle="emit('toggle')" v-model="model" />
 </template>
-
-<style scoped>
-.onoffswitch {
-  -moz-user-select: none;
-  -ms-user-select: none;
-  -webkit-user-select: none;
-  user-select: none;
-  position: relative;
-  width: 76px;
-}
-
-.onoffswitch-checkbox {
-  display: none;
-}
-
-.onoffswitch-label {
-  border: 2px solid #929e9e;
-  border-radius: 20px;
-  cursor: pointer;
-  display: block;
-  margin-top: 8px;
-  overflow: hidden;
-}
-
-.onoffswitch-inner {
-  display: block;
-  margin-left: -100%;
-  transition: margin 0.3s ease-in 0s;
-  width: 200%;
-}
-
-.onoffswitch-inner:before,
-.onoffswitch-inner:after {
-  box-sizing: border-box;
-  color: white;
-  display: block;
-  float: left;
-  font-family: Trebuchet, Arial, sans-serif;
-  font-size: 14px;
-  font-weight: bold;
-  height: 30px;
-  line-height: 30px;
-  padding: 0;
-  width: 50%;
-}
-
-.onoffswitch-inner:before {
-  background-color: #00a3c4;
-  color: #ffffff;
-  content: "ON";
-  padding-left: 10px;
-}
-
-.onoffswitch-inner:after {
-  background-color: #f2f6f7;
-  color: #929e9e;
-  content: "OFF";
-  padding-right: 10px;
-  text-align: right;
-}
-
-.onoffswitch-switch {
-  background: #ffffff;
-  border: 2px solid #929e9e;
-  border-radius: 20px;
-  bottom: 0;
-  display: block;
-  margin: 6px;
-  position: absolute;
-  right: 42px;
-  top: 0;
-  transition: all 0.3s ease-in 0s;
-  width: 22px;
-}
-
-.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {
-  margin-left: 0;
-}
-
-.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {
-  right: 0;
-}
-</style>
