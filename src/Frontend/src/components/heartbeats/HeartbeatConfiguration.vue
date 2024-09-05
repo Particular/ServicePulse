173c4c4a61a764d27fc7d17c79359ddcba8fe440
Phil Bastian <phil.bastian@particular.net>
2024-09-05 15:28:14 +0800

    convert heartbeat configuration to sortable grid

--- src/Frontend/src/components/heartbeats/HeartbeatConfiguration.vue
+++ src/Frontend/src/components/heartbeats/HeartbeatConfiguration.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { useHeartbeatsStore } from "@/stores/HeartbeatsStore";
+import { ColumnNames, useHeartbeatsStore } from "@/stores/HeartbeatsStore";
 import { storeToRefs } from "pinia";
 import NoData from "../NoData.vue";
 import TimeSince from "../TimeSince.vue";
+import SortableColumn from "@/components/SortableColumn.vue";
+import routeLinks from "@/router/routeLinks";
+import { Tippy } from "vue-tippy";
 import { useShowToast } from "@/composables/toast";
 import { TYPE } from "vue-toastification";
 import { LogicalEndpoint } from "@/resources/Heartbeat";
@@ -10,11 +13,8 @@ import EndpointSettingsSupported from "@/components/heartbeats/EndpointSettingsS
 import OnOffSwitch from "../OnOffSwitch.vue";
 
 const store = useHeartbeatsStore();
-const { sortedEndpoints, defaultTrackingInstancesValue } = storeToRefs(store);
-const dropdownOptions = [
-  { text: "Track Instances", value: true },
-  { text: "Do Not Track Instances", value: false },
-];
+const { sortedEndpoints, defaultTrackingInstancesValue, sortByInstances } = storeToRefs(store);
+
 async function toggleDefaultSetting() {
   try {
     await store.updateEndpointSettings({ name: "", track_instances: defaultTrackingInstancesValue.value });
@@ -32,6 +32,13 @@ async function changeEndpointSettings(endpoint: LogicalEndpoint) {
     useShowToast(TYPE.ERROR, "Save failed", "", false, { timeout: 3000 });
   }
 }
+
+function endpointHealth(endpoint: LogicalEndpoint) {
+  if (endpoint.alive_count === 0) return "text-danger";
+  if (endpoint.track_instances) {
+    return endpoint.down_count > 0 ? "text-warning" : "text-success";
+  } else return "text-success";
+}
 </script>
 
 <template>
@@ -40,22 +47,45 @@ async function changeEndpointSettings(endpoint: LogicalEndpoint) {
       <div class="row">
         <div class="col-9 no-side-padding">
           <no-data v-if="sortedEndpoints.length === 0" message="Nothing to configure" />
-          <div class="row box box-no-click no-side-padding" v-for="endpoint in sortedEndpoints" :key="endpoint.name">
-            <div class="col-sm-12 no-side-padding">
-              <div class="row">
-                <div class="col-8">
-                  <div class="row box-header">
-                    <div class="col-xs-12">
-                      <p class="lead">{{ endpoint.name }}</p>
-                      <p class="endpoint-metadata"><i class="fa fa-heartbeat"></i> <time-since :date-utc="endpoint.heartbeat_information?.last_report_at" default-text-on-failure="No recent heartbeat information available" /></p>
-                      <p class="endpoint-metadata" v-if="!endpoint.heartbeat_information"><i class="fa fa-plug"></i> No heartbeat plugin installed</p>
-                    </div>
+          <div v-else class="row box box-no-click no-side-padding">
+            <!--Table headings-->
+            <div role="row" aria-label="column-headers" class="row table-head-row">
+              <div role="columnheader" :aria-label="ColumnNames.Name" class="col-6">
+                <SortableColumn :sort-by="ColumnNames.Name" v-model="sortByInstances" :default-ascending="true">Name</SortableColumn>
+              </div>
+              <div role="columnheader" :aria-label="ColumnNames.Instances" class="col-2">
+                <SortableColumn :sort-by="ColumnNames.Instances" v-model="sortByInstances" :default-ascending="true">Instances</SortableColumn>
+              </div>
+              <div role="columnheader" :aria-label="ColumnNames.LastHeartbeat" class="col-2">
+                <SortableColumn :sort-by="ColumnNames.LastHeartbeat" v-model="sortByInstances">Last Heartbeat</SortableColumn>
+              </div>
+              <div role="columnheader" :aria-label="ColumnNames.LastHeartbeat" class="col-2 centre">
+                <SortableColumn :sort-by="ColumnNames.Tracked" v-model="sortByInstances">Track Instances</SortableColumn>
+              </div>
+            </div>
+            <!--Table rows-->
+            <div role="rowgroup" aria-label="endpoints">
+              <div role="row" :aria-label="endpoint.name" class="row grid-row" v-for="endpoint in sortedEndpoints" :key="endpoint.name">
+                <div role="cell" aria-label="instance-name" class="col-6 host-name">
+                  <div class="box-header">
+                    <tippy :aria-label="endpoint.name" :content="endpoint.name" class="no-side-padding lead righ-side-ellipsis endpoint-details-link">
+                      <RouterLink aria-label="details-link" :to="{ path: routeLinks.heartbeats.instances.link(endpoint.name), query: { back: routeLinks.heartbeats.configuration.link } }"> {{ endpoint.name }} </RouterLink>
+                    </tippy>
                   </div>
                 </div>
-                <div class="col-4">
-                  <select class="form-select dropDownOptions" @change="() => changeEndpointSettings(endpoint)">
-                    <option :value="item.value" :selected="endpoint.track_instances === item.value" v-for="item in dropdownOptions" :key="item.text">{{ item.text }}</option>
-                  </select>
+                <div role="cell" aria-label="instance-count" class="col-2">
+                  <i v-if="endpoint.track_instances" class="fa fa-server" :class="endpointHealth(endpoint)"></i>
+                  <i v-else class="fa fa-sellsy" :class="endpointHealth(endpoint)"></i>
+                  <span class="endpoint-count">{{ store.endpointDisplayName(endpoint) }}</span>
+                </div>
+                <div role="cell" aria-label="last-heartbeat" class="col-2 last-heartbeat">
+                  <p v-if="endpoint.heartbeat_information"><time-since :date-utc="endpoint.heartbeat_information?.last_report_at" default-text-on-failure="unknown" /></p>
+                  <p v-else>No plugin installed</p>
+                </div>
+                <div role="cell" aria-label="tracked-instances" class="col-2 centre">
+                  <div class="switch">
+                    <OnOffSwitch :id="endpoint.name" @toggle="changeEndpointSettings(endpoint)" v-model="endpoint.track_instances" />
+                  </div>
                 </div>
               </div>
             </div>
@@ -104,9 +134,5 @@ async function changeEndpointSettings(endpoint: LogicalEndpoint) {
 
 .instructions > div {
   margin-bottom: 5px;
-}
-
-.dropDownOptions {
-  width: 250px;
 }
 </style>
