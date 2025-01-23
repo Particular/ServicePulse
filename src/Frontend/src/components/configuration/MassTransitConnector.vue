7432751a4949e6fbb66bc42a352fd3625859ce6f
Phil Bastian <phil.bastian@particular.net>
2025-01-23 12:41:20 +0800

    style using existing colours and formats, and change pixel sizing to em

--- src/Frontend/src/components/configuration/MassTransitConnector.vue
+++ src/Frontend/src/components/configuration/MassTransitConnector.vue
@@ -5,34 +5,35 @@ import moment from "moment";
 const configuration = useConfiguration();
 // "Wed, Jan 15th 2025 10:56:21 +10:00",
 function formatDate(date: string) {
-  return moment(date).format("ddd, MMM Do YYYY HH:mm:ss Z");
+  return moment(date).local().format("LLLL"); //.format("ddd, MMM Do YYYY HH:mm:ss Z");
 }
 </script>
 
 <template>
   <div class="box" v-if="configuration?.mass_transit_connector !== undefined">
     <div class="row margin-bottom-10">
-      <div class="heading">
+      <h4>
         Connector Version: <span class="version-format">{{ configuration.mass_transit_connector.version }}</span>
-      </div>
+      </h4>
     </div>
     <div class="row margin-bottom-10">
-      <div class="heading">List of error queues configured in the connector.</div>
+      <h4>List of error queues configured in the connector.</h4>
       <div class="queues-container">
         <div class="row margin-gap hover-highlight" v-for="queue in configuration.mass_transit_connector.error_queues" :key="queue.name">
           <div class="col-sm-6">{{ queue.name }}</div>
           <div class="col-sm-6 error-color" v-if="!queue.ingesting">Not ingesting</div>
+          <div class="col-sm-6 ok-color" v-else>Ok</div>
         </div>
       </div>
     </div>
     <div class="row">
-      <div class="heading">The entries below are the most recent warning and error-level events recorded on the ServiceControl Connector.</div>
+      <h4>The entries below are the most recent warning and error-level events recorded on the ServiceControl Connector.</h4>
       <div class="logs-container">
         <div class="row margin-gap hover-highlight" v-for="log in configuration.mass_transit_connector.logs" :key="log.date">
           <div class="col-2">{{ formatDate(log.date) }}</div>
           <div class="col-1" :class="`${log.level.toLowerCase()}-color`">{{ log.level }}</div>
           <div class="col-9" :class="`${log.level.toLowerCase()}-color`">
-            <pre class="reset-style">{{ log.message }}</pre>
+            <span>{{ log.message }}</span>
           </div>
         </div>
       </div>
@@ -48,34 +49,32 @@ function formatDate(date: string) {
 .hover-highlight:hover {
   background-color: #ededed;
 }
-.heading {
-  font-size: 16px;
-}
-.reset-style {
-  all: revert;
-  margin: 0;
-}
+
 .margin-gap {
   margin-bottom: 3px;
 }
 .queues-container {
-  max-width: 400px;
-  padding: 10px;
+  max-width: 30em;
+  padding: 0.75rem;
 }
 .logs-container {
-  padding: 10px;
+  padding: 0.75rem;
 }
 .version-format {
   font-weight: bold;
 }
-.margin-bottom-10 {
-  padding-bottom: 10px;
+.box > .row:not(:last-child) {
+  padding-bottom: 0.5rem;
   border-bottom: 1px solid #ccc;
+  margin-bottom: 0.5rem;
 }
 .warning-color {
-  color: #ff9800;
+  color: var(--bs-warning);
 }
 .error-color {
-  color: #f44336;
+  color: var(--bs-danger);
+}
+.ok-color {
+  color: var(--bs-success);
 }
 </style>
