7a3d63dcd48b1916ca5c57a04ee2d3326c421aed
Tamara Rivera <tamita.rivera@gmail.com>
2025-02-03 11:25:14 -0800

    Update build.ps1

--- src/ServicePulse.Host/build.ps1
+++ src/ServicePulse.Host/build.ps1
@@ -10,6 +10,11 @@ New-Item -ItemType Directory -Force -Path $AppOutputFolder
 
 Push-Location $FrontendSourceFolder
 npm audit --audit-level=info
+
+if ( $? -eq $false ) {
+    exit $LastExitCode
+}
+
 npm install
 npm run build
 Remove-Item -Path "./dist/mockServiceWorker.js"
