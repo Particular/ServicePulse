cbca6596c47c2156b34bab105f3fa2bb2b75b772
Dan Kent <83468000+kentdr@users.noreply.github.com>
2025-02-07 12:09:30 -0500

    Update src/Frontend/test/specs/configuration/viewing-license.spec.ts
    Co-authored-by: Christian <christian.quiros@particular.net>

--- src/Frontend/test/specs/configuration/viewing-license.spec.ts
+++ src/Frontend/test/specs/configuration/viewing-license.spec.ts
@@ -11,7 +11,9 @@ import { licenseTabList, licenseTabNames } from "./questions/licenseTabs";
 
 describe("FEATURE: License", () => {
   describe("RULE: Platform license type should be shown shown", () => {
-    test("EXAMPLE: Valid platform license type is shown", async ({ driver }) => {
+    test.todo("EXAMPLE: The platform is running a trial license");
+    
+    test("EXAMPLE: The platform is running a commercial license", async ({ driver }) => {
       await driver.setUp(precondition.serviceControlWithMonitoring);
       await driver.setUp(precondition.hasActiveLicense);
       await driver.goTo("/configuration/license");
