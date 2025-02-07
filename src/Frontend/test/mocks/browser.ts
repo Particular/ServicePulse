7ef214ac85cfa78e9e7151825414ecb2b03203de
Dan Kent <dan.kent@particular.net>
2025-02-07 12:17:26 -0500

    Revert browser.ts

--- src/Frontend/test/mocks/browser.ts
+++ src/Frontend/test/mocks/browser.ts
@@ -2,7 +2,6 @@ import { setupWorker } from "msw/browser";
 import { Driver } from "../driver";
 import { makeMockEndpoint, makeMockEndpointDynamic } from "../mock-endpoint";
 import * as precondition from "../preconditions";
-
 export const worker = setupWorker();
 const mockEndpoint = makeMockEndpoint({ mockServer: worker });
 const mockEndpointDynamic = makeMockEndpointDynamic({ mockServer: worker });
@@ -25,5 +24,24 @@ const driver = makeDriver();
 
 (async () => {
   await driver.setUp(precondition.serviceControlWithMonitoring);
-  await driver.setUp(precondition.HasHealthyAndUnHealthyEndpoints(1, 1));
+  //override the default mocked endpoints with a custom list
+  await driver.setUp(
+    precondition.monitoredEndpointsNamed([
+      "Universe.Solarsystem.Mercury.Endpoint1",
+      "Universe.Solarsystem.Mercury.Endpoint2",
+      "Universe.Solarsystem.Venus.Endpoint3",
+      "Universe.Solarsystem.Venus.Endpoint4",
+      "Universe.Solarsystem.Earth.Endpoint5",
+      "Universe.Solarsystem.Earth.Endpoint6",
+    ])
+  );
+
+  await driver.setUp(
+    precondition.hasFailedMessage({
+      withGroupId: "81dca64e-76fc-e1c3-11a2-3069f51c58c8",
+      withMessageId: "40134401-bab9-41aa-9acb-b19c0066f22d",
+      withContentType: "application/json",
+      withBody: { Index: 0, Data: "" },
+    })
+  );
 })();
