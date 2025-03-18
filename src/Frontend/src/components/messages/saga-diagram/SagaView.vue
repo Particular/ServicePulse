4d8a4a1ae245b507f38aedf0ede335667a86874f
Jayanthi <jayanthi.sourirajan@particular.net>
2025-03-18 16:35:03 -0700

    added feature for no data available

---
+++ src/Frontend/src/components/messages/saga-diagram/SagaView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <span aria-label="message-not-involved-in-saga">No Saga Data Available</span>
+  </div>
+  <div>
+    <span aria-label="no-saga-plugin">To visualize your saga, please install the appropriate nuget package in your endpoint. Saga audit plugin needed to visualize saga</span>
+  </div>
+</template>
+
+<style scoped></style>
