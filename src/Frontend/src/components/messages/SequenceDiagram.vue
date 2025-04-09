881c51af2dfa7a16e402c7bc052e6ada59afb4bc
Phil Bastian <phil.bastian@particular.net>
2025-04-09 14:00:56 +0800

    fix sequence diagram not re-rendering on tab switch away and back

--- src/Frontend/src/components/messages/SequenceDiagram.vue
+++ src/Frontend/src/components/messages/SequenceDiagram.vue
@@ -6,6 +6,7 @@ import Routes from "./SequenceDiagram/RoutesComponent.vue";
 import { useSequenceDiagramStore } from "@/stores/SequenceDiagramStore";
 import { storeToRefs } from "pinia";
 import useTooltips from "./SequenceDiagram/tooltipOverlay.ts";
+import { onUnmounted } from "vue";
 
 const store = useSequenceDiagramStore();
 store.setConversationId("4de4bdad-83ce-4785-87de-b2b701813673");
@@ -13,6 +14,8 @@ store.setConversationId("4de4bdad-83ce-4785-87de-b2b701813673");
 const { maxWidth, maxHeight } = storeToRefs(store);
 
 useTooltips();
+
+onUnmounted(() => store.setConversationId(""));
 </script>
 
 <template>
