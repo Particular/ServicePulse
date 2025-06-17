d3e3b8e64b1ad663df949a1e15b8a5747e2cac1d
Christian <christian.quiros+github@gmail.com>
2025-06-16 21:41:26 -0600

    Individual legend components

---
+++ src/Frontend/src/views/throughputreport/LegendSendOnlyEndpoint.vue
@@ -0,0 +1,3 @@
+<template>
+  <span>An endpoint that only sends messages and does not process any messages that is not yet configured to be send-only</span>
+</template>
---
+++ src/Frontend/src/views/throughputreport/LegendTransactionalSessionProcessorEndpoint.vue
@@ -0,0 +1,7 @@
+<template>
+  <span>
+    An Endpoint that uses the
+    <a href="https://docs.particular.net/nservicebus/transactional-session/" target="_blank">transactional session</a>, or a dedicated endpoint that has been indicated as the
+    <a href="https://docs.particular.net/nservicebus/transactional-session/#remote-processor" target="_blank">remote processor</a>
+  </span>
+</template>
