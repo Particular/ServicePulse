96546a64378f06b8ba193e98bebbc0ea06bc8659
Phil Bastian <phil.bastian@particular.net>
2025-10-24 12:08:55 +0800

    revert sc check since it caused tests to fail and doesn't really matter

--- src/Frontend/src/views/CustomChecksView.vue
+++ src/Frontend/src/views/CustomChecksView.vue
@@ -4,7 +4,6 @@ import CustomCheckView from "@/components/customchecks/CustomCheckView.vue";
 import { storeToRefs } from "pinia";
 import PaginationStrip from "@/components/PaginationStrip.vue";
 import useCustomChecksStoreAutoRefresh from "@/composables/useCustomChecksStoreAutoRefresh";
-import ServiceControlAvailable from "@/components/ServiceControlAvailable.vue";
 
 const { store } = useCustomChecksStoreAutoRefresh();
 
@@ -12,25 +11,23 @@ const { pageNumber, failingCount, failedChecks } = storeToRefs(store);
 </script>
 
 <template>
-  <ServiceControlAvailable>
-    <div class="container">
-      <div class row="row">
-        <div class="col-sm-12 padded">
-          <h1>Custom checks</h1>
-        </div>
+  <div class="container">
+    <div class row="row">
+      <div class="col-sm-12 padded">
+        <h1>Custom checks</h1>
       </div>
+    </div>
 
-      <section name="custom_checks">
-        <NoData v-if="failingCount === 0" message="No failed custom checks" role="note" aria-label="customcheck-message" />
-        <div v-else class="row" role="table" aria-label="custom-check-list">
-          <div class="col-sm-12">
-            <CustomCheckView v-for="item of failedChecks" :key="item.id" :custom-check="item" />
-            <div class="row">
-              <PaginationStrip :items-per-page="10" :total-count="failingCount" v-model="pageNumber" role="row" aria-label="custom-check-pagination" />
-            </div>
+    <section name="custom_checks">
+      <NoData v-if="failingCount === 0" message="No failed custom checks" role="note" aria-label="customcheck-message" />
+      <div v-else class="row" role="table" aria-label="custom-check-list">
+        <div class="col-sm-12">
+          <CustomCheckView v-for="item of failedChecks" :key="item.id" :custom-check="item" />
+          <div class="row">
+            <PaginationStrip :items-per-page="10" :total-count="failingCount" v-model="pageNumber" role="row" aria-label="custom-check-pagination" />
           </div>
         </div>
-      </section>
-    </div>
-  </ServiceControlAvailable>
+      </div>
+    </section>
+  </div>
 </template>
