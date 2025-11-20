289e7cd847e7c5a6496e52db950d76a065bb3f06
Warwick Schroeder <warwick.schroeder@particular.net>
2025-11-20 12:43:18 +0800

    Initial checkin for the Platform Capabilities section on the dashboard

---
+++ src/Frontend/src/components/platformcapabilities/types.ts
@@ -0,0 +1,12 @@
+export interface StatusIndicator {
+  label: string;
+  status: CapabilityStatus;
+  tooltip: string;
+}
+
+export enum CapabilityStatus {
+  Unavailable = "Unavailable", // Instance is configured but not responding or not available
+  Available = "Available", // Instance is available and responding
+  PartiallyAvailable = "Data Unavailable", // Instance is available but not data is flowing for reasons
+  NotConfigured = "Not Configured", // Instance is not configured. Promo should be shown
+}
