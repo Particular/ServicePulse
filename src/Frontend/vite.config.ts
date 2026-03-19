22aa5e5bf6d596560a19a34c0a69db7d079a9eda
John Simons <john.simons@particular.net>
2026-03-19 16:39:12 +1000

    Use wildcard for localhost WebSocket port in CSP
    This simplifies the CSP configuration by allowing any port on localhost for WebSocket connections, removing the need to pass the specific host port as an argument and it doesn't really matter because this is only for dev

--- src/Frontend/vite.config.ts
+++ src/Frontend/vite.config.ts
@@ -5,12 +5,12 @@ import { DevTools } from "@vitejs/devtools";
 import path from "path";
 import checker from "vite-plugin-checker";
 
-function createCSPOverrides(hostPort: number, configuredDestinations: string[]) {
+function createCSPOverrides(configuredDestinations: string[]) {
   const destinations = configuredDestinations.join(" ");
 
   return (
     "default-src 'self';" +
-    `connect-src ws://localhost:${hostPort} https://platformupdate.particular.net ${destinations} 'self';` +
+    `connect-src ws://localhost:* https://platformupdate.particular.net ${destinations} 'self';` +
     "font-src 'self' https://fonts.gstatic.com/ data:;" +
     `img-src data: 'self';` +
     `script-src eval: inline: https://platformupdate.particular.net ${destinations} 'self' 'unsafe-eval' 'unsafe-inline';` +
@@ -68,7 +68,7 @@ export default defineConfig({
   },
   server: {
     headers: {
-      "Content-Security-Policy": createCSPOverrides(port, defaultUrls),
+      "Content-Security-Policy": createCSPOverrides(defaultUrls),
     },
     host: true,
     port: port,
