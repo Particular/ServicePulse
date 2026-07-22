9c3334ed5b1dc3e9505d07961f380ecb4b63f1e7
Phil Bastian <phil.bastian@particular.net>
2026-07-22 09:31:20 +0800

    more lint issues

--- src/Frontend/src/views/throughputreport/licenseDetails/UploadMetadata.vue
+++ src/Frontend/src/views/throughputreport/licenseDetails/UploadMetadata.vue
@@ -21,6 +21,7 @@ async function handleSubmit() {
   try {
     await licenseDetailsStore.uploadEndpointMetadata(file.value);
   } catch (e) {
+    // eslint-disable-next-line no-console
     console.error(e);
     uploadError.value = true;
   } finally {
